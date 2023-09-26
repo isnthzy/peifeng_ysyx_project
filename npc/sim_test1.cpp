cff1bf469e55e95589d24268eb9244144d73df3d
tracer-ysyx <tracer@ysyx.org>
2023-09-26 22:02:43 +0800

    > sim RTL ysyx_22040000 王植鑫 Linux ysyx 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:02:43 up  3:04,  3 users,  load average: 0.27, 0.22, 0.19

---
+++ npc/sim_test1.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Vtop.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+int main(int argc,char** argv){
+    VerilatedContext* contextp=new VerilatedContext;
+    contextp->commandArgs(argc,argv);
+    Vtop* top=new Vtop{contextp};
+
+    VerilatedVcdC* tfp=new VerilatedVcdC;
+    contextp->traceEverOn(true);
+    top->trace(tfp,0);
+    tfp->open("wave.vcd");
+    int i=20;
+    while(!contextp->gotFinish()&&i>=0){
+        int a=rand()&1;
+        int b=rand()&1;
+        top->a=a;
+        top->b=b;
+        top->eval();
+        printf("a=%d,b=%d,f=%d\n",a,b,top->f);
+
+        tfp->dump(contextp->time());
+        contextp->timeInc(1);
+
+        assert(top->f==(a^b));
+
+        i--;
+    }
+    delete top;
+    delete contextp;
+    tfp->close();
+    return 0;
+}
