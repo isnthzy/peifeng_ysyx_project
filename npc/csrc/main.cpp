1c66082d7473080beda9edbe90b5f0328f5c2486
tracer-ysyx <tracer@ysyx.org>
2023-10-03 15:52:14 +0800

    > sim RTL ysyx_22040000 王植鑫 Linux ysyx 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:52:14 up  1:12,  1 user,  load average: 1.61, 1.83, 1.66

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,10 @@
 #include <nvboard.h>
 #include <Vtop.h>
-
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "verilated.h"
+#include "verilated_vcd_c.h"
 static TOP_NAME dut;
 
 void nvboard_bind_all_pins(Vtop* top);
@@ -16,14 +20,27 @@ static void reset(int n) {
   dut.rst = 0;
 }
 
-int main() {
+int main(int argc,char** argv) {
+  VerilatedContext* contextp=new VerilatedContext;
+  contextp->commandArgs(argc,argv);
+  Vtop* top=new Vtop{contextp};
+
+  VerilatedVcdC* tfp=new VerilatedVcdC;
+  contextp->traceEverOn(true);
+  top->trace(tfp,0);
+  tfp->open("wave.vcd");
+
   nvboard_bind_all_pins(&dut);
   nvboard_init();
 
   reset(10);
-
-  while(1) {
+  int i=100;
+  while(i--) {
     nvboard_update();
     single_cycle();
   }
+  delete top;
+  delete contextp;
+  tfp->close();
+  return 0;
 }
