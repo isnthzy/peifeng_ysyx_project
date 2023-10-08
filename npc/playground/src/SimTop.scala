0fda2a5095cb1e0b50ff59da922d7493da0c78e9
tracer-ysyx <tracer@ysyx.org>
2023-10-08 17:04:32 +0800

    > generate verilog ysyx_22040000 王植鑫 Linux ysyx 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:04:32 up 22:24,  1 user,  load average: 1.41, 1.31, 1.30

--- npc/playground/src/SimTop.scala
+++ npc/playground/src/SimTop.scala
@@ -7,14 +7,44 @@ class SimTop extends Module {
     val en = Input(Bool())
     val out = Output(UInt(3.W))
   })
-  when(io.en){
-    io.out:=0.U
+  val encode = Module(new encode82())
+  val seg = Module(new bcd7seg())
+  encode.encode.in := io.in
+  encode.encode.en := io.en
+
+  seg.seg.in := encode.encode.out
+  io.out := seg.seg.out
+
+}
+
+class encode82 extends Module {
+  val encode = IO(new Bundle {
+    val in = Input(UInt(8.W))
+    val en = Input(Bool())
+    val out = Output(UInt(3.W))
+  })
+  when(encode.en){
+    encode.out:=0.U
     for(i<-0 until 8){
-      when(io.in(i)===1.U){
-        io.out:=i.U(3.W)
+      when(encode.in(i)===1.U){
+        encode.out:=i.U(3.W)
       }
     }
   }otherwise{
-    io.out:=0.U
+    encode.out := 0.U
   }
+}
+
+class bcd7seg extends Module{
+  val seg = IO(new Bundle {
+    val in = Input(UInt(3.W))
+    val out= Output(UInt(7.W))
+  })
+  seg.out := MuxLookup(seg.in, 0.U)(Seq(
+    0.U -> "b0000000".U, 1.U -> "b1111001".U, 
+    2.U -> "b0100100".U, 3.U -> "b0110000".U,
+    4.U -> "b0011001".U, 5.U -> "b0010010".U,
+    6.U -> "b0000010".U, 7.U -> "b1111000".U, 
+    8.U -> "b0000000".U, 9.U -> "b0010000".U
+  ))
 }
