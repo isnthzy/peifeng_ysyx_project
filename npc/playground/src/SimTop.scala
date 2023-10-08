9d0716754bc7e6657aedda4691be9582141fe7d6
tracer-ysyx <tracer@ysyx.org>
2023-10-08 17:19:27 +0800

    > generate verilog ysyx_22040000 王植鑫 Linux ysyx 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:19:27 up 22:39,  1 user,  load average: 1.28, 1.46, 1.36

--- npc/playground/src/SimTop.scala
+++ npc/playground/src/SimTop.scala
@@ -5,17 +5,19 @@ class SimTop extends Module {
   val io = IO(new Bundle {
     val in = Input(UInt(8.W))
     val en = Input(Bool())
-    val out = Output(UInt(3.W))
-    val a = Output(Bool())
+    val led_out = Output(UInt(3.W))
+    val hex_out = Output(UInt(7.W))
+    val led4 = Output(Bool())
   })
   val encode = Module(new encode82())
   val seg = Module(new bcd7seg())
   encode.encode.in := io.in
   encode.encode.en := io.en
-  io.a := Mux(io.in === 0.U, true.B, false.B)
+  io.led4 := Mux(io.in === 0.U, true.B, false.B)
 
   seg.seg.in := encode.encode.out
-  io.out := seg.seg.out
+  io.led_out := encode.encode.out
+  io.hex_out := seg.seg.out
 
 }
 
