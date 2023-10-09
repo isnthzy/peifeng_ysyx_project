1a5dbb126462b3dd79a0abc6a6dd51baf00359c4
tracer-ysyx <tracer@ysyx.org>
2023-10-09 18:36:29 +0800

    > generate verilog ysyx_22040000 王植鑫 Linux ysyx 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:36:29 up 1 day, 23:08,  1 user,  load average: 1.73, 1.11, 0.98

--- npc/playground/src/SimTop.scala
+++ npc/playground/src/SimTop.scala
@@ -3,51 +3,29 @@ import chisel3.util._
 
 class SimTop extends Module {
   val io = IO(new Bundle {
-    val in = Input(UInt(8.W))
-    val en = Input(Bool())
-    val led_out = Output(UInt(3.W))
-    val hex_out = Output(UInt(7.W))
-    val led4 = Output(Bool())
+    val op = Input(UInt(3.W))
+    val a  = Input(UInt(4.W))
+    val b  = Input(UInt(4.W))
+    val result = Output(UInt(4.W))
   })
-  val encode = Module(new encode82())
-  val seg = Module(new bcd7seg())
-  encode.encode.in := io.in
-  encode.encode.en := io.en
-  io.led4 := Mux(io.in === 0.U, true.B, false.B)
-
-  seg.seg.in := encode.encode.out
-  io.led_out := encode.encode.out
-  io.hex_out := seg.seg.out
-
-}
-
-class encode82 extends Module {
-  val encode = IO(new Bundle {
-    val in = Input(UInt(8.W))
-    val en = Input(Bool())
-    val out = Output(UInt(3.W))
-  })
-  when(encode.en){
-    encode.out:=0.U
-    for(i<-0 until 8){
-      when(encode.in(i)===1.U){
-        encode.out:=i.U(3.W)
-      }
-    }
-  }otherwise{
-    encode.out := 0.U
-  }
-}
-
-class bcd7seg extends Module{
-  val seg = IO(new Bundle {
-    val in = Input(UInt(3.W))
-    val out= Output(UInt(7.W))
-  })
-  seg.out := MuxLookup(seg.in, 0.U)(Seq(
-    0.U -> "b1000000".U, 1.U -> "b1111001".U, 
-    2.U -> "b0100100".U, 3.U -> "b0110000".U,
-    4.U -> "b0011001".U, 5.U -> "b0010010".U,
-    6.U -> "b0000010".U, 7.U -> "b1111000".U, 
+  
+  val op_add=io.a+io.b
+  val overflow_add=(io.a(3)&&io.b(3))&&(op_add(4)!=io.a(4))
+  
+  val op_sub=io.a-io.b
+  val overflow_sub=(io.a(3)&&io.b(3))&&(op_sub(4)!=io.a(4))
+  
+  val op_neg=~io.a
+  
+  val op_and=
+  val op_or =
+  val op_xor=
+  val op tha=
+  val op_eq =
+  io.result := MuxLookup(io.op, 0.U)(Seq(
+    0.U -> op_add, 1.U -> op_sub, 
+    2.U -> op_neg, 3.U -> op_and,
+    4.U -> op_or , 5.U -> op_xor,
+    6.U -> op tha, 7.U -> op tha, 
   ))
 }
