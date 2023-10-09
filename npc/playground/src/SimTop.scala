21a63f8b4b4524863d2a189bac2151e45d04bcac
tracer-ysyx <tracer@ysyx.org>
2023-10-09 19:22:56 +0800

    > generate verilog ysyx_22040000 王植鑫 Linux ysyx 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:22:56 up 1 day, 23:55,  1 user,  load average: 1.24, 1.16, 1.07

--- npc/playground/src/SimTop.scala
+++ npc/playground/src/SimTop.scala
@@ -1,5 +1,5 @@
 import chisel3._
-import chisel3.util._
+import chisel3.util._  
 
 class SimTop extends Module {
   val io = IO(new Bundle {
@@ -7,12 +7,14 @@ class SimTop extends Module {
     val a  = Input(UInt(4.W))
     val b  = Input(UInt(4.W))
     val result = Output(UInt(4.W))
+    val of = Output(Bool())
   })
   
   val op_add =io.a+io.b
   val overflow_add =(io.a(3)&&io.b(3))&&(op_add(3)=/=io.a(3))
   
-  val op_sub =io.a-io.b
+  val tmp = (("b1111".U)^io.b)+1.U
+  val op_sub =io.a+tmp
   val overflow_sub =(io.a(3)&&io.b(3))&&(op_sub(3)=/=io.a(3))
   
   val op_neg= ~io.a
@@ -31,5 +33,8 @@ class SimTop extends Module {
     2.U -> op_neg, 3.U -> op_and,
     4.U -> op_or , 5.U -> op_xor,
     6.U -> op_tha, 7.U -> op_eq
+  ))
+  io.of := MuxLookup(io.op, 0.U)(Seq(
+    0.U -> overflow_add, 1.U -> overflow_sub, 
   ))
 }
