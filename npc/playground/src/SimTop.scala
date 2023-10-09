f09524e082f4afe62ec45caa51fb0b091ae7925c
tracer-ysyx <tracer@ysyx.org>
2023-10-09 18:52:52 +0800

    > generate verilog ysyx_22040000 王植鑫 Linux ysyx 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:52:52 up 1 day, 23:25,  1 user,  load average: 1.03, 1.00, 0.99

--- npc/playground/src/SimTop.scala
+++ npc/playground/src/SimTop.scala
@@ -9,23 +9,27 @@ class SimTop extends Module {
     val result = Output(UInt(4.W))
   })
   
-  val op_add=io.a+io.b
-  val overflow_add=(io.a(3)&&io.b(3))&&(op_add(4)!=io.a(4))
+  val op_add =io.a+io.b
+  val overflow_add =(io.a(3)&&io.b(3))&&((op_add(4)!=io.a(4))).asBool()
   
-  val op_sub=io.a-io.b
-  val overflow_sub=(io.a(3)&&io.b(3))&&(op_sub(4)!=io.a(4))
+  val op_sub =io.a-io.b
+  val overflow_sub =(io.a(3)&&io.b(3))&&((op_sub(4)!=io.a(4))).asBool()
   
-  val op_neg=~io.a
+  val op_neg= ~io.a
+
+  val op_and= io.a&io.b
   
-  val op_and=
-  val op_or =
-  val op_xor=
-  val op tha=
-  val op_eq =
+  val op_or = io.a|io.b
+  
+  val op_xor= io.a^io.b
+  
+  val op_tha= Mux(io.a<io.b, 1.U, 0.U)
+  
+  val op_eq = Mux(io.a===io.b,1.U,0.U)
   io.result := MuxLookup(io.op, 0.U)(Seq(
     0.U -> op_add, 1.U -> op_sub, 
     2.U -> op_neg, 3.U -> op_and,
     4.U -> op_or , 5.U -> op_xor,
-    6.U -> op tha, 7.U -> op tha, 
+    6.U -> op_tha, 7.U -> op_eq
   ))
 }
