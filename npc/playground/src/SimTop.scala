8bacddcb5875a32c32f937cf5d7b95dd25ccd2ec
tracer-ysyx <tracer@ysyx.org>
2023-10-10 12:45:59 +0800

    > generate verilog ysyx_22040000 王植鑫 Linux ysyx 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:45:59 up 2 days,  5:30,  1 user,  load average: 2.12, 1.43, 1.04

--- npc/playground/src/SimTop.scala
+++ npc/playground/src/SimTop.scala
@@ -7,7 +7,7 @@ class SimTop extends Module {
     val seg1 =Output(UInt(7.W))
     val seg2 =Output(UInt(7.W))
   })  
-  val reg =RegInit("b01000000".U)
+  val reg =RegInit("b01000000".U(8.W))
   val x8 =reg(0)+reg(2)+reg(3)+reg(4)
   when(io.in===1.U){
     reg := Cat(x8,reg(6,0))
