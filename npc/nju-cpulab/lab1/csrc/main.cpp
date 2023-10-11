f7adad421af9d17bab76abc3539150bda772cf48
wangxin <1721791534@qq.com>
2023-10-11 19:40:26 +0800

    pa1练习0.1

---
+++ npc/nju-cpulab/lab1/csrc/main.cpp
@@ -0,0 +1,48 @@
+#include <nvboard.h>
+#include <VSimTop.h>
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(VSimTop* top);
+void step_and_dump_wave(){
+  top->eval();
+  contextp->timeInc(1);
+  tfp->dump(contextp->time());
+}
+void sim_init(){
+  contextp = new VerilatedContext;
+  tfp = new VerilatedVcdC;
+  top = new Vmux21;
+  contextp->traceEverOn(true);
+  top->trace(tfp, 0);
+  tfp->open("dump.vcd");
+}
+void sim_exit(){
+  step_and_dump_wave();
+  tfp->close();
+}
+
+static void single_cycle() {
+  dut.clock = 0; dut.eval();
+  dut.clock = 1; dut.eval();
+}
+
+static void reset(int n) {
+  dut.reset = 1;
+  while (n -- > 0) single_cycle();
+  dut.reset = 0;
+}
+
+int main() {
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+
+  reset(10);
+
+  while(1) {
+    nvboard_update();
+    single_cycle();
+  }
+}
---
+++ npc/nju-cpulab/lab3/playground/src/SimTop.scala
@@ -0,0 +1,45 @@
+import chisel3._
+import chisel3.util._  
+
+class SimTop extends Module {
+  val io = IO(new Bundle {
+    val op = Input(UInt(3.W))
+    val a  = Input(UInt(4.W))
+    val b  = Input(UInt(4.W))
+    val out = Output(UInt(4.W))
+    val of = Output(Bool())
+    val out_c = Output(UInt(1.W))
+  })
+  val sum =io.a+&io.b
+  val (out_add,op_add)=(sum(4),sum(3,0))
+  val overflow_add =(io.a(3)&&io.b(3))&&(op_add(3)=/=io.a(3))
+  
+  val tmp = (("b1111".U)^io.b)+1.U
+  val sub =io.a+&tmp
+  val (out_sub,op_sub)=(sub(4),sub(3,0))
+  val overflow_sub =(io.a(3)&&io.b(3))&&(op_sub(3)=/=io.a(3))
+  
+  val op_neg= ~io.a
+
+  val op_and= io.a&io.b
+  
+  val op_or = io.a|io.b
+  
+  val op_xor= io.a^io.b
+  
+  val op_tha= Mux(io.a<io.b, 1.U, 0.U)
+  
+  val op_eq = Mux(io.a===io.b,1.U,0.U)
+  io.out := MuxLookup(io.op, 0.U)(Seq(
+    0.U -> op_add, 1.U -> op_sub, 
+    2.U -> op_neg, 3.U -> op_and,
+    4.U -> op_or , 5.U -> op_xor,
+    6.U -> op_tha, 7.U -> op_eq
+  ))
+  io.of := MuxLookup(io.op, 0.U)(Seq(
+    0.U -> overflow_add, 1.U -> overflow_sub,
+  ))
+  io.out_c := MuxLookup(io.op, 0.U)(Seq(
+    0.U -> out_add, 1.U -> out_sub 
+  ))
+}
