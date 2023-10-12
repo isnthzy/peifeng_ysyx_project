9fec7fa24cfae6160347c9f2d9946c92ff615fc0
tracer-ysyx <tracer@ysyx.org>
2023-10-12 20:30:41 +0800

    > generate verilog ysyx_22040000 王植鑫 Linux ysyx 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:30:41 up  3:42,  1 user,  load average: 3.80, 2.55, 1.85

--- npc/playground/src/SimTop.scala
+++ npc/playground/src/SimTop.scala
@@ -12,24 +12,24 @@ class SimTop extends Module {
     val seg5 = Output(UInt(7.W))
     val seg6 = Output(UInt(7.W))
   })  
-  val PS2Key   = Module(new PS2Keyboard())
-  val bcd7seg1 = Module(new bcd7seg())
-  val bcd7seg2 = Module(new bcd7seg())
-  val bcd7seg3 = Module(new bcd7seg())
-  val bcd7seg4 = Module(new bcd7seg())
-  val bcd7seg5 = Module(new bcd7seg())
-  val bcd7seg6 = Module(new bcd7seg())
-  PS2Key.keyboard.ps2_clk := io.ps2_clk
-  PS2Key.keyboard.ps2_data := io.ps2_data
+  // val PS2Key   = Module(new PS2Keyboard())
+  // val bcd7seg1 = Module(new bcd7seg())
+  // val bcd7seg2 = Module(new bcd7seg())
+  // val bcd7seg3 = Module(new bcd7seg())
+  // val bcd7seg4 = Module(new bcd7seg())
+  // val bcd7seg5 = Module(new bcd7seg())
+  // val bcd7seg6 = Module(new bcd7seg())
+  // PS2Key.keyboard.ps2_clk := io.ps2_clk
+  // PS2Key.keyboard.ps2_data := io.ps2_data
 
-  bcd7seg1.seg.in := PS2Key.keyboard.out(3,0)
-  bcd7seg2.seg.in := PS2Key.keyboard.out(7,4)
-  bcd7seg1.seg.in := PS2Key.keyboard.num(3,0)
-  bcd7seg2.seg.in := PS2Key.keyboard.num(7,4)
-  io.seg1 := bcd7seg1.seg.out
-  io.seg2 := bcd7seg2.seg.out
-  io.seg5 := bcd7seg1.seg.out
-  io.seg6 := bcd7seg2.seg.out
+  // bcd7seg1.seg.in := PS2Key.keyboard.out(3,0)
+  // bcd7seg2.seg.in := PS2Key.keyboard.out(7,4)
+  // bcd7seg1.seg.in := PS2Key.keyboard.num(3,0)
+  // bcd7seg2.seg.in := PS2Key.keyboard.num(7,4)
+  // io.seg1 := bcd7seg1.seg.out
+  // io.seg2 := bcd7seg2.seg.out
+  // io.seg5 := bcd7seg1.seg.out
+  // io.seg6 := bcd7seg2.seg.out
 }
 
 class KeycodeToAscii extends Module {
@@ -57,35 +57,35 @@ class bcd7seg extends Module{
   ))
 }
 
-class PS2Keyboard extends Module {
-  val keyboard = IO(new Bundle {
-    val ps2_clk = Input(Bool())
-    val ps2_data = Input(Bool())
-    val out = Output(UInt(8.W))
-    val num = Output(UInt(8.W))
-  })
+// class PS2Keyboard extends Module {
+//   val keyboard = IO(new Bundle {
+//     val ps2_clk = Input(Bool())
+//     val ps2_data = Input(Bool())
+//     val out = Output(UInt(8.W))
+//     val num = Output(UInt(8.W))
+//   })
 
-  val buffer = RegInit(0.U(10.W))
-  val count = RegInit(0.U(4.W))
-  val ps2_clk_sync = RegInit(0.U(3.W))
-  val numReg = RegInit(0.U(8.W))
-  ps2_clk_sync := Cat(ps2_clk_sync(1, 0), keyboard.ps2_clk)
+//   val buffer = RegInit(0.U(10.W))
+//   val count = RegInit(0.U(4.W))
+//   val ps2_clk_sync = RegInit(0.U(3.W))
+//   val numReg = RegInit(0.U(8.W))
+//   ps2_clk_sync := Cat(ps2_clk_sync(1, 0), keyboard.ps2_clk)
 
-  val sampling = ps2_clk_sync(2) & ~ps2_clk_sync(1)
-  keyboard.num := numReg
+//   val sampling = ps2_clk_sync(2) & ~ps2_clk_sync(1)
+//   keyboard.num := numReg
 
-  when(sampling === true.B) {
-    when(count === 10.U) {
-      when((buffer(0) === 0.U) && keyboard.ps2_data && (~buffer(9, 1).orR)) { // start bit, stop bit, odd parity
-        keyboard.out := buffer(8, 1)
+//   when(sampling === true.B) {
+//     when(count === 10.U) {
+//       when((buffer(0) === 0.U) && keyboard.ps2_data && (~buffer(9, 1).orR)) { // start bit, stop bit, odd parity
+//         keyboard.out := buffer(8, 1)
 
-        val incrementedNum = numReg + 1.U // 递增numReg的值
-        numReg := incrementedNum // 更新numReg的值
-      }
-      count := 0.U // for next
-    }.otherwise {
-      buffer(count) := keyboard.ps2_data // store ps2_data
-      count := count + 1.U
-    }
-  }
-}
+//         val incrementedNum = numReg + 1.U // 递增numReg的值
+//         numReg := incrementedNum // 更新numReg的值
+//       }
+//       count := 0.U // for next
+//     }.otherwise {
+//       buffer(count) := keyboard.ps2_data // store ps2_data
+//       count := count + 1.U
+//     }
+//   }
+// }
