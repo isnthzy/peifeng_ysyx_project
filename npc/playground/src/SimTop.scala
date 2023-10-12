3088e58b03b58aa5ca62d6bd84dd840022d35fa0
tracer-ysyx <tracer@ysyx.org>
2023-10-12 22:22:57 +0800

    > generate verilog ysyx_22040000 王植鑫 Linux ysyx 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:22:57 up 11 min,  1 user,  load average: 4.13, 2.45, 1.48

--- npc/playground/src/SimTop.scala
+++ npc/playground/src/SimTop.scala
@@ -68,20 +68,22 @@ class PS2Keyboard extends Module {
   val ps2_clk_sync = RegInit(0.U(3.W))
   ps2_clk_sync := Cat(ps2_clk_sync(1, 0), keyboard.ps2_clk)
   val sampling = ps2_clk_sync(2) & ~ps2_clk_sync(1)
+  
+  keyboard.out := 0.U
+  keyboard.num := 0.U
   when(sampling) {
-    keyboard.out := 0.U
-    keyboard.num := 0.U
-    // when(count === 10.U) {
-    //   count := 0.U 
-    //   when((buffer(0) === 0.U) && keyboard.ps2_data && (~buffer(9, 1).orR)) { // start bit, stop bit, odd parity
 
-    //   }
+    when(count === 10.U) {
+      // count := 0.U 
+      // when((buffer(0) === 0.U) && keyboard.ps2_data && (~buffer(9, 1).orR)) { // start bit, stop bit, odd parity
+
+      // }
       
-    // }.otherwise {
-    //   count := count + 1.U
-    //   buffer(count) := keyboard.ps2_data // store ps2_data
+    }.otherwise {
+      count := count + 1.U
+      buffer(count) := keyboard.ps2_data // store ps2_data
       
-    // }
+    }
   }
 
 }
