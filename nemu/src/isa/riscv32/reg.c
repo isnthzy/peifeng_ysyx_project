0acc3c7cd4d624b89453e2b15831baeed136033a
tracer-ysyx <tracer@ysyx.org>
2023-10-14 19:52:43 +0800

    > compile NEMU ysyx_22040000 王植鑫 Linux ysyx 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:52:43 up 12:43,  1 user,  load average: 3.91, 3.14, 2.47

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -25,6 +25,7 @@ const char *regs[] = {
 
 void isa_reg_display() {
   int i;
+  printf("name value name value name value name value");
   for(i=0;i<32;i+=4){
     printf("name:%3s 0x%08x name:%3s 0x%08x name:%3s 0x%08x name:%3s 0x%08x\n",\
     regs[i],gpr(i),regs[i+1],gpr(i+1),regs[i+2],gpr(i+2),regs[i+3],gpr(i+3));
