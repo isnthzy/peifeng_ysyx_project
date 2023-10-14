8c2cc0ff9fd3259e082e78333601e4e1e7dea3be
tracer-ysyx <tracer@ysyx.org>
2023-10-14 17:19:45 +0800

    > compile NEMU ysyx_22040000 王植鑫 Linux ysyx 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:19:45 up 11:02,  1 user,  load average: 1.58, 1.94, 1.89

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -89,11 +89,10 @@ static struct {
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
   { "si [N]", "Lets the program pause after executing N instructions in a single step.\
-    When N is not given, the default is 1", cmd_si },
-  { "info SUBCMD", "info r:Printing Register Status \n \
-    info w:Print watchpoint information",cmd_info },
+When N is not given, the default is 1", cmd_si },
+  { "info SUBCMD", "info r:Printing Register Status \n info w:Print watchpoint information",cmd_info },
   { "x N EXPR", "Find the value of the expression EXPR, use the result as the starting memory \
-address, and output N consecutive 4-byte outputs in hexadecimal." ,cmd_x},
+address,\n and output N consecutive 4-byte outputs in hexadecimal." ,cmd_x},
   { "p EXPR", "Find the value of the expression EXPR" , cmd_p},
   { "w EXPR", "Suspends program execution when the value of expression EXPR changes." ,cmd_w},
   { "d N", "Delete the monitoring point with serial number N" ,cmd_d},
