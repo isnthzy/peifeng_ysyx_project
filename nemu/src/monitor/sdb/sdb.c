ea04476f811bb283f6b40ff250573d6153b6ea3e
tracer-ysyx <tracer@ysyx.org>
2023-10-15 11:14:39 +0800

    > compile NEMU ysyx_22040000 王植鑫 Linux ysyx 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:14:39 up  2:33,  1 user,  load average: 0.65, 1.10, 1.05

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -90,7 +90,7 @@ static int cmd_x(char *args) {
   for(i=0;i<s1;i++){
     printf("0x%08x: ",addr);
     vaddr_t data = vaddr_read(addr,4);
-    printf("%08x\n",data); //查阅得多数riscv为小段序,后续应改为小端显示内存
+    // printf("%08x\n",data); //查阅得多数riscv为小段序,后续应改为小端显示内存
     for(j=0;j<4;j++){
       printf("0x%02x ",data&0xff);
       data=data<<8;
