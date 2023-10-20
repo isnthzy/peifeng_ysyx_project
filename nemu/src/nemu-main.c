2e53e425b53443124c05193ebf5173416f5f3ebe
tracer-ysyx <tracer@ysyx.org>
2023-10-20 08:32:14 +0800

    > compile NEMU ysyx_22040000 王植鑫 Linux ysyx 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:32:14 up 2 days,  3:21,  1 user,  load average: 3.21, 2.38, 2.10

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -30,25 +30,25 @@ int main(int argc, char *argv[]) {
 #else
   init_monitor(argc, argv);
 #endif
-  // char ea[255];
-  // char *val;
-  // char *evall;
-  // /* Start engine. */
-  // FILE *fp=fopen("/home/wangxin/ysyx-workbench/nemu/tools/gen-expr/input", "r");
-  // if (fp == NULL) {
-  //       printf("无法打开文件。\n");
-  //       return 1;
-  // }
-  // int cnt=1;
-  // while(fgets(ea,255,fp)){
-  //   val = strtok(ea, " ");
-  //   evall = strtok(ea, " ");
-  //   bool flag=true;
-  //   word_t value_p = expr(evall,&flag);
-  //   word_t u32;
-  //   u32 = strtoul(val, NULL, 10);
-  //   if(u32==value_p) printf("%d true\n",cnt++);
-  // }
+  char ea[1024];
+  char *val;
+  char *evall;
+  /* Start engine. */
+  FILE *fp=fopen("/home/wangxin/ysyx-workbench/nemu/tools/gen-expr/input", "r");
+  if (fp == NULL) {
+        printf("无法打开文件。\n");
+        return 1;
+  }
+  int cnt=1;
+  while(fgets(ea,1024,fp)){
+    val = strtok(ea, " ");
+    evall = strtok(ea, " ");
+    bool flag=true;
+    word_t value_p = expr(evall,&flag);
+    word_t u32;
+    u32 = strtoul(val, NULL, 10);
+    if(u32==value_p) printf("%d true\n",cnt++);
+  }
   engine_start();
 
   return is_exit_status_bad();
