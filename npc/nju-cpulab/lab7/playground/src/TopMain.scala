6d3d2ef04507927d6a300d006bcf9db467ea1162
tracer-ysyx <tracer@ysyx.org>
2023-10-21 19:45:34 +0800

    > compile NEMU ysyx_22040000 王植鑫 Linux ysyx 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:45:34 up 3 days, 14:34,  1 user,  load average: 1.31, 0.85, 0.54

--- npc/nju-cpulab/lab7/playground/src/TopMain.scala
+++
@@ -1,7 +0,0 @@
-import circt.stage._
-
-object TopMain extends App {
-  def top = new SimTop()
-  val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
-  (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
-}
