3d4757babb59d26cecb16c6de730f9b3ff27176c
tracer-ysyx <tracer@ysyx.org>
2023-10-27 07:30:50 +0800

    > sim RTL ysyx_22040000 王植鑫 Linux ysyx 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:30:50 up  9:43,  1 user,  load average: 1.00, 0.73, 0.86

--- npc/vsrc/ps2_keyboard.v
+++ npc/vsrc/ps2_keyboard.v
@@ -22,6 +22,7 @@ module ps2_keyboard(clk,resetn,ps2_clk,ps2_data,data);
                 if ((buffer[0] == 0) &&  // start bit
                     (ps2_data)       &&  // stop bit
                     (^buffer[9:1])) begin      // odd  parity
+                    $display("receive %x", buffer[8:1]);
                     data <=buffer[8:1];
                 end
                 count <= 0;     // for next
