92469bcb2fcf4cbd9e87a1bc5c5d03d5ca211080
tracer-ysyx <tracer@ysyx.org>
2023-10-26 22:27:40 +0800

    > sim RTL ysyx_22040000 王植鑫 Linux ysyx 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:27:40 up 40 min,  1 user,  load average: 0.46, 0.73, 0.73

--- npc/vsrc/SimTop.v
+++ npc/vsrc/SimTop.v
@@ -1,10 +1,14 @@
 `timescale 1ns / 1ps
 module SimTop(
   input reset,
+  input ps2_clk,
+  input ps2_data,
   output [6:0]io_seg1,
   output [6:0]io_seg2,
   output [6:0]io_seg3,
-  output [6:0]io_seg4
+  output [6:0]io_seg4,
+  output [6:0]io_seg5,
+  output [6:0]io_seg6
 );
 
 /* parameter */
@@ -17,10 +21,10 @@ wire ready,overflow;
 wire kbd_clk, kbd_data;
 reg nextdata_n;
 
-ps2_keyboard_model model(
-    .ps2_clk(kbd_clk),
-    .ps2_data(kbd_data)
-);
+// ps2_keyboard_model model(
+//     .ps2_clk(kbd_clk),
+//     .ps2_data(kbd_data)
+// );
 
 ps2_keyboard inst(
     .clk(clk),
@@ -32,6 +36,7 @@ ps2_keyboard inst(
     .nextdata_n(nextdata_n),
     .overflow(overflow)
 );
+reg [7:0]  count;
 wire [7:0]asciicode;
 wire [3:0]seg1_in;
 wire [3:0]seg2_in;
@@ -63,15 +68,24 @@ bcd7seg seg4(
     .seg_in(seg4_in),
     .seg_out(io_seg4)
 );
-
-// bcd7seg seg5(
-//     .seg_in(),
-//     .seg_out(io_seg5)
-// );
-// bcd7seg seg6(
-//     .seg_in(),
-//     .seg_out(io_seg6)
-// );
+always @(posedge clk)
+begin
+  if(data==8'hF0) begin
+    count=count+8'h1;
+  end
+end
+wire [3:0]count_l;
+wire [3:0]count_h;
+assign count_l=count[3:0];
+assign count_h=count[7:4];
+bcd7seg seg5(
+    .seg_in(count_l),
+    .seg_out(io_seg5)
+);
+bcd7seg seg6(
+    .seg_in(count_h),
+    .seg_out(io_seg6)
+);
 
 initial begin /* clock driver */
     clk = 0;
@@ -79,22 +93,22 @@ initial begin /* clock driver */
         #5 clk = ~clk;
 end
 
-initial begin
-    clrn = 1'b0;  #20;
-    clrn = 1'b1;  #20;
-    model.kbd_sendcode(8'h1C); // press 'A'
-    #20 nextdata_n =1'b0; #20 nextdata_n =1'b1;//read data
-    model.kbd_sendcode(8'hF0); // break code
-    #20 nextdata_n =1'b0; #20 nextdata_n =1'b1; //read data
-    model.kbd_sendcode(8'h1C); // release 'A'
-    #20 nextdata_n =1'b0; #20 nextdata_n =1'b1; //read data
-    model.kbd_sendcode(8'h1B); // press 'S'
-    #20 model.kbd_sendcode(8'h1B); // keep pressing 'S'
-    #20 model.kbd_sendcode(8'h1B); // keep pressing 'S'
-    model.kbd_sendcode(8'hF0); // break code
-    model.kbd_sendcode(8'h1B); // release 'S'
-    #20;
-    $stop;
-end
+// initial begin
+//     clrn = 1'b0;  #20;
+//     clrn = 1'b1;  #20;
+//     model.kbd_sendcode(8'h1C); // press 'A'
+//     #20 nextdata_n =1'b0; #20 nextdata_n =1'b1;//read data
+//     model.kbd_sendcode(8'hF0); // break code
+//     #20 nextdata_n =1'b0; #20 nextdata_n =1'b1; //read data
+//     model.kbd_sendcode(8'h1C); // release 'A'
+//     #20 nextdata_n =1'b0; #20 nextdata_n =1'b1; //read data
+//     model.kbd_sendcode(8'h1B); // press 'S'
+//     #20 model.kbd_sendcode(8'h1B); // keep pressing 'S'
+//     #20 model.kbd_sendcode(8'h1B); // keep pressing 'S'
+//     model.kbd_sendcode(8'hF0); // break code
+//     model.kbd_sendcode(8'h1B); // release 'S'
+//     #20;
+//     $stop;
+// end
 
 endmodule
