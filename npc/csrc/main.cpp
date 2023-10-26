d241c5a0faa406205fa79289703b8acd5af9226d
tracer-ysyx <tracer@ysyx.org>
2023-10-26 20:23:39 +0800

    > sim RTL ysyx_22040000 王植鑫 Linux ysyx 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:23:39 up 23:08,  1 user,  load average: 1.66, 1.33, 1.21

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -4,61 +4,44 @@
 #include "verilated_vcd_c.h"
 
 static TOP_NAME dut;
-VerilatedContext* contextp = NULL;
-VerilatedVcdC* tfp = NULL;
-
-static VSimTop* SimTop;
-
 
 void nvboard_bind_all_pins(VSimTop* top);
 // void step_and_dump_wave(){
-//   SimTop->eval();
+//   top->eval();
 //   contextp->timeInc(1);
 //   tfp->dump(contextp->time());
 // }
 // void sim_init(){
 //   contextp = new VerilatedContext;
 //   tfp = new VerilatedVcdC;
-//   SimTop = new VSimTop;
+//   top = new Vmux21;
 //   contextp->traceEverOn(true);
-//   SimTop->trace(tfp, 0);
-//   tfp->open("wave.vcd");
+//   top->trace(tfp, 0);
+//   tfp->open("dump.vcd");
 // }
 // void sim_exit(){
 //   step_and_dump_wave();
 //   tfp->close();
 // }
 
-// static void single_cycle() {
-//   dut.clock = 0; dut.eval();
-//   dut.clock = 1; dut.eval();
-// }
+static void single_cycle() {
+  dut.eval();
+}
 
-// static void reset(int n) {
-//   dut.reset = 1;
-//   while (n -- > 0) single_cycle();
-//   dut.reset = 0;
-// }
+static void reset(int n) {
+  dut.reset = 1;
+  while (n -- > 0) single_cycle();
+  dut.reset = 0;
+}
 
+int main() {
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
 
-int main(int argc,char** argv){
-    VerilatedContext* contextp=new VerilatedContext;
-    contextp->commandArgs(argc,argv);
-    VSimTop* top=new VSimTop{contextp};
+  reset(10);
 
-    VerilatedVcdC* tfp=new VerilatedVcdC;
-    contextp->traceEverOn(true);
-    top->trace(tfp,0);
-    tfp->open("wave.vcd");
-    int i=10000000;
-    while(!contextp->gotFinish()&&i>=0){
-        tfp->dump(contextp->time());
-        contextp->timeInc(1);
-        top->eval();
-        i--;
-    }
-    delete top;
-    delete contextp;
-    tfp->close();
-    return 0;
+  while(1) {
+    nvboard_update();
+    single_cycle();
+  }
 }
