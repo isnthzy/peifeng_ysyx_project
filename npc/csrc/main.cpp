c1058d75089c265e2ce398e7ff309b7603474f27
tracer-ysyx <tracer@ysyx.org>
2023-10-26 20:45:24 +0800

    > sim RTL ysyx_22040000 王植鑫 Linux ysyx 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:45:24 up 23:30,  1 user,  load average: 0.62, 0.63, 0.92

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -4,44 +4,61 @@
 #include "verilated_vcd_c.h"
 
 static TOP_NAME dut;
+VerilatedContext* contextp = NULL;
+VerilatedVcdC* tfp = NULL;
+
+static VSimTop* SimTop;
+
 
 void nvboard_bind_all_pins(VSimTop* top);
 // void step_and_dump_wave(){
-//   top->eval();
+//   SimTop->eval();
 //   contextp->timeInc(1);
 //   tfp->dump(contextp->time());
 // }
 // void sim_init(){
 //   contextp = new VerilatedContext;
 //   tfp = new VerilatedVcdC;
-//   top = new Vmux21;
+//   SimTop = new VSimTop;
 //   contextp->traceEverOn(true);
-//   top->trace(tfp, 0);
-//   tfp->open("dump.vcd");
+//   SimTop->trace(tfp, 0);
+//   tfp->open("wave.vcd");
 // }
 // void sim_exit(){
 //   step_and_dump_wave();
 //   tfp->close();
 // }
 
-static void single_cycle() {
-  dut.eval();
-}
+// static void single_cycle() {
+//   dut.clock = 0; dut.eval();
+//   dut.clock = 1; dut.eval();
+// }
 
-static void reset(int n) {
-  dut.reset = 1;
-  while (n -- > 0) single_cycle();
-  dut.reset = 0;
-}
+// static void reset(int n) {
+//   dut.reset = 1;
+//   while (n -- > 0) single_cycle();
+//   dut.reset = 0;
+// }
 
-int main() {
-  nvboard_bind_all_pins(&dut);
-  nvboard_init();
 
-  reset(10);
+int main(int argc,char** argv){
+    VerilatedContext* contextp=new VerilatedContext;
+    contextp->commandArgs(argc,argv);
+    VSimTop* top=new VSimTop{contextp};
 
-  while(1) {
-    nvboard_update();
-    single_cycle();
-  }
+    VerilatedVcdC* tfp=new VerilatedVcdC;
+    contextp->traceEverOn(true);
+    top->trace(tfp,0);
+    tfp->open("wave.vcd");
+    int i=100000000;
+    while(!contextp->gotFinish()&&i>=0){
+        tfp->dump(contextp->time());
+        contextp->timeInc(1);
+        top->eval();
+        i--;
+    }
+    delete top;
+    delete contextp;
+    tfp->close();
+    return 0;
 }
