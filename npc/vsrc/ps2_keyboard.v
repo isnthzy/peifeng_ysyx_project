33836fd894a016708234053e240f166c838f5857
tracer-ysyx <tracer@ysyx.org>
2023-10-27 08:56:55 +0800

    > sim RTL ysyx_22040000 王植鑫 Linux ysyx 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:56:55 up 11:09,  1 user,  load average: 0.49, 0.66, 0.65

--- npc/vsrc/ps2_keyboard.v
+++ npc/vsrc/ps2_keyboard.v
@@ -6,7 +6,7 @@ module ps2_keyboard(clk,resetn,ps2_clk,ps2_data,data,num);
     reg [9:0] buffer;        // ps2_data bits
     reg [3:0] count;  // count ps2_data bits
     reg [2:0] ps2_clk_sync;
-    reg brek;
+
     always @(posedge clk) begin
         ps2_clk_sync <=  {ps2_clk_sync[1:0],ps2_clk};
     end
@@ -17,7 +17,6 @@ module ps2_keyboard(clk,resetn,ps2_clk,ps2_data,data,num);
         if (resetn == 0) begin // reset
             count <= 0;
             num<=0;
-            brek<=0;
         end
         else begin
             if (sampling) begin
@@ -25,20 +24,12 @@ module ps2_keyboard(clk,resetn,ps2_clk,ps2_data,data,num);
                 if ((buffer[0] == 0) &&  // start bit
                     (ps2_data)       &&  // stop bit
                     (^buffer[9:1])) begin      // odd  parity
-                    
-                    if(brek==1)begin
-                        data<=8'b0;
-                        brek<=0;
-                    end
-                    else begin
-                        data <=buffer[8:1];
-                    end
+                    $display("receive %x", buffer[8:1]);
+                    data <=buffer[8:1];
                     if(data==8'hf0)begin
-                        brek<=1;
                         num<=num+8'b1;
                         $display("num %x", num);
                     end
-                    $display("receive %x", data[7:0]);
                 end
                 count <= 0;     // for next
               end else begin
