2b87258d2024270c9954d2c3203a3c92895c24c8
tracer-ysyx <tracer@ysyx.org>
2023-10-28 15:15:27 +0800

    > generate verilog ysyx_22040000 王植鑫 Linux ysyx 6.2.0-35-generic #35~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Oct  6 10:23:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:15:27 up  3:54,  1 user,  load average: 0.30, 0.37, 0.55

--- npc/playground/src/SimTop.scala
+++ npc/playground/src/SimTop.scala
@@ -1,47 +1,51 @@
 import chisel3._
-import chisel3.util._
+import chisel3.util._  
 
 class SimTop extends Module {
   val io = IO(new Bundle {
-    val in = Input(UInt(8.W))
-    val en = Input(Bool())
-    val led_out = Output(UInt(3.W))
-    val hex_out = Output(UInt(7.W))
-    val led4 = Output(Bool())
-  })
-  val encode = Module(new encode82())
-  val seg = Module(new bcd7seg())
-  encode.encode.in := io.in
-  encode.encode.en := io.en
-  io.led4 := Mux(io.in === 0.U, true.B, false.B)
-
-  seg.seg.in := encode.encode.out
-  io.led_out := encode.encode.out
-  io.hex_out := seg.seg.out
-
-}
-
-class encode82 extends Module {
-  val encode = IO(new Bundle {
-    val in = Input(UInt(8.W))
-    val en = Input(Bool())
-    val out = Output(UInt(3.W))
-  })
-  when(encode.en){
-    encode.out:=0.U
-    for(i<-0 until 8){
-      when(encode.in(i)===1.U){
-        encode.out:=i.U(3.W)
-      }
+    val TimeOut = Input(Bool()) //为1暂停,为0开始
+    val Zero = Input(Bool())
+    val Hex1 =Output(UInt(7.W))
+    val Hex2 =Output(UInt(7.W))
+  })  
+  val counterMax50hz=24999999
+  val counterMax1s=100
+  val clkcount= RegInit(0.asUInt(log2Ceil(counterMax50hz).W))
+  val clk1scount= RegInit(0.asUInt(log2Ceil(counterMax1s).W))
+  val clk10scount= RegInit(0.asUInt(log2Ceil(10).W))
+  when(io.TimeOut===false.B){
+    clkcount := clkcount + 1.U
+    when(io.Zero===true.B){
+      clkcount := 0.U
+      clk1scount :=0.U
+      clk10scount :=0.U
     }
-  }otherwise{
-    encode.out := 0.U
+    when(clkcount===counterMax50hz.U){
+      clkcount := 0.U
+      clk1scount := clk1scount+1.U
+    }
+    when(clk1scount===counterMax1s.U){
+      clk1scount :=0.U
+    }
+    when(clk1scount===10.U){
+      clk1scount :=0.U
+      clk10scount :=clk10scount+1.U
+    }
+    when(clk10scount===10.U){
+      clk10scount :=0.U
+    }
   }
+  val seg1 = Module(new bcd7seg())
+  val seg2 = Module(new bcd7seg())
+  seg1.seg.in := clk1scount(3,0)
+  seg2.seg.in := clk10scount(3,0)
+  io.Hex1 := seg1.seg.out
+  io.Hex2 := seg2.seg.out
 }
 
 class bcd7seg extends Module{
   val seg = IO(new Bundle {
-    val in = Input(UInt(3.W))
+    val in = Input(UInt(4.W))
     val out= Output(UInt(7.W))
   })
   seg.out := MuxLookup(seg.in, 0.U)(Seq(
@@ -49,5 +53,6 @@ class bcd7seg extends Module{
     2.U -> "b0100100".U, 3.U -> "b0110000".U,
     4.U -> "b0011001".U, 5.U -> "b0010010".U,
     6.U -> "b0000010".U, 7.U -> "b1111000".U, 
+    8.U -> "b0000000".U, 9.U -> "b0010000".U
   ))
 }
