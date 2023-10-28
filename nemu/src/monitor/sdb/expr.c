aafb665b9bb48e630847454442bfaf818e68c6ce
wangxin <1721791534@qq.com>
2023-10-28 18:37:42 +0800

    before starting pa2

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -180,7 +180,12 @@ word_t eval(int p,int q) {
       case '+': return val1 + val2;
       case '-': return val1 - val2;
       case '*': return val1 * val2;
-      case '/': return val1 / val2;
+      case '/':
+        if(val2==0){
+          Log("Val2 is invalid(val=0)");
+          assert(0);
+        }
+        else return val1 / val2;
       case TK_NEG: return -1*val2;
       case TK_EQ:
         if(val1==val2) return 1;
