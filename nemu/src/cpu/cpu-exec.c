66e108d499b4b4ac1239cd609a126d480b42e2e0
tracer-ysyx <tracer@ysyx.org>
2023-11-03 22:13:39 +0800

    > compile NEMU ysyx_22040000 王植鑫 Linux ysyx 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:13:39 up  5:11,  1 user,  load average: 1.44, 1.08, 1.14

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -40,7 +40,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   char decodelog[128];
-  printf("%x",_this->pc);
+  // printf("0x%x\n",_this->pc);
   strcpy(decodelog,_this->logbuf);
   wp_trace(decodelog);
 }
