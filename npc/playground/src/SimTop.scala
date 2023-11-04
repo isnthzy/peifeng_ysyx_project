be0326cfbfd1fe686cf49cb5185129a157b9257c
tracer-ysyx <tracer@ysyx.org>
2023-11-04 09:04:32 +0800

    > sim RTL ysyx_22040000 王植鑫 Linux ysyx 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:04:32 up  7:13,  1 user,  load average: 0.43, 0.52, 0.77

--- npc/playground/src/SimTop.scala
+++ npc/playground/src/SimTop.scala
@@ -3,15 +3,51 @@ import chisel3.util._
 
 class SimTop extends Module {
   val io = IO(new Bundle {
-    val in = Input(UInt(4.W))
-    val sel = Input(UInt(2.W))
-    val out = Output(UInt(1.W))
+    val in = Input(UInt(8.W))
+    val en = Input(Bool())
+    val led_out = Output(UInt(3.W))
+    val hex_out = Output(UInt(7.W))
+    val led4 = Output(Bool())
   })
+  val encode = Module(new encode82())
+  val seg = Module(new bcd7seg())
+  encode.encode.in := io.in
+  encode.encode.en := io.en
+  io.led4 := Mux(io.in === 0.U, true.B, false.B)
 
-  io.out := MuxLookup(io.sel, 0.U)(Seq(
-    0.U -> io.in(0),
-    1.U -> io.in(1),
-    2.U -> io.in(2),
-    3.U -> io.in(3)
+  seg.seg.in := encode.encode.out
+  io.led_out := encode.encode.out
+  io.hex_out := seg.seg.out
+
+}
+
+class encode82 extends Module {
+  val encode = IO(new Bundle {
+    val in = Input(UInt(8.W))
+    val en = Input(Bool())
+    val out = Output(UInt(3.W))
+  })
+  when(encode.en){
+    encode.out:=0.U
+    for(i<-0 until 8){
+      when(encode.in(i)===1.U){
+        encode.out:=i.U(3.W)
+      }
+    }
+  }otherwise{
+    encode.out := 0.U
+  }
+}
+
+class bcd7seg extends Module{
+  val seg = IO(new Bundle {
+    val in = Input(UInt(3.W))
+    val out= Output(UInt(7.W))
+  })
+  seg.out := MuxLookup(seg.in, 0.U)(Seq(
+    0.U -> "b1000000".U, 1.U -> "b1111001".U, 
+    2.U -> "b0100100".U, 3.U -> "b0110000".U,
+    4.U -> "b0011001".U, 5.U -> "b0010010".U,
+    6.U -> "b0000010".U, 7.U -> "b1111000".U, 
   ))
 }
