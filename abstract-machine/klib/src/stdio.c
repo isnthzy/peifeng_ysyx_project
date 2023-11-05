267505963be16241ae7e430640601d68ff5286f6
tracer-ysyx <tracer@ysyx.org>
2023-11-05 16:38:46 +0800

    > compile NEMU ysyx_22040000 王植鑫 Linux ysyx 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:38:46 up 20:02,  1 user,  load average: 1.70, 1.45, 1.04

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,23 +6,54 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  return 0;
+  // panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  return 0;
+  // panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+int sprintf(char *out, const char *fmt, ...) { //fmt可以当个字符串处理
+  va_list ap;
+  va_start(ap,fmt);
+  int i=0;
+  char c,*s,*d;
+  char str[200];
+  for(i=0;fmt[i]!='\0';i++){
+    if(fmt[i]!='%'){
+      strcat(str,&fmt[i]);
+      continue;
+    }
+    switch (*fmt++){
+    case 's':
+      s=va_arg(ap,char *);
+      strcat(str,s);
+      break;
+    case 'd':
+      d=va_arg(ap,char *);
+      strcat(str,d);
+      break;
+    case 'c':
+      c=(char)va_arg(ap,int);
+      strcat(str,&c);
+      break;
+    }
+  }
+  va_end(ap);
+  return 0;
+  // panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  return 0;
+  // panic("Not implemented");
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  return 0;
+  // panic("Not implemented");
 }
 
 #endif
