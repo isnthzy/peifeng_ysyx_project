61fdabc5632ad81538271fb69bf532d148a6d3ec
tracer-ysyx <tracer@ysyx.org>
2023-11-08 21:00:19 +0800

    > compile NEMU ysyx_22040000 王植鑫 Linux ysyx 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:00:19 up 23:03,  1 user,  load average: 1.44, 1.33, 1.15

--- nemu/src/monitor/sdb/elf.c
+++ nemu/src/monitor/sdb/elf.c
@@ -33,21 +33,21 @@ void init_elf(const char *elf_file){
 void find_symtab_func(const char *pbuff){
     //从节区里面定位到偏移
     Elf64_Ehdr* pfilehead = (Elf64_Ehdr*)pbuff;
-    Elf64_Half eshstrndx = pfilehead->e_shstrndx;
+    // Elf64_Half eshstrndx = pfilehead->e_shstrndx;
     Elf64_Shdr* psecheader = (Elf64_Shdr*)(pbuff + pfilehead->e_shoff);
-    Elf64_Shdr* pshstr = (Elf64_Shdr*)(psecheader + eshstrndx);
-    char* pshstrbuff = (char *)(pbuff + pshstr->sh_offset);
+    // Elf64_Shdr* pshstr = (Elf64_Shdr*)(psecheader + eshstrndx);
+    // char* pshstrbuff = (char *)(pbuff + pshstr->sh_offset);
     for(int i = 0;i<pfilehead->e_shnum;++i)
     {
-        if(!strcmp(psecheader[i].sh_name + pshstrbuff, ".symtab"))
-        {
+        // if(!strcmp(psecheader[i].sh_name + pshstrbuff, ".symtab"))
+        // {
             Elf64_Sym* psym = (Elf64_Sym*)(pbuff + psecheader[i].sh_offset);
             int ncount = psecheader[i].sh_size / psecheader[i].sh_entsize;
             char* pbuffstr = (char*)((psecheader + psecheader[i].sh_link)->sh_offset + pbuff);
-            printf("Symbol table '%s' contains %d entries:\r\n", psecheader[i].sh_name + pshstrbuff, ncount);
+            // printf("Symbol table '%s' contains %d entries:\r\n", psecheader[i].sh_name + pshstrbuff, ncount);
             outputsyminfo(psym, pbuffstr, ncount);
             break;
-        }
+        // }
     }
 }
 void outputsyminfo(const Elf64_Sym *psym, const char *pbuffstr, int ncount)
