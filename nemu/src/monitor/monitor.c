c90c9a2f962c6caf8b09086bcc879e5c82a8ccd9
tracer-ysyx <tracer@ysyx.org>
2023-11-08 20:54:32 +0800

    > compile NEMU ysyx_22040000 王植鑫 Linux ysyx 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:54:32 up 22:57,  1 user,  load average: 1.40, 1.38, 1.09

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -18,6 +18,7 @@
 
 void init_rand();
 void init_log(const char *log_file);
+void init_elf(const char *elf_file);
 void init_mem();
 void init_difftest(char *ref_so_file, long img_size, int port);
 void init_device();
@@ -43,6 +44,7 @@ void sdb_set_batch_mode();
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
+static char *elf_file = NULL;
 static int difftest_port = 1234;
 
 static long load_img() {
@@ -74,15 +76,17 @@ static int parse_args(int argc, char *argv[]) {
     {"diff"     , required_argument, NULL, 'd'},
     {"port"     , required_argument, NULL, 'p'},
     {"help"     , no_argument      , NULL, 'h'},
+    {"ftrace"   , required_argument, NULL, 'f'},
     {0          , 0                , NULL,  0 },
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:e:", table, NULL)) != -1) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
+      case 'f': elf_file = optarg; break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
@@ -90,6 +94,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\t-f,--ftrace=ELF         use ${IMAGE}.elf to ftrance\n");
         printf("\n");
         exit(0);
     }
@@ -108,6 +113,9 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Open the log file. */
   init_log(log_file);
+
+  /* Open the ${IMAGE}.elf file */
+  init_elf(elf_file);
 
   /* Initialize memory. */
   init_mem();
---
+++ nemu/src/monitor/sdb/elf.c
@@ -0,0 +1,101 @@
+#include <common.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <malloc.h>
+#include <string.h>
+#include <elf.h>
+// 解析elf文件代码
+void outputsyminfo(const Elf64_Sym *psym, const char *pbuffstr, int ncount);
+void find_symtab_func(const char *pbuff);
+void init_elf(const char *elf_file){
+    int fd=open(elf_file, O_RDONLY);//以只读的形式打开elf_file
+    if(fd==-1){
+        Log("文件打开失败!\n");
+        assert(0);
+    }
+    long int end=lseek(fd, 0, SEEK_END); //把指针移动到末尾位置,生成相应大小
+    lseek(fd, 0, SEEK_SET); //把指针复原移动到头位置
+    char* pbuff=malloc(end); //分配空间,从头开始
+    if(!pbuff){
+        Log("文件分配内存失败\n");
+        assert(0);
+    }
+     //初始化0
+    memset(pbuff, 0, end);
+    if(read(fd, pbuff, end)==-1){
+        Log("文件读取失败");
+        assert(0);
+    }
+    find_symtab_func(pbuff);
+}
+void find_symtab_func(const char *pbuff){
+    //从节区里面定位到偏移
+    Elf64_Ehdr* pfilehead = (Elf64_Ehdr*)pbuff;
+    Elf64_Half eshstrndx = pfilehead->e_shstrndx;
+    Elf64_Shdr* psecheader = (Elf64_Shdr*)(pbuff + pfilehead->e_shoff);
+    Elf64_Shdr* pshstr = (Elf64_Shdr*)(psecheader + eshstrndx);
+    char* pshstrbuff = (char *)(pbuff + pshstr->sh_offset);
+    for(int i = 0;i<pfilehead->e_shnum;++i)
+    {
+        if(!strcmp(psecheader[i].sh_name + pshstrbuff, ".symtab"))
+        {
+            Elf64_Sym* psym = (Elf64_Sym*)(pbuff + psecheader[i].sh_offset);
+            int ncount = psecheader[i].sh_size / psecheader[i].sh_entsize;
+            char* pbuffstr = (char*)((psecheader + psecheader[i].sh_link)->sh_offset + pbuff);
+            printf("Symbol table '%s' contains %d entries:\r\n", psecheader[i].sh_name + pshstrbuff, ncount);
+            outputsyminfo(psym, pbuffstr, ncount);
+            break;
+        }
+    }
+}
+void outputsyminfo(const Elf64_Sym *psym, const char *pbuffstr, int ncount)
+{
+    printf("%7s  %-8s          %s  %s    %s   %s      %s  %s\r\n",
+           "Num:", "Value", "Size", "Type", "Bind", "Vis", "Ndx", "Name");
+    for(int i = 0;i<ncount;++i)
+    {
+        printf("%6d:  %016lx  %-6lu", i, psym[i].st_value, psym[i].st_size);
+        char typelow = ELF32_ST_TYPE(psym[i].st_info);
+        char bindhig = ELF32_ST_BIND(psym[i].st_info);
+        switch(typelow)
+        {
+            case STT_NOTYPE:
+                printf("%-8s", "NOTYPE");break;
+            case STT_OBJECT:
+                printf("%-8s", "OBJECT");break;
+            case STT_FUNC:
+                printf("%-8s", "FUNC");break;
+            case STT_SECTION:
+                printf("%-8s", "SECTION");break;
+            case STT_FILE:
+                printf("%-8s", "FILE");break;
+            default:
+                break;
+        }
+        switch(bindhig)
+        {
+            case STB_LOCAL:
+                printf("%-8s", "LOCAL"); break;
+            case STB_GLOBAL:
+                printf("%-8s", "GLOBAL"); break;
+            case STB_WEAK:
+                printf("%-8s", "WEAK"); break;
+            default:
+                break;
+        }
+        printf("%-8d", psym[i].st_other);
+        switch(psym[i].st_shndx)
+        {
+            case SHN_UNDEF:
+                printf("%s  %s\r\n", "UND", psym[i].st_name + pbuffstr);break;
+            case SHN_ABS:
+                printf("%s  %s\r\n", "ABS", psym[i].st_name + pbuffstr);break;
+            case SHN_COMMON:
+                printf("%s  %s\r\n", "COM", psym[i].st_name + pbuffstr);break;
+            default:
+                printf("%3d  %s\r\n", psym[i].st_shndx, psym[i].st_name + pbuffstr);break;
+        }
+    }
+}
