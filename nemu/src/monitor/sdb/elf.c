ddbcadfebc24e7da74ae94c0980a70c03700195c
tracer-ysyx <tracer@ysyx.org>
2023-11-09 20:10:02 +0800

    > compile NEMU ysyx_22040000 王植鑫 Linux ysyx 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:10:02 up 1 day,  3:49,  1 user,  load average: 1.39, 1.42, 1.05

--- nemu/src/monitor/sdb/elf.c
+++ nemu/src/monitor/sdb/elf.c
@@ -8,12 +8,12 @@ void init_elf(const char *elf_file){
         Log("文件打开失败!\n");
         assert(0);
     }
-    size_t resultt;
+    size_t result;
     
     // 读取 ELF 文件的头部信息
     Elf_Ehdr elf_header;
-    resultt=fread(&elf_header, sizeof(Elf_Ehdr), 1, file);
-    if (resultt!= 0) puts("");
+    result=fread(&elf_header, sizeof(Elf_Ehdr), 1, file);
+    if (result== 0) assert(0);
     // 获取节头表的偏移量和条目数量
     Elf_Off section_header_offset = elf_header.e_shoff;
     Elf_Half section_header_entry_count = elf_header.e_shnum;
@@ -21,13 +21,13 @@ void init_elf(const char *elf_file){
     fseek(file, section_header_offset, SEEK_SET);
     // 读取节头表
     Elf_Shdr section_headers[section_header_entry_count];
-    resultt=fread(section_headers, sizeof(Elf64_Shdr), section_header_entry_count, file);
+    result=fread(section_headers, sizeof(Elf64_Shdr), section_header_entry_count, file);
     // 定位到字符串表节
     Elf_Shdr string_table_header = section_headers[elf_header.e_shstrndx];
     fseek(file, string_table_header.sh_offset, SEEK_SET);
     // 读取字符串表内容
     char string_table[string_table_header.sh_size];
-    resultt=fread(string_table, string_table_header.sh_size, 1, file);
+    result=fread(string_table, string_table_header.sh_size, 1, file);
     // 查找符号表节和字符串表节
     Elf_Shdr symtab_header;
     for (int i = 0; i < section_header_entry_count; ++i) {
@@ -42,7 +42,7 @@ void init_elf(const char *elf_file){
     size_t symbol_count = symtab_header.sh_size / symtab_header.sh_entsize;
     // 读取符号表
     Elf_Sym symbols[symbol_count];
-    resultt=fread(symbols, sizeof(Elf64_Sym), symbol_count, file);
+    result=fread(symbols, sizeof(Elf64_Sym), symbol_count, file);
     // 遍历符号表，筛选出类型为FUNC的符号
     for (size_t i = 0; i < symbol_count; ++i) {
         if (ELF32_ST_TYPE(symbols[i].st_info) == STT_FUNC) {
@@ -50,7 +50,7 @@ void init_elf(const char *elf_file){
             char* symbol_name = string_table + symbols[i].st_name;
             // 获取符号的地址
             Elf64_Addr symbol_address = symbols[i].st_value;
-            printf("Function: %s\nAddress: 0x%lx\n\n", symbol_name, symbol_address);
+            printf("Function: %s Address: 0x%lx\n", symbol_name, symbol_address);
         }
     }
     fclose(file);
