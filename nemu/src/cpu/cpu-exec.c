83e8dfce317c5edf989132d36354dd49e232be80
tracer-ysyx <tracer@ysyx.org>
2023-11-09 21:19:06 +0800

    > compile NEMU ysyx_22040000 王植鑫 Linux ysyx 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:19:06 up 1 day,  4:58,  1 user,  load average: 1.59, 1.29, 1.02

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -112,7 +112,8 @@ void putIringbuf(){
   while(!isIRingBufferEmpty(&iring_buffer)){
     char pop_iringbufdata[100];
     dequeueIRingBuffer(&iring_buffer,pop_iringbufdata);
-    Log("%s",pop_iringbufdata);
+    if(iring_buffer.size==1) Log("-->%s",pop_iringbufdata);
+    else Log("   %s",pop_iringbufdata);
   }
 }
 
