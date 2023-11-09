a963ca50b0dc45fbb6f59fe654aadf4df0c831a9
tracer-ysyx <tracer@ysyx.org>
2023-11-09 20:07:40 +0800

    > compile NEMU ysyx_22040000 王植鑫 Linux ysyx 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:07:40 up 1 day,  3:46,  1 user,  load average: 1.60, 1.50, 1.01

--- nemu/src/monitor/sdb/elf.c
+++ nemu/src/monitor/sdb/elf.c
@@ -1,102 +1,58 @@
 #include <common.h>
 #include <stdio.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <malloc.h>
-#include <string.h>
-#include <elf.h>
 // 解析elf文件代码
-void outputsyminfo(const Elf32_Sym *psym, const char *pbuffstr, int ncount);
-void find_symtab_func(const char *pbuff);
+
 void init_elf(const char *elf_file){
-    int fd=open(elf_file, O_RDONLY);//以只读的形式打开elf_file
-    if(fd==-1){
+    FILE* file = fopen(elf_file, "rb");//以只读的形式打开elf_file
+    if(!file){
         Log("文件打开失败!\n");
         assert(0);
     }
-    long int end=lseek(fd, 0, SEEK_END); //把指针移动到末尾位置,生成相应大小
-    lseek(fd, 0, SEEK_SET); //把指针复原移动到头位置
-    char* pbuff=malloc(end); //分配空间,从头开始
-    if(!pbuff){
-        Log("文件分配内存失败\n");
-        assert(0);
-    }
-     //初始化0
-    memset(pbuff, 0, end);
-    if(read(fd, pbuff, end)==-1){
-        Log("文件读取失败");
-        assert(0);
-    }
-    find_symtab_func(pbuff);
-}
-void find_symtab_func(const char *pbuff){
-    //从节区里面定位到偏移
-    Elf32_Ehdr* pfilehead = (Elf32_Ehdr*)pbuff;
-    Elf32_Half eshstrndx = pfilehead->e_shstrndx;
-    // printf("%d",eshstrndx);
-    Elf32_Shdr* psecheader = (Elf32_Shdr*)(pbuff + pfilehead->e_shoff);
-    Elf32_Shdr* pshstr = (Elf32_Shdr*)(psecheader + eshstrndx);
-    char* pshstrbuff = (char *)(pbuff + pshstr->sh_offset);
-    for(int i = 0;i<pfilehead->e_shnum;++i)
-    {
-        if(!strcmp(psecheader[i].sh_name + pshstrbuff, ".symtab"))
-        {
-            Elf32_Sym* psym = (Elf32_Sym*)(pbuff + psecheader[i].sh_offset);
-            int ncount = psecheader[i].sh_size / psecheader[i].sh_entsize;
-            char* pbuffstr = (char*)((psecheader + psecheader[i].sh_link)->sh_offset + pbuff);
-            printf("Symbol table '%s' contains %d entries:\r\n", psecheader[i].sh_name + pshstrbuff, ncount);
-            outputsyminfo(psym, pbuffstr, ncount);
+    size_t resultt;
+    
+    // 读取 ELF 文件的头部信息
+    Elf_Ehdr elf_header;
+    resultt=fread(&elf_header, sizeof(Elf_Ehdr), 1, file);
+    if (resultt!= 0) puts("");
+    // 获取节头表的偏移量和条目数量
+    Elf_Off section_header_offset = elf_header.e_shoff;
+    Elf_Half section_header_entry_count = elf_header.e_shnum;
+    // 定位到节头表
+    fseek(file, section_header_offset, SEEK_SET);
+    // 读取节头表
+    Elf_Shdr section_headers[section_header_entry_count];
+    resultt=fread(section_headers, sizeof(Elf64_Shdr), section_header_entry_count, file);
+    // 定位到字符串表节
+    Elf_Shdr string_table_header = section_headers[elf_header.e_shstrndx];
+    fseek(file, string_table_header.sh_offset, SEEK_SET);
+    // 读取字符串表内容
+    char string_table[string_table_header.sh_size];
+    resultt=fread(string_table, string_table_header.sh_size, 1, file);
+    // 查找符号表节和字符串表节
+    Elf_Shdr symtab_header;
+    for (int i = 0; i < section_header_entry_count; ++i) {
+        if (section_headers[i].sh_type == SHT_SYMTAB) {
+            symtab_header = section_headers[i];
             break;
         }
     }
-}
-void outputsyminfo(const Elf32_Sym *psym, const char *pbuffstr, int ncount)
-{
-    printf("%7s  %-8s          %s  %s    %s   %s      %s  %s\r\n",
-           "Num:", "Value", "Size", "Type", "Bind", "Vis", "Ndx", "Name");
-    for(int i = 0;i<ncount;++i)
-    {
-        printf("%6d:  %016x  %-6u", i, psym[i].st_value, psym[i].st_size);
-        char typelow = ELF32_ST_TYPE(psym[i].st_info);
-        char bindhig = ELF32_ST_BIND(psym[i].st_info);
-        switch(typelow)
-        {
-            case STT_NOTYPE:
-                printf("%-8s", "NOTYPE");break;
-            case STT_OBJECT:
-                printf("%-8s", "OBJECT");break;
-            case STT_FUNC:
-                printf("%-8s", "FUNC");break;
-            case STT_SECTION:
-                printf("%-8s", "SECTION");break;
-            case STT_FILE:
-                printf("%-8s", "FILE");break;
-            default:
-                break;
-        }
-        switch(bindhig)
-        {
-            case STB_LOCAL:
-                printf("%-8s", "LOCAL"); break;
-            case STB_GLOBAL:
-                printf("%-8s", "GLOBAL"); break;
-            case STB_WEAK:
-                printf("%-8s", "WEAK"); break;
-            default:
-                break;
-        }
-        printf("%-8d", psym[i].st_other);
-        switch(psym[i].st_shndx)
-        {
-            case SHN_UNDEF:
-                printf("%s  %s\r\n", "UND", psym[i].st_name + pbuffstr);break;
-            case SHN_ABS:
-                printf("%s  %s\r\n", "ABS", psym[i].st_name + pbuffstr);break;
-            case SHN_COMMON:
-                printf("%s  %s\r\n", "COM", psym[i].st_name + pbuffstr);break;
-            default:
-                printf("%3d  %s\r\n", psym[i].st_shndx, psym[i].st_name + pbuffstr);break;
+    // 定位到符号表节
+    fseek(file, symtab_header.sh_offset, SEEK_SET);
+    // 计算符号表中的符号数量
+    size_t symbol_count = symtab_header.sh_size / symtab_header.sh_entsize;
+    // 读取符号表
+    Elf_Sym symbols[symbol_count];
+    resultt=fread(symbols, sizeof(Elf64_Sym), symbol_count, file);
+    // 遍历符号表，筛选出类型为FUNC的符号
+    for (size_t i = 0; i < symbol_count; ++i) {
+        if (ELF32_ST_TYPE(symbols[i].st_info) == STT_FUNC) {
+            // 获取符号的名称
+            char* symbol_name = string_table + symbols[i].st_name;
+            // 获取符号的地址
+            Elf64_Addr symbol_address = symbols[i].st_value;
+            printf("Function: %s\nAddress: 0x%lx\n\n", symbol_name, symbol_address);
         }
     }
+    fclose(file);
 }
+
