cbf875fba35854095c9b1e745cd25ee282a2c6de
tracer-ysyx <tracer@ysyx.org>
2023-11-10 20:19:06 +0800

    > compile NEMU ysyx_22040000 王植鑫 Linux ysyx 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:19:06 up  4:20,  1 user,  load average: 1.58, 1.18, 0.88

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -30,26 +30,29 @@ int main(int argc, char *argv[]) {
 #else
   init_monitor(argc, argv);
 #endif
-  // char ea[10240];
-  // char *val;
-  // char *evall;
+  char ea[10240];
+  char *val;
+  char *evall;
   /* Start engine. */
-  // FILE *fp=fopen("/home/wangxin/ysyx-workbench/nemu/tools/gen-expr/input", "r");
-  // if (fp == NULL) {
-  //       printf("无法打开文件。\n");
-  //       return 1;
-  // }
-  // int cnt=1;
-  // while(fgets(ea,10240,fp)){
-  //   val = strtok(ea, " ");
-  //   evall = strtok(ea, " ");
-  //   bool flag=true;
-  //   word_t value_p = expr(evall,&flag);
-  //   word_t u32;
-  //   u32 = strtoul(val, NULL, 10);
-  //   if(u32==value_p) printf("%d true\n",cnt++);
-  //   else assert(0);
-  // } //此段代码为表达式求值检测正确性代码,可删
+  FILE *fp=fopen("/home/wangxin/ysyx-workbench/nemu/tools/gen-expr/input", "r");
+  if (fp == NULL) {
+        printf("无法打开文件。\n");
+        return 1;
+  }
+  int cnt=1;
+  int i=20;
+  while(fgets(ea,10240,fp)){
+    if(i==0) assert(0);
+    val = strtok(ea, " ");
+    evall = strtok(ea, " ");
+    bool flag=true;
+    word_t value_p = expr(evall,&flag);
+    word_t u32;
+    u32 = strtoul(val, NULL, 10);
+    printf("%d %d\n",u32,value_p);
+    if(u32==value_p) printf("%d true\n",cnt++);
+    else assert(0);
+  } //此段代码为表达式求值检测正确性代码,可删
   engine_start();
 
   return is_exit_status_bad();
