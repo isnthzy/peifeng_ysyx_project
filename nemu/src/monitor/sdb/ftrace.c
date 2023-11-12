1489c59a10b36765b289ffee8d53a7da278fce3b
tracer-ysyx <tracer@ysyx.org>
2023-11-12 19:01:54 +0800

    > compile NEMU ysyx_22040000 王植鑫 Linux ysyx 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:01:54 up 29 min,  1 user,  load average: 0.65, 0.48, 0.46

--- nemu/src/monitor/sdb/ftrace.c
+++ nemu/src/monitor/sdb/ftrace.c
@@ -85,12 +85,12 @@ void func_call(paddr_t pc,paddr_t dnpc){
     func_depth++;
     strcpy(put_name,find_funcname(dnpc));
     generateSpaces(func_depth,n_spaces);
-    printf("0x%x:%s call[%s@0x%x]",pc,n_spaces,put_name,dnpc);
+    printf("0x%x:%s call[%s@0x%x]\n",pc,n_spaces,put_name,dnpc);
 }
 void func_ret(paddr_t pc,paddr_t dnpc){
     strcpy(put_name,find_funcname(dnpc));
     generateSpaces(func_depth,n_spaces);
-    printf(" 0x%x:%s ret [%s@0x%x]",pc,n_spaces,put_name,dnpc);
+    printf(" 0x%x:%s ret [%s@0x%x]\n",pc,n_spaces,put_name,dnpc);
     func_depth--;
 }
 
