ea13aaf7b3db2abd5e8e4356d453741b7e699641
tracer-ysyx <tracer@ysyx.org>
2023-11-17 21:26:53 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux ysyx 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:26:53 up  5:01,  1 user,  load average: 3.28, 2.25, 1.69

--- npc/playground/src/Alu.scala
+++ npc/playground/src/Alu.scala
@@ -4,9 +4,9 @@ import chisel3.util._
 class Alu extends Module {
   val io = IO(new Bundle {
     val op = Input(UInt(12.W))
-    val src1 = Input(SInt(32.W))
-    val src2 = Input(SInt(32.W))
-    val result = Output(SInt(32.W))
+    val src1 = Input(UInt(32.W))
+    val src2 = Input(UInt(32.W))
+    val result = Output(UInt(32.W))
   })
   val op_add= io.src1 + io.src2
   val op_sub= io.src1 - io.src2
@@ -14,11 +14,11 @@ class Alu extends Module {
   val op_and= io.src1 & io.src2
   val op_or = io.src1 | io.src2
   val op_xor= io.src1 ^ io.src2
-  val op_tha= Mux(io.src1  <  io.src2, 1.S, 0.S)
-  val op_eq = Mux(io.src1 === io.src2, 1.S, 0.S)
-  io.result := MuxLookup(io.op, 0.S)(Seq(
-    "b000000000001".U -> op_add.asSInt, 
-    "b000000000010".U -> op_sub.asSInt,
+  val op_tha= Mux(io.src1  <  io.src2, 1.U, 0.U)
+  val op_eq = Mux(io.src1 === io.src2, 1.U, 0.U)
+  io.result := MuxLookup(io.op, 0.U)(Seq(
+    "b000000000001".U -> op_add, 
+    "b000000000010".U -> op_sub,
     "b000000000100".U -> op_neg,
     "b000000001000".U -> op_and,
     "b000000010000".U -> op_or,
