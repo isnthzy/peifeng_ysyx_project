6f87049e3d1671afc728dc500fe653549dd396fe
tracer-ysyx <tracer@ysyx.org>
2023-11-18 19:16:31 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux ysyx 6.2.0-36-generic #37~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  9 15:34:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:16:31 up 1 day,  1:05,  1 user,  load average: 1.15, 1.06, 1.07

--- npc/playground/src/TopMain.scala
+++ npc/playground/src/TopMain.scala
@@ -2,6 +2,20 @@ import circt.stage._
 
 object TopMain extends App {
   def top = new SimTop()
-  val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
-  (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
+  val ChiselStageOptions=Seq(
+    chisel3.stage.ChiselGeneratorAnnotation(() => top),
+    CIRCTTargetAnnotation(CIRCTTarget.Verilog)
+  )
+  // (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
+
+  val firtoolOptions=Seq(
+    FirtoolOption(
+      "--lowering-options=disallowLocalVariables,disallowPackedArrays,locationInfoStyle=wrapInAtSquareBracket"
+    ),
+    FirtoolOption("--split-verilog"),
+    FirtoolOption("-o-build/sv-gen"),
+    FirtoolOption("--disable-all-randomization")
+  )
+  val executeOptions=ChiselStageOptions++firtoolOptions
+  (new ChiselStage).execute(args,executeOptions)
 }
