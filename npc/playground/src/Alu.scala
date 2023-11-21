51bb3b2ec334b00832e5d4adc9a10ab5979f503f
tracer-ysyx <tracer@ysyx.org>
2023-11-22 07:50:36 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  07:50:36 up 23:02,  1 user,  load average: 0.40, 0.29, 0.25

--- npc/playground/src/Alu.scala
+++ npc/playground/src/Alu.scala
@@ -6,24 +6,49 @@ class Alu extends Module {
     val op = Input(UInt(12.W))
     val src1 = Input(UInt(32.W))
     val src2 = Input(UInt(32.W))
+    val sign = Input(Bool()) //1为有符号数，0为无符号数
     val result = Output(UInt(32.W))
   })
-  val op_add= io.src1 + io.src2
-  val op_sub= io.src1 - io.src2
-  val op_neg= ~io.src1
-  val op_and= io.src1 & io.src2
-  val op_or = io.src1 | io.src2
-  val op_xor= io.src1 ^ io.src2
-  val op_tha= Mux(io.src1  <  io.src2, 1.U, 0.U)
-  val op_eq = Mux(io.src1 === io.src2, 1.U, 0.U)
+  val alu_sadd= io.src1.asSInt + io.src2.asSInt
+  val alu_ssub= io.src1.asSInt - io.src2.asSInt
+  val alu_uadd= io.src1 + io.src2
+  val alu_usub= io.src1 - io.src2 
+  val alu_utha= Mux(io.src1 < io.src2,1.U,0.U)
+  val alu_stha= Mux(io.src1.asSInt < io.src2.asSInt,1.U,0.U)
+
+  val alu_add = Mux(io.sign,alu_sadd.asUInt,alu_uadd)
+
+  val alu_sub = Mux(io.sign,alu_ssub.asUInt,alu_usub)
+
+  val alu_neg = ~io.src1
+
+  val alu_and = io.src1 & io.src2
+
+  val alu_or  = io.src1 | io.src2
+
+  val alu_xor = io.src1 ^ io.src2
+
+  val alu_eq  = Mux(io.src1===io.src2,1.U,0.U)
+
+  val alu_tha = Mux(io.sign,alu_stha.asUInt,alu_utha)
+
+  val alu_sl  = io.src1 << io.src2 //左移
+
+  val alu_sra = io.src1.asSInt >> io.src2
+
+  val alu_srl = io.src1 >> io.src2
+
   io.result := MuxLookup(io.op, 0.U)(Seq(
-    "b000000000001".U -> op_add, 
-    "b000000000010".U -> op_sub,
-    "b000000000100".U -> op_neg,
-    "b000000001000".U -> op_and,
-    "b000000010000".U -> op_or,
-    "b000000100000".U -> op_xor,
-    "b000001000000".U -> op_tha, 
-    "b000010000000".U -> op_eq,
+    "b000000000001".U -> alu_add, 
+    "b000000000010".U -> alu_sub,
+    "b000000000100".U -> alu_neg,
+    "b000000001000".U -> alu_and,
+    "b000000010000".U -> alu_or,
+    "b000000100000".U -> alu_xor,
+    "b000001000000".U -> alu_eq,
+    "b000010000000".U -> alu_tha,
+    "b000100000000".U -> alu_sl, 
+    "b001000000000".U -> alu_sra.asUInt,
+    "b010000000000".U -> alu_srl,
   ))
 }
