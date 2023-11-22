3963cf82b1d06ae55200c5189b5385bef982799c
tracer-ysyx <tracer@ysyx.org>
2023-11-22 12:27:13 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:27:13 up 1 day,  3:39,  1 user,  load average: 0.08, 0.16, 0.17

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -4,20 +4,41 @@
 #include "verilated_vcd_c.h"
 #include "svdpi.h"
 #include "VSimTop__Dpi.h"
+#define CONFIG_MSIZE 0x8000000
+#define START_ADDR   0x80000000
+#define CONFIG_MBASE 0x80000000
+#define PG_ALIGN __attribute((aligned(4096)))
 typedef unsigned int   uint32_t;
-#define START_ADDR 0x80000000
-uint32_t pmem[0x8000000]= {
-  0xC0010093,
-  0x40008213,
-  0x40008213,
-  0x40008213,
-  0x40008213,
-  0x40008213,
-  0x40008213,
-  0x40008213,
-  0x40008213,
-  0x00100073
-};
+typedef unsigned char  uint8_t;  
+typedef unsigned short int  uint16_t; 
+static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
+static inline uint32_t host_read(void *addr, int len) {
+  switch (len) {
+    case 1: return *(uint8_t  *)addr;
+    case 2: return *(uint16_t *)addr;
+    case 4: return *(uint32_t *)addr;
+    default:  return 0;
+  }
+}
+static inline void host_write(void *addr, int len, uint32_t data) {
+  switch (len) {
+    case 1: *(uint8_t  *)addr = data; return;
+    case 2: *(uint16_t *)addr = data; return;
+    case 4: *(uint32_t *)addr = data; return;
+    default: return;
+  }
+}
+uint8_t* guest_to_host(uint32_t paddr) { return pmem + paddr - CONFIG_MBASE; }
+uint32_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
+static uint32_t pmem_read(uint32_t addr, int len) {
+  uint32_t ret = host_read(guest_to_host(addr), len);
+  return ret;
+}
+
+static void pmem_write(uint32_t addr, int len, uint32_t data) {
+  host_write(guest_to_host(addr), len, data);
+}
+
 
 VerilatedContext* contextp = NULL;
 VerilatedVcdC* tfp = NULL;
@@ -25,13 +46,23 @@ static VSimTop* top;
 // void nvboard_bind_all_pins(VSimTop* top);
 // static TOP_NAME dut;
 
+static long load_img() {
+  if (img_file == NULL) {
+    printf("No image is given. Use the default build-in image.");
+    return 4096; // built-in image size
+  }
 
-// void* guest_to_host(uint32_t paddr) {
-//   return pmem + paddr - START_ADDR;
-// }
-// static inline uint32_t host_read(void* addr) {
-//   return *(uint32_t*)addr;
-// }
+  FILE *fp = fopen(img_file, "rb");
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+  printf("The image is %s, size = %ld", img_file, size);
+  fseek(fp, 0, SEEK_SET);
+  int ret = fread(guest_to_host(START_ADDR), size, 1, fp);
+  assert(ret == 1);
+  fclose(fp);
+  return size;
+}
+
 uint32_t pmem_read(uint32_t addr) {
   uint32_t ret =pmem[(addr-0x80000000)/4];
   printf("%x %x\n",addr,ret);
@@ -89,6 +120,7 @@ int main() {
   //   single_cycle();
   // }
   // int i=2;
+  load_img();
   sim_init();
   reset(10);
   while(sim_end){
@@ -96,7 +128,7 @@ int main() {
     step_and_dump_wave();
 
     top->clock=1;
-    top->io_inst=pmem_read(top->io_pc);
+    top->io_inst=pmem_read(top->io_pc,4);
     step_and_dump_wave();
   }
   sim_exit();
