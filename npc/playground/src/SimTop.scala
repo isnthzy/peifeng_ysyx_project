839e895bd33c5543a6ac92066275ac958939d4ae
tracer-ysyx <tracer@ysyx.org>
2023-11-25 12:17:59 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:17:59 up  2:46,  1 user,  load average: 1.24, 0.38, 0.19

--- npc/playground/src/SimTop.scala
+++ npc/playground/src/SimTop.scala
@@ -180,8 +180,8 @@ class SimTop extends Module {
   RegFile.io.wdata:=io.result
 
   val singal_dpi=Module(new singal_dpi())
-  singal_dpi.io.clock:=reset
   singal_dpi.io.clock:=clock
+  singal_dpi.io.clock:=reset
   singal_dpi.io.pc:=io.pc
   singal_dpi.io.ebreak_flag:=IsaI.ebreak
   singal_dpi.io.inv_flag   :=Inst_inv
