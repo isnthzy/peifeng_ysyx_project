c22d79f9592138b473b9a5781165d1827fdc60da
tracer-ysyx <tracer@ysyx.org>
2023-11-25 11:29:24 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:29:24 up  1:58,  1 user,  load average: 0.32, 0.19, 0.12

--- npc/playground/src/v_resource/dpi.sv
+++ npc/playground/src/v_resource/dpi.sv
@@ -1,6 +1,6 @@
 import "DPI-C" function void sim_break(input int pc,input int ret_reg);
 import "DPI-C" function void inv_break(input int pc);
-module singal_ebreak(
+module singal_dpi(
     input        clock,
     input [31:0] pc,
     input        ebreak_flag,
