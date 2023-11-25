c952b633f68b7e967ef9e17da6912a9ff4dd1886
tracer-ysyx <tracer@ysyx.org>
2023-11-25 15:27:27 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:27:27 up  5:56,  1 user,  load average: 0.72, 0.38, 0.25

--- npc/playground/src/SimTop.scala
+++ npc/playground/src/SimTop.scala
@@ -26,7 +26,8 @@ class SimTop extends Module {
 
 // IFU begin
   val pc=RegInit(START_ADDR)
-  dnpc:=Mux(IsaI.jalr,(pc+Imm)& ~1.U,pc+Imm) //下一条动态指令
+  dnpc:=Mux(IsaI.jalr,(pc+Imm)& ~1.U,
+          Mux(IsaU.jal,pc+Imm,snpc)) //下一条动态指令
   snpc:=pc+4.U //下一条静态指令
   when(is_jump){
     pc := dnpc
