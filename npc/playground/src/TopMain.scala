4dbd687cf80691e70d864c0a24db26df8d2deaf7
tracer-ysyx <tracer@ysyx.org>
2023-12-01 17:08:23 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:08:23 up  1:51,  1 user,  load average: 1.47, 0.38, 0.14

--- npc/playground/src/TopMain.scala
+++ npc/playground/src/TopMain.scala
@@ -4,7 +4,7 @@ object TopMain extends App {
   def top = new SimTop()
   val ChiselStageOptions=Seq(
     chisel3.stage.ChiselGeneratorAnnotation(() => top),
-    CIRCTTargetAnnotation(CIRCTTarget.SystemVerilog)
+    CIRCTTargetAnnotation(CIRCTTarget.Verilog)
   )
   // (new ChiselStage).execute(args, generator :+ CIRCTTargetAnnotation(CIRCTTarget.Verilog))
 
@@ -12,8 +12,8 @@ object TopMain extends App {
     FirtoolOption(
       "--lowering-options=disallowLocalVariables,disallowPackedArrays,locationInfoStyle=wrapInAtSquareBracket"
     ), //香山firtool参数
-    FirtoolOption("--split-verilog"), //分离生成的verilog文件
-    FirtoolOption("-o=vsrc"), //设置分离后verilog文件的路径
+    // FirtoolOption("--split-verilog"), //分离生成的verilog文件
+    // FirtoolOption("-o=vsrc"), //设置分离后verilog文件的路径
     FirtoolOption("--disable-all-randomization") //禁止随机数
   )
   val executeOptions=ChiselStageOptions++firtoolOptions
