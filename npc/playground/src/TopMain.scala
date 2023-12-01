d8853fa07d9261ac21a64b8d6a7ecb02a5c016cf
tracer-ysyx <tracer@ysyx.org>
2023-12-01 17:09:35 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:09:35 up  1:52,  1 user,  load average: 0.67, 0.39, 0.17

--- npc/playground/src/TopMain.scala
+++ npc/playground/src/TopMain.scala
@@ -13,7 +13,7 @@ object TopMain extends App {
       "--lowering-options=disallowLocalVariables,disallowPackedArrays,locationInfoStyle=wrapInAtSquareBracket"
     ), //香山firtool参数
     // FirtoolOption("--split-verilog"), //分离生成的verilog文件
-    FirtoolOption("-o=vsrc"), //设置分离后verilog文件的路径
+    // FirtoolOption("-o=vsrc"), //设置分离后verilog文件的路径
     FirtoolOption("--disable-all-randomization") //禁止随机数
   )
   val executeOptions=ChiselStageOptions++firtoolOptions
