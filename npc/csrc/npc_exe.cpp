e4365a5b80c6e71863f2e8bcf7db3d3a4afb53b8
tracer-ysyx <tracer@ysyx.org>
2023-12-01 16:35:15 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:35:15 up  1:17,  1 user,  load average: 0.15, 0.06, 0.04

--- npc/csrc/npc_exe.cpp
+++ npc/csrc/npc_exe.cpp
@@ -28,7 +28,7 @@ void assert_fail_msg() {
 static void npc_execute(uint64_t n) {
   for (;n > 0; n --) {
     top->clock=1;
-    top->io_inst=pmem_read(top->io_pc,4);
+    // top->io_inst=pmem_read(top->io_pc,4);
     step_and_dump_wave();
 
     top->clock=0;
