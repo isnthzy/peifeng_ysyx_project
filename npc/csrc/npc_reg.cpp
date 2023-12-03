d7f0637bfad8500c5c0b8c37f2fdae104c52ad66
tracer-ysyx <tracer@ysyx.org>
2023-12-03 12:39:47 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:39:47 up  1:01,  1 user,  load average: 0.43, 0.19, 0.12

--- npc/csrc/npc_reg.cpp
+++ npc/csrc/npc_reg.cpp
@@ -2,10 +2,42 @@
 #include "include/npc_verilator.h"
 
 uint32_t read_reg_idx(int idx) {
-  // uint32_t* npc_reg_value=(uint32_t*)(top->rootp->SimTop__DOT__RegFile__DOT__rf_0+idx*sizeof(uint32_t));
-  uint32_t npc_reg_value=top->rootp->SimTop__DOT__RegFile__DOT__rf_0+idx*sizeof(uint32_t);
-  return npc_reg_value;
+  uint32_t rf_tmp;
+  if(idx==0) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_0;
+  else if(idx==1)  rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_1;
+  else if(idx==2)  rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_2;
+  else if(idx==3)  rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_3;
+  else if(idx==4)  rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_4;
+  else if(idx==5)  rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_5;
+  else if(idx==6)  rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_6;
+  else if(idx==7)  rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_7;
+  else if(idx==8)  rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_8;
+  else if(idx==9)  rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_9;
+  else if(idx==10) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_10;
+  else if(idx==11) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_11;
+  else if(idx==12) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_12;
+  else if(idx==13) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_13;
+  else if(idx==14) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_14;
+  else if(idx==15) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_15;
+  else if(idx==16) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_16;
+  else if(idx==17) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_17;
+  else if(idx==18) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_18;
+  else if(idx==19) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_19;
+  else if(idx==20) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_20;
+  else if(idx==21) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_21;
+  else if(idx==22) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_22;
+  else if(idx==23) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_23;
+  else if(idx==24) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_24;
+  else if(idx==25) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_25;
+  else if(idx==26) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_26;
+  else if(idx==27) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_27;
+  else if(idx==28) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_28;
+  else if(idx==29) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_29;
+  else if(idx==30) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_30;
+  else if(idx==31) rf_tmp=top->rootp->SimTop__DOT__RegFile__DOT__rf_31;
+  return rf_tmp;
 }
+
 
 static inline int check_reg_idx(int idx) {
   assert(idx >= 0 && idx <32);
