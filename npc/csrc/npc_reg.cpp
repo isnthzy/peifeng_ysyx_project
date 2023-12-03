afc9bee1d7221f97bda62557a25be67f34dc13c6
tracer-ysyx <tracer@ysyx.org>
2023-12-03 11:49:48 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:49:48 up 11 min,  1 user,  load average: 0.25, 0.12, 0.09

--- npc/csrc/npc_reg.cpp
+++ npc/csrc/npc_reg.cpp
@@ -2,8 +2,9 @@
 #include "include/npc_verilator.h"
 
 uint32_t read_reg_idx(int idx) {
-  uint32_t* npc_reg_value=(uint32_t*)(top->rootp->SimTop__DOT__RegFile__DOT__rf_0+idx*sizeof(uint32_t));
-  return *npc_reg_value;
+  // uint32_t* npc_reg_value=(uint32_t*)(top->rootp->SimTop__DOT__RegFile__DOT__rf_0+idx*sizeof(uint32_t));
+  uint32_t npc_reg_value=top->rootp->SimTop__DOT__RegFile__DOT__rf_0;
+  return npc_reg_value;
 }
 
 static inline int check_reg_idx(int idx) {
