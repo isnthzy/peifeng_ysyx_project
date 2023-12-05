05cbf80aa32a4a0720c4b2c7371c16e35c63701b
tracer-ysyx <tracer@ysyx.org>
2023-12-05 12:27:46 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:27:46 up  4:02,  2 users,  load average: 0.06, 0.06, 0.06

--- npc/csrc/npc_monitor.cpp
+++ npc/csrc/npc_monitor.cpp
@@ -45,6 +45,10 @@ void init_disasm(const char *triple);
 
 bool ftrace_flag=false;
 static void welcome() {
+  Log("Trace: %s", MUXDEF(CONFIG_TRACE, ANSI_FMT("ON", ANSI_FG_GREEN), ANSI_FMT("OFF", ANSI_FG_RED)));
+  IFDEF(CONFIG_TRACE, Log("If trace is enabled, a log file will be generated "
+        "to record the trace. This may lead to a large log file. "
+        "If it is not necessary, you can disable it in menuconfig"));
   Log("Build time: %s, %s", __TIME__, __DATE__);
   printf("Welcome to %s-NPC!\n", ANSI_FMT(str(riscv32e), ANSI_FG_YELLOW ANSI_BG_RED));
   printf("For help, type \"help\"\n");
