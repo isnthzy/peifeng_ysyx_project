ba305527f6807c78c51a9aa77a0af28ee11ac59e
tracer-ysyx <tracer@ysyx.org>
2023-12-05 08:59:10 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:59:10 up 38 min,  3 users,  load average: 0.00, 0.00, 0.01

--- npc/csrc/npc_log.cpp
+++ npc/csrc/npc_log.cpp
@@ -13,22 +13,22 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
-#include "include/npc_common.h"
+// #include "include/npc_common.h"
 
-// extern uint64_t g_nr_guest_inst;
-FILE *log_fp = NULL;
+// // extern uint64_t g_nr_guest_inst;
+// FILE *log_fp = NULL;
 
-void init_log(const char *log_file) {
-  log_fp = stdout;
-  if (log_file != NULL) {
-    FILE *fp = fopen(log_file, "w");
-    Assert(fp, "Can not open '%s'", log_file);
-    log_fp = fp;
-  }
-  Log("Log is written to %s", log_file ? log_file : "stdout");
-}
-
-// bool log_enable() {
-//   return MUXDEF(CONFIG_TRACE, (g_nr_guest_inst >= CONFIG_TRACE_START) &&
-//          (g_nr_guest_inst <= CONFIG_TRACE_END), false);
+// void init_log(const char *log_file) {
+//   log_fp = stdout;
+//   if (log_file != NULL) {
+//     FILE *fp = fopen(log_file, "w");
+//     Assert(fp, "Can not open '%s'", log_file);
+//     log_fp = fp;
+//   }
+//   Log("Log is written to %s", log_file ? log_file : "stdout");
 // }
+
+// // bool log_enable() {
+// //   return MUXDEF(CONFIG_TRACE, (g_nr_guest_inst >= CONFIG_TRACE_START) &&
+// //          (g_nr_guest_inst <= CONFIG_TRACE_END), false);
+// // }
