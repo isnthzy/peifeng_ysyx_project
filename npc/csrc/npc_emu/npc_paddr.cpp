603cd3cdd3619d0cbf73d0cc8e8e82d61cb04697
tracer-ysyx <tracer@ysyx.org>
2023-12-05 12:55:52 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:55:52 up  4:30,  2 users,  load average: 0.15, 0.18, 0.12

--- npc/csrc/npc_emu/npc_paddr.cpp
+++ npc/csrc/npc_emu/npc_paddr.cpp
@@ -1,7 +1,7 @@
 #include "../include/npc_common.h"
 #include "../include/npc_verilator.h"
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN={};
-
+extern void putIringbuf();
 static inline uint32_t host_read(void *addr, int len) {
   switch (len) {
     case 1: return *(uint8_t  *)addr;
@@ -27,7 +27,7 @@ uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static void out_of_bound(paddr_t addr) {
-  // putIringbuf();
+  putIringbuf();
   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
       addr, PMEM_LEFT, PMEM_RIGHT,top->io_pc);
 }
