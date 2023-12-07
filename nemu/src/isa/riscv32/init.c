0ca6fb52b8a88d83e026178babb642d6a00cb6de
tracer-ysyx <tracer@ysyx.org>
2023-12-07 22:09:26 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:09:26 up  7:42,  3 users,  load average: 0.19, 0.22, 0.19

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -29,7 +29,6 @@ static const uint32_t img [] = {
 static void restart() {
   /* Set the initial program counter. */
   cpu.pc = RESET_VECTOR;
-    printf("%x\n\n\n\n\n\n",cpu.pc);
 
   /* The zero register is always 0. */
   cpu.gpr[0] = 0;
