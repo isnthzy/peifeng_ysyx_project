9fe38495195177d17f35a1cf8d45aed027e845f7
tracer-ysyx <tracer@ysyx.org>
2023-12-10 14:18:26 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:18:26 up 18 min,  3 users,  load average: 0.32, 0.15, 0.11

--- npc/csrc/npc_exe.cpp
+++ npc/csrc/npc_exe.cpp
@@ -111,7 +111,7 @@ static void npc_execute(uint64_t n) {
     static word_t this_pc;
     static word_t next_pc;
     
-    
+    cpy_reg();
     step_and_dump_wave(); //step_and_dump_wave();要放对位置，因为放错位置排查好几个小时
     
     this_pc=cpu.pc;
@@ -122,7 +122,7 @@ static void npc_execute(uint64_t n) {
     /*------------------------分割线每个npc_execute其实是clk变化两次，上边变化一次，下边也变化一次*/
 
     top->clock=0;
-    cpy_reg();
+    
     step_and_dump_wave();
     if(difftest_flag) difftest_step(cpu.pc,next_pc);
     if (npc_state.state != NPC_RUNNING) break;
