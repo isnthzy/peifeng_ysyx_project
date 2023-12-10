7348ce0ead4053fbdd82e42dd7eaeaccceba6a64
tracer-ysyx <tracer@ysyx.org>
2023-12-10 14:58:12 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:58:12 up 58 min,  3 users,  load average: 0.42, 0.31, 0.20

--- npc/csrc/npc_exe.cpp
+++ npc/csrc/npc_exe.cpp
@@ -26,12 +26,12 @@ void step_and_dump_wave(){
 //----------------------------dpi-c----------------------------
 extern "C" void sim_break(int nextpc,int ret_reg){
   npc_state.halt_ret=ret_reg;
-  npc_state.halt_pc=nextpc;
+  npc_state.halt_pc=cpu.nextpc;
   npc_state.state=NPC_END;
 }
 extern "C" void inv_break(int nextpc){
   printf("????");
-  npc_state.halt_pc=nextpc;
+  npc_state.halt_pc=cpu.nextpc;
   npc_state.state=NPC_ABORT;
 }
 
