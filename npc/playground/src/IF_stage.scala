e4894924f56db7c8e7b6a30ace02638090d6d1fe
tracer-ysyx <tracer@ysyx.org>
2023-12-13 20:07:54 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:07:54 up  4:48,  1 user,  load average: 0.25, 0.19, 0.27

--- npc/playground/src/IF_stage.scala
+++ npc/playground/src/IF_stage.scala
@@ -1,49 +1,48 @@
 import chisel3._
-import chisel3.util._  
+import chisel3.util._
 import config.Configs._
 
-class IF_stage extends Module{
-  val io=IO(new Bundle {
-    val jalr_taget=Input(UInt(32.W))
-    val is_not_jalr=Input(Bool())
-    val is_jump=Input(Bool())
-    val Imm=Input(UInt(32.W))
-    val pc=Output(UInt(ADDR_WIDTH.W))
-    val nextpc=Output(UInt(ADDR_WIDTH.W))
-    val inst=Output(UInt(32.W))
-    val f_dbus=Output(new if_to_id_bus())
+class IF_stage extends Module {
+  val io = IO(new Bundle {
+    val jalr_taget  = Input(UInt(32.W))
+    val is_not_jalr = Input(Bool())
+    val is_jump     = Input(Bool())
+    val Imm         = Input(UInt(32.W))
+    val pc          = Output(UInt(ADDR_WIDTH.W))
+    val nextpc      = Output(UInt(ADDR_WIDTH.W))
+    val inst        = Output(UInt(32.W))
+    val f_dbus      = Output(new if_to_id_bus())
   })
-  val REGpc=RegInit(START_ADDR)
-  val fetch_inst=Module(new fetch_inst())
-  val snpc=dontTouch(Wire(UInt(ADDR_WIDTH.W)))
-  val dnpc=dontTouch(Wire(UInt(ADDR_WIDTH.W)))
-  fetch_inst.io.clock:=clock
-  fetch_inst.io.reset:=reset
-  fetch_inst.io.pc:=REGpc
-  fetch_inst.io.nextpc:=io.nextpc
-  io.inst:=fetch_inst.io.inst
-  
-  val Imm=WireInit(0.U(32.W))
-  val is_not_jalr=WireInit(true.B)
-  val jalr_taget=WireInit(0.U(32.W))
-  val is_jump=WireInit(true.B)
-  snpc:=REGpc+4.U
-  dnpc:=Mux(is_not_jalr,REGpc+Imm,jalr_taget) //不是jalr就是jal和IsaB
-  REGpc:=Mux(is_jump,dnpc,snpc)
-  
-  io.f_dbus.snpc:=snpc
-  io.nextpc:=dnpc
-  io.pc:=REGpc
+  val REGpc      = RegInit(START_ADDR)
+  val fetch_inst = Module(new fetch_inst())
+  val snpc       = dontTouch(Wire(UInt(ADDR_WIDTH.W)))
+  val dnpc       = dontTouch(Wire(UInt(ADDR_WIDTH.W)))
+  fetch_inst.io.clock  := clock
+  fetch_inst.io.reset  := reset
+  fetch_inst.io.pc     := REGpc
+  fetch_inst.io.nextpc := io.nextpc
+  io.inst              := fetch_inst.io.inst
+
+  val Imm         = WireInit(0.U(32.W))
+  val is_not_jalr = WireInit(true.B)
+  val jalr_taget  = WireInit(0.U(32.W))
+  val is_jump     = WireInit(true.B)
+  snpc  := REGpc + 4.U
+  dnpc  := Mux(is_not_jalr, REGpc + Imm, jalr_taget) //不是jalr就是jal和IsaB
+  REGpc := Mux(is_jump, dnpc, snpc)
+
+  io.f_dbus.snpc := snpc
+  io.nextpc      := dnpc
+  io.pc          := REGpc
 }
 
-
-class fetch_inst extends BlackBox with HasBlackBoxPath{
-  val io=IO(new Bundle {
-    val clock=Input(Clock())
-    val reset=Input(Bool())
-    val pc=Input(UInt(32.W))
-    val nextpc=Input(UInt(32.W))
-    val inst=Output(UInt(32.W))
+class fetch_inst extends BlackBox with HasBlackBoxPath {
+  val io = IO(new Bundle {
+    val clock  = Input(Clock())
+    val reset  = Input(Bool())
+    val pc     = Input(UInt(32.W))
+    val nextpc = Input(UInt(32.W))
+    val inst   = Output(UInt(32.W))
   })
   addPath("playground/src/v_resource/fetch_inst.sv")
 }
--- npc/playground/src/SimTop.scala
+++ npc/playground/src/SimTop.scala
@@ -1,51 +1,49 @@
 import chisel3._
-import chisel3.util._  
+import chisel3.util._
 import config.Configs._
 
 class SimTop extends Module {
   val io = IO(new Bundle {
-    val result=Output(UInt(32.W))
+    val result = Output(UInt(32.W))
   })
 
-//定义变量 
-  val Imm   =dontTouch(Wire(UInt(32.W)))
-  val pc    =dontTouch(Wire(UInt(32.W)))
-  val nextpc=dontTouch(Wire(UInt(32.W)))
-  val inst  =dontTouch(Wire(UInt(32.W)))
-  val is_not_jalr=dontTouch(Wire(Bool()))
-  val is_jump=dontTouch(Wire(Bool()))
-  val jalr_taget=dontTouch(Wire(UInt(32.W)))
-  val d_ebus=Wire(new id_to_es_bus())
+//定义变量
+  val Imm         = dontTouch(Wire(UInt(32.W)))
+  val pc          = dontTouch(Wire(UInt(32.W)))
+  val nextpc      = dontTouch(Wire(UInt(32.W)))
+  val inst        = dontTouch(Wire(UInt(32.W)))
+  val is_not_jalr = dontTouch(Wire(Bool()))
+  val is_jump     = dontTouch(Wire(Bool()))
+  val jalr_taget  = dontTouch(Wire(UInt(32.W)))
+  val d_ebus      = Wire(new id_to_es_bus())
 // IFU begin
-  val IF_stage=Module(new IF_stage())
-  pc:=IF_stage.io.pc
-  nextpc:=IF_stage.io.nextpc
-  inst:=IF_stage.io.inst
-  IF_stage.io.Imm:=Imm
-  IF_stage.io.jalr_taget:=jalr_taget
-  IF_stage.io.is_not_jalr:=is_not_jalr
-  IF_stage.io.is_jump:=is_jump
+  val IF_stage = Module(new IF_stage())
+  pc                      := IF_stage.io.pc
+  nextpc                  := IF_stage.io.nextpc
+  inst                    := IF_stage.io.inst
+  IF_stage.io.Imm         := Imm
+  IF_stage.io.jalr_taget  := jalr_taget
+  IF_stage.io.is_not_jalr := is_not_jalr
+  IF_stage.io.is_jump     := is_jump
 // IDU begin
-  val ID_stage=Module(new ID_stage())
-  ID_stage.io.inst:=inst
-  ID_stage.io.pc:=pc
-  ID_stage.io.nextpc:=nextpc
-  ID_stage.io.result:=io.result
-  Imm:=ID_stage.io.Imm
-  is_jump:=ID_stage.io.is_jump
-  is_not_jalr:=ID_stage.io.is_not_jalr
+  val ID_stage = Module(new ID_stage())
+  ID_stage.io.inst   := inst
+  ID_stage.io.pc     := pc
+  ID_stage.io.nextpc := nextpc
+  ID_stage.io.result := io.result
+  Imm                := ID_stage.io.Imm
+  is_jump            := ID_stage.io.is_jump
+  is_not_jalr        := ID_stage.io.is_not_jalr
 
-  ID_stage.io.f_dbus:=IF_stage.io.f_dbus
-  d_ebus:=ID_stage.io.d_ebus
+  ID_stage.io.f_dbus := IF_stage.io.f_dbus
+  d_ebus             := ID_stage.io.d_ebus
 // EXU begin
-  val EXE_stage=Module(new EXE_stage())
-  EXE_stage.io.pc:=pc
-  jalr_taget:=EXE_stage.io.jalr_taget
-  io.result:=EXE_stage.io.result
+  val EXE_stage = Module(new EXE_stage())
+  EXE_stage.io.pc := pc
+  jalr_taget      := EXE_stage.io.jalr_taget
+  io.result       := EXE_stage.io.result
 
-  EXE_stage.io.d_ebus:=ID_stage.io.d_ebus
-  
+  EXE_stage.io.d_ebus := ID_stage.io.d_ebus
+
 // WB begin
 }
-
-
