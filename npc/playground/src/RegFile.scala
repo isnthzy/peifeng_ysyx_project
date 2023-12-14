eb50ce1ee4cd869d68c9c76c7acaf706f83010fc
tracer-ysyx <tracer@ysyx.org>
2023-12-14 19:19:22 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:19:22 up  3:15,  3 users,  load average: 0.59, 0.47, 0.31

--- npc/playground/src/RegFile.scala
+++ npc/playground/src/RegFile.scala
@@ -14,8 +14,8 @@ class RegFile extends Module{
   val rf=RegInit(VecInit(Seq.fill(32)(0.U(32.W))))
   val wdata=Mux(io.waddr===0.U,0.U,io.wdata)
   when(io.wen){ rf(io.waddr):=wdata }
-  printf("%x %x",io.waddr,wdata)
-  printf("rf_1 %x ,rf_2 %x",rf(1),rf(2))
+  printf("%x %x\n",io.waddr,wdata)
+  printf("rf_1 %x ,rf_2 %x\n",rf(1),rf(2))
   io.rdata1:=Mux(io.raddr1=/=0.U,rf(io.raddr1),0.U)
   io.rdata2:=Mux(io.raddr2=/=0.U,rf(io.raddr2),0.U)
 }
