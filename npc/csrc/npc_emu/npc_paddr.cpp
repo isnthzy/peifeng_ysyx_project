5d5c95ea70351071a531edc4362da239ef29252a
tracer-ysyx <tracer@ysyx.org>
2023-12-14 16:27:28 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:27:28 up 23 min,  1 user,  load average: 0.20, 0.14, 0.10

--- npc/csrc/npc_emu/npc_paddr.cpp
+++ npc/csrc/npc_emu/npc_paddr.cpp
@@ -45,7 +45,7 @@ void pmem_write(paddr_t addr, int len, word_t data) {
 //----------------------------dpi-c----------------------------
 extern "C" void pmem_read(int raddr, int *rdata) {
   *rdata=paddr_read(raddr,4);
-  cpu.inst=paddr_read(raddr,4);
+  cpu.inst=*rdata;
   // 总是读取地址为`raddr & ~0x3u`的4字节返回给`rdata`
 }
 extern "C" void pmem_write(int waddr, int wdata, char wmask) {
@@ -73,7 +73,6 @@ void paddr_write(paddr_t addr, int len, word_t data) {
   Log("w: 0x%x data:0x%08x",addr,data);
   #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  printf("131312313123");
   // IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
