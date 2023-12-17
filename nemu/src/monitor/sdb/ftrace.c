fa37df270bd1ca3350fde476a6c60228a3d8012b
tracer-ysyx <tracer@ysyx.org>
2023-12-17 18:44:16 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:44:16 up 11:39,  6 users,  load average: 0.04, 0.09, 0.08

--- nemu/src/monitor/sdb/ftrace.c
+++ nemu/src/monitor/sdb/ftrace.c
@@ -23,62 +23,89 @@ void init_elf(const char *elf_file){
         return ;
     }
     else Log("Ftrace: ON");
-
-    FILE* file = fopen(elf_file, "rb");//以只读的形式打开elf_file
-    if(!file){
-        Log("文件打开失败!\n");
-        assert(0);
+   FILE* file = fopen(elf_file, "rb");
+    if (!file) {
+        printf("Failed to open file: %s\n", elf_file);
+        return ;
     }
+
+    Elf32_Ehdr elf_header;
     size_t result;
-    
-    // 读取 ELF 文件的头部信息
-    Elf_Ehdr elf_header;
-    result=fread(&elf_header, sizeof(Elf_Ehdr), 1, file);
-    if (result== 0) assert(0);
-    // 获取节头表的偏移量和条目数量
-    Elf_Off section_header_offset = elf_header.e_shoff;
-    Elf_Half section_header_entry_count = elf_header.e_shnum;
-    // 定位到节头表
-    fseek(file, section_header_offset, SEEK_SET);
-    // 读取节头表
-    Elf_Shdr section_headers[section_header_entry_count];
-    result=fread(section_headers, sizeof(Elf_Shdr), section_header_entry_count, file);
-    // 定位到字符串表节
-    Elf_Shdr string_table_header = section_headers[elf_header.e_shstrndx];
-    fseek(file, string_table_header.sh_offset, SEEK_SET);
-    // 读取字符串表内容
-    char string_table[string_table_header.sh_size];
-    result=fread(string_table, string_table_header.sh_size, 1, file);
-    // 查找符号表节和字符串表节
-    Elf_Shdr symtab_header;
-    for (int i = 0; i < section_header_entry_count; ++i) {
+    result=fread(&elf_header, sizeof(Elf32_Ehdr), 1, file);
+    if(result==0) assert(0);
+    if (memcmp(elf_header.e_ident, ELFMAG, SELFMAG) != 0) {
+        printf("Invalid ELF file: %s\n", elf_file);
+        fclose(file);
+        return ;
+    }
+
+    fseek(file, elf_header.e_shoff, SEEK_SET);
+    Elf32_Shdr* section_headers = (Elf32_Shdr*)malloc(elf_header.e_shentsize * elf_header.e_shnum);
+    result=fread(section_headers, elf_header.e_shentsize, elf_header.e_shnum, file);
+
+    Elf32_Shdr* string_table_header = &section_headers[elf_header.e_shstrndx];
+    char* string_table = (char*)malloc(string_table_header->sh_size);
+    fseek(file, string_table_header->sh_offset, SEEK_SET);
+    result=fread(string_table, string_table_header->sh_size, 1, file);
+
+    Elf32_Shdr* symbol_table_header = NULL;
+    Elf32_Shdr* text_section_header = NULL;
+
+    for (int i = 0; i < elf_header.e_shnum; ++i) {
         if (section_headers[i].sh_type == SHT_SYMTAB) {
-            symtab_header = section_headers[i];
+            symbol_table_header = &section_headers[i];
             break;
         }
     }
-    // 定位到符号表节
-    fseek(file, symtab_header.sh_offset, SEEK_SET);
-    // 计算符号表中的符号数量
-    size_t symbol_count = symtab_header.sh_size / symtab_header.sh_entsize;
-    // 读取符号表
-    Elf32_Sym symbols[symtab_header.sh_size];
-    result=fread(symbols,symtab_header.sh_size,1, file);
-    // 遍历符号表，筛选出类型为FUNC的符号
-    for (size_t i = 0; i < symbol_count; ++i) {
-        if (ELF32_ST_TYPE(symbols[i].st_info) == STT_FUNC) {
-            if(symbols[i].st_size==0) continue; //不符合的大小直接略过
-            // 获取符号的名称
-            char* symbol_name=string_table + symbols[i].st_name;
-            strcpy(elf_func[func_cnt].func_name,symbol_name);
+
+    if (!symbol_table_header) {
+        printf("No symbol table found in the ELF file.\n");
+        free(section_headers);
+        free(string_table);
+        fclose(file);
+        return ;
+    }
+
+    for (int i = 0; i < elf_header.e_shnum; ++i) {
+        if (section_headers[i].sh_addr == symbol_table_header->sh_link) {
+            text_section_header = &section_headers[i];
+            break;
+        }
+    }
+
+    if (!text_section_header) {
+        printf("No associated text section found for the symbol table.\n");
+        free(section_headers);
+        free(string_table);
+        fclose(file);
+        return ;
+    }
+
+    Elf32_Sym* symbols = (Elf32_Sym*)malloc(symbol_table_header->sh_size);
+    fseek(file, symbol_table_header->sh_offset, SEEK_SET);
+    result=fread(symbols, symbol_table_header->sh_size, 1, file);
+
+    int symbol_count = symbol_table_header->sh_size / sizeof(Elf32_Sym);
+
+    for (int i = 0; i < symbol_count; ++i) {
+        Elf32_Sym* symbol = &symbols[i];
+        if (ELF32_ST_TYPE(symbol->st_info) == STT_FUNC) {
+            const char* function_name = string_table + symbol->st_name;
+            strcpy(elf_func[func_cnt].func_name,function_name);
             // 获取符号的地址
-            elf_func[func_cnt].value=symbols[i].st_value;
-            elf_func[func_cnt].size =symbols[i].st_size;
+            elf_func[func_cnt].value=symbol->st_value;
+            elf_func[func_cnt].size =symbol->st_size;
             printf("Function: %s\nAddress: 0x%lx %ld(Dec) %lx(Hec)\n",elf_func[func_cnt].func_name,elf_func[func_cnt].value,elf_func[func_cnt].size,elf_func[func_cnt].size);
             func_cnt++; //func_cnt用于只筛出来符合要求的函数
         }
     }
+
+    free(section_headers);
+    free(string_table);
+    free(symbols);
     fclose(file);
+
+    return ;
 }
 
 /*ftrace追踪内容*/
