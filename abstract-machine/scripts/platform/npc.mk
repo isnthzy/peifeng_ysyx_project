cace78715ebb06ead0262988d8f56f3b70e14e85
tracer-ysyx <tracer@ysyx.org>
2023-12-17 15:48:35 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:48:35 up  8:43,  5 users,  load average: 0.08, 0.24, 0.17

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -15,9 +15,9 @@ LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 
 NPCFLAGS+=-l $(shell dirname $(IMAGE).elf)/npc-log.txt
-NPCFLAGS+=-f $(IMAGE).elf
+# NPCFLAGS+=-f $(IMAGE).elf
 NPCFLAGS+=--diff=$(NEMU_HOME)/build/riscv32-nemu-interpreter-so
-NPCFLAGS+=-b 
+# NPCFLAGS+=-b 
 #批处理模式
 
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
