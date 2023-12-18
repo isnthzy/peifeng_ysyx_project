143a175629318ebe106e03e3357567222c2971d1
tracer-ysyx <tracer@ysyx.org>
2023-12-18 16:55:15 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:55:15 up 16 min,  1 user,  load average: 0.05, 0.05, 0.01

--- nemu/src/monitor/sdb/ftrace.c
+++ nemu/src/monitor/sdb/ftrace.c
@@ -61,7 +61,7 @@ void init_elf(const char *elf_file){
     size_t symbol_count = symtab_header.sh_size / symtab_header.sh_entsize;
     // 读取符号表
     Elf32_Sym symbols[symbol_count*2];
-    result=fread(symbols, sizeof(Elf64_Sym), symbol_count, file);
+    result=fread(symbols, sizeof(Elf32_Sym), symbol_count, file);
     // 遍历符号表，筛选出类型为FUNC的符号
     for (size_t i = 0; i < symbol_count; ++i) {
         if (ELF32_ST_TYPE(symbols[i].st_info) == STT_FUNC) {
