cbca2efe5c0a9cf1f0c87463a883bf803c93f8ae
tracer-ysyx <tracer@ysyx.org>
2023-12-20 16:16:13 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:16:13 up  1:46,  3 users,  load average: 0.20, 0.15, 0.10

--- am-kernels/tests/am-tests/src/tests/rtc.c
+++ am-kernels/tests/am-tests/src/tests/rtc.c
@@ -6,9 +6,9 @@ void rtc_test() {
   while (1) {
     while(io_read(AM_TIMER_UPTIME).us / 1000000 < sec) ;
     rtc = io_read(AM_TIMER_RTC);
-    char abc[]="adadadad";
-    printf(" %s",abc);
-    printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
+    char buffer[1000];
+    sprintf(buffer,"%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
+    putstr(buffer);
     if (sec == 1) {
       printf("%d second).\n", sec);
     } else {
