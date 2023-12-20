ca399a6fa5d1be77e442f3f7b0d79bb5fb81310c
wangxin-wsl2 <isnthzy@gmail.com>
2023-12-20 17:40:57 +0800

    实现printf和sprintf，但是看起来sprintf还是有点bug

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -8,23 +8,30 @@
 int printf(const char *fmt, ...) {
   char out_buffer[1024];
   va_list args;
-  va_start(args, fmt);
-  int len=sprintf(out_buffer,fmt,args);
-  va_end(args);
+  va_start(args,fmt);
+  int len=vsprintf(out_buffer,fmt,args);
+  //传入的参数是va_list类型还是 ...类型还是有区别的
   putstr(out_buffer);
+  va_end(args);
   return len;
   // panic("Not implemented");
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
-  // panic("Not implemented");
+#include <stdlib.h>
+
+char* gSpaces(int length) { //空格生成器
+  if (length < 0) {
+    return NULL;
+  }
+  char* spaces=(char*)malloc((length + 1)*sizeof(char)); 
+  for (int i=0;i<length;i++) {
+    spaces[i]=' ';
+  }
+  spaces[length] = '\0'; // Null-terminate the string
+  return spaces;
 }
 
-int sprintf(char *out, const char *fmt, ...) { //fmt可以当个字符串处理
-  va_list ap;
-  va_start(ap, fmt);
-//   printf("%s\n",out);
+int vsprintf(char *out, const char *fmt, va_list ap) {
   *out='\0';
   char *s,c;
   int d,i;
@@ -34,16 +41,33 @@ int sprintf(char *out, const char *fmt, ...) { //fmt可以当个字符串处理
       continue;
     }
     i++;
+    int width=-1;
+    while (fmt[i]>='0'&&fmt[i]<='9') {
+      width=width*10+(fmt[i]-'0');
+      i++;
+    }
     switch(fmt[i]){
     case 's':
 	    s=va_arg(ap,char*);
+      int s_len=strlen(s);
+      if(width>s_len){
+        s=strcat(gSpaces(width-s_len),s);
+      }
 	    strcat(out,s);
 	    break;
     case 'd':
       d=va_arg(ap,int);
-      char tmp[30];
-      itoa(d,tmp,10);
-      strcat(out,tmp);
+      char d_tmp[32];
+      itoa(d,d_tmp,10);
+      int d_len=strlen(d_tmp);
+      if (width > d_len) {
+        char d_out[64];
+        strcpy(d_out, gSpaces(width - d_len));
+        strcat(d_out, d_tmp);
+        strcat(out, d_out);
+      } else {
+        strcat(out, d_tmp);
+      }
       break;
     case 'c':
       c=(char)va_arg(ap,int);
@@ -51,8 +75,17 @@ int sprintf(char *out, const char *fmt, ...) { //fmt可以当个字符串处理
       break;
     }
   }
-  va_end(ap);
+  free(gSpaces);
   return 0;
+  // panic("Not implemented");
+}
+
+int sprintf(char *out, const char *fmt, ...) { //fmt可以当个字符串处理
+  va_list ap;
+  va_start(ap, fmt);
+  int len = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return len;
   // panic("Not implemented");
 }
 
--- am-kernels/tests/klib-tests/src/sprintf.c
+++ am-kernels/tests/klib-tests/src/sprintf.c
@@ -1,9 +1,9 @@
 #include <klibtest.h>
 #include <limits.h>
 char src1[128];
-char *src2[]={"0","126322567","2147483647","-2147483648","-2147483647","252645135","126322567","-1"};
+char *src2[]={"0","126322567","2147483647","-2147483648","-2147483647","252645135","126322567","-1","1900"};
 int data[] = {0, INT_MAX / 17, INT_MAX, INT_MIN, INT_MIN + 1,
-              UINT_MAX / 17, INT_MAX / 17, UINT_MAX};
+              UINT_MAX / 17, INT_MAX / 17, UINT_MAX,1900};
 int main() {
     for(int i=0;i<sizeof(data)/sizeof(data[0]);i++){
         sprintf(src1, "%d", data[i]);
