176343f62af53cd741c78e229deb9d678eff3884
tracer-ysyx <tracer@ysyx.org>
2023-12-21 18:06:29 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:06:29 up 1 day,  1:34,  8 users,  load average: 0.29, 0.22, 0.15

--- am-kernels/tests/am-tests/src/tests/hello.c
+++ am-kernels/tests/am-tests/src/tests/hello.c
@@ -1,6 +1,8 @@
 #include <amtest.h>
 
 void hello() {
+  AM_TIMER_RTC_T rtc = io_read(AM_TIMER_RTC);
+  if(rtc.year>0) printf("111111");
   for (int i = 0; i < 10; i ++) {
     putstr("Hello, AM World @ " __ISA__ "\n");
   }
