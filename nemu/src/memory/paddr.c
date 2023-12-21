4d0035526e590f8cedb69bb939eb2e2cbbbbe4ed
wangxin-wsl2 <isnthzy@gmail.com>
2023-12-21 18:44:42 +0800

    报段错误，死活运行不通过

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -68,7 +68,7 @@ word_t paddr_read(paddr_t addr, int len,int model) {
   #ifdef CONFIG_MTRACE
   if(model==1){
     char mtrace_logbuf[120];
-    sprintf(mtrace_logbuf,"pc:0x%08x r:0x%x data:0x%08x",cpu.pc,addr,pmem_read(addr, len));
+    sprintf(mtrace_logbuf,"pc:0x%08x addr:0x%x rdata:0x%08x",cpu.pc,addr,pmem_read(addr, len));
     enqueueIRingBuffer(&mtrace_buffer,mtrace_logbuf);
   }
   #endif
@@ -81,7 +81,7 @@ word_t paddr_read(paddr_t addr, int len,int model) {
 void paddr_write(paddr_t addr, int len, word_t data) {
   #ifdef CONFIG_MTRACE
   char mtrace_logbuf[120];
-  sprintf(mtrace_logbuf,"pc:0x%08x w:0x%x data:0x%08x",cpu.pc,addr,data);
+  sprintf(mtrace_logbuf,"pc:0x%08x addr:0x%x wdata:0x%08x",cpu.pc,addr,data);
   enqueueIRingBuffer(&mtrace_buffer,mtrace_logbuf);
   #endif
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
