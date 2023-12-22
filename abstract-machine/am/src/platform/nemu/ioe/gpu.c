c18a2b4124ba3ad30c69a7471bc05f38a06208c7
tracer-ysyx <tracer@ysyx.org>
2023-12-22 20:57:08 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:57:08 up  6:02,  3 users,  load average: 0.11, 0.22, 0.19

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -24,7 +24,9 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   if (ctl->sync) {
+    *fb=(uintptr_t)ctl->pixels;
     outl(SYNC_ADDR, 1);
   }
 }
