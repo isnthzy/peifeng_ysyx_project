12691b65ef5e1cef95f2a960ededbcb760f29080
tracer-ysyx <tracer@ysyx.org>
2023-12-22 18:43:36 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:43:36 up  4:01,  3 users,  load average: 0.24, 0.23, 0.18

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -7,5 +7,5 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t tmp_key=inl(KBD_ADDR);
   bool tmp_keydown=((tmp_key&KEYDOWN_MASK)!=0);
   kbd->keydown = tmp_keydown;
-  kbd->keycode = tmp_key;
+  kbd->keycode = tmp_key&~KEYDOWN_MASK;
 }
