0ebb5c576c19c39ae945fa4f3278806a86db1b6f
tracer-ysyx <tracer@ysyx.org>
2023-12-23 14:25:39 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:25:39 up 16 min,  3 users,  load average: 0.24, 0.27, 0.25

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,34 +4,35 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  // int i;
-  // int w = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
-  // int h = io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // for (i = 0; i < w * h; i ++) fb[i] = i;
-  // outl(SYNC_ADDR, 1);
+  int i;
+  int w = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
+  int h = io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  uint32_t wh_data = inl(VGACTL_ADDR);
-  uint32_t h = wh_data & 0xffff;
-  uint32_t w = wh_data >> 16;
+  uint32_t tmp_gpu_config=inl(VGACTL_ADDR);
+  // int tmp_config_low=(int)(tmp_gpu_config&0xFFFF);
+  // int tmp_config_high=(int)((tmp_gpu_config>>16)&0xFFFF);
+  uint32_t tmp_config_low=tmp_gpu_config&0xFFFF;
+  uint32_t tmp_config_high=(tmp_gpu_config>>16)&0xFFFF;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = w, .height = h,
+    .width = tmp_config_low, .height = tmp_config_high,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-  if (!ctl->sync && (w == 0 || h == 0)) return;
-  uint32_t *pixels = ctl->pixels;
+  if (!ctl->sync && (ctl->h== 0 ||ctl->w== 0)) return;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
-  for (int i = y; i < y+h; i++) {
-    for (int j = x; j < x+w; j++) {
-      fb[screen_w*i+j] = pixels[w*(i-y)+(j-x)];
+  uint32_t vag_begin=inl(VGACTL_ADDR)>>16;
+  uint32_t *pixels=ctl->pixels;
+  for(int y=ctl->y;y<=ctl->y+ctl->h;y++){
+    for(int x=ctl->x;x<=ctl->x+ctl->w;x++){
+      fb[vag_begin*y+x]=pixels[(y-ctl->y)*ctl->w+(x-ctl->x)];
     }
   }
   if (ctl->sync) {
