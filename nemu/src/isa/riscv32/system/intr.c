56dd613f5f8e1e732dc977832f49a94a8ccbfdcb
tracer-ysyx <tracer@ysyx.org>
2023-12-25 20:55:06 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:55:06 up 1 day,  4:16,  7 users,  load average: 0.27, 0.16, 0.15

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -18,9 +18,9 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-  cpu.mepc=cpu.pc;
+  cpu.mepc=epc;
   cpu.mcause=NO;
-  return epc;
+  return cpu.mtvec;
 }
 
 word_t isa_query_intr() {
