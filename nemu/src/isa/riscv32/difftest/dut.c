341cdbf6577549638071d95972db4056e6bdcc5c
tracer-ysyx <tracer@ysyx.org>
2023-12-25 21:01:30 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:01:30 up 1 day,  4:22,  7 users,  load average: 0.27, 0.19, 0.17

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -30,12 +30,11 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
       return false;
     }
   }
-  DIFF_CHECK(ref_r->pc   ,pc       ,"mtvec");
-  DIFF_CHECK(ref_r->mcause ,cpu.mcause ,"mcause");
+  DIFF_CHECK(ref_r->pc   ,pc       ,"pc   ");
   DIFF_CHECK(ref_r->mtvec,cpu.mtvec,"mtvec");
   DIFF_CHECK(ref_r->mepc ,cpu.mepc ,"mepc ");
   DIFF_CHECK(ref_r->mstatus,cpu.mstatus,"mstatus");
-  
+  DIFF_CHECK(ref_r->mcause ,cpu.mcause ,"mcause");
 
   return true;
 }
