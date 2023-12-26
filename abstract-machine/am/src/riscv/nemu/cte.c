71c814f979356c67aface0ee1df4d4bdc184c6ea
tracer-ysyx <tracer@ysyx.org>
2023-12-26 17:43:28 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:43:28 up 2 days,  1:04,  7 users,  load average: 0.11, 0.14, 0.09

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,7 +9,9 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->mcause) {
       case 0xb: //11 Environment call from M-mode
-        if(c->GPR1==-1) ev.event =EVENT_YIELD;
+        if(c->GPR1==-1){ev.event =EVENT_YIELD;
+          printf("is_yield");
+        }
         break;
       default: ev.event = EVENT_ERROR; break;
     }
