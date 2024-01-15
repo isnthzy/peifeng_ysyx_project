a8e7c8b8d406a2116bf9756fb88f11f1ebd54e56
tracer-ysyx <tracer@ysyx.org>
2024-01-15 11:22:22 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:22:22 up 19:58,  3 users,  load average: 0.71, 0.36, 0.29

--- npc/vsrc/Alu.sv
+++ npc/vsrc/Alu.sv
@@ -6,45 +6,45 @@ module Alu(	// @[<stdin>:814:3]
   output [31:0] io_result	// @[playground/src/Alu.scala:5:14]
 );
 
-  reg  [31:0] casez_tmp;	// @[playground/src/Alu.scala:36:37]
+  reg  [31:0] casez_tmp;	// @[playground/src/Alu.scala:37:37]
   wire [94:0] _sll_T_1 = {63'h0, io_src1} << io_src2[5:0];	// @[playground/src/Alu.scala:14:{18,28}]
   wire [31:0] _GEN = {27'h0, io_src2[4:0]};	// @[playground/src/Alu.scala:28:{33,43}]
-  always_comb begin	// @[playground/src/Alu.scala:36:37]
-    casez (io_op)	// @[playground/src/Alu.scala:36:37]
+  always_comb begin	// @[playground/src/Alu.scala:37:37]
+    casez (io_op)	// @[playground/src/Alu.scala:37:37]
       4'b0000:
-        casez_tmp = io_src1 + io_src2;	// @[playground/src/Alu.scala:16:24, :36:37]
+        casez_tmp = io_src1 + io_src2;	// @[playground/src/Alu.scala:16:24, :37:37]
       4'b0001:
-        casez_tmp = io_src1 - io_src2;	// @[playground/src/Alu.scala:18:24, :36:37]
+        casez_tmp = io_src1 - io_src2;	// @[playground/src/Alu.scala:18:24, :37:37]
       4'b0010:
-        casez_tmp = io_src1 & io_src2;	// @[playground/src/Alu.scala:20:25, :36:37]
+        casez_tmp = io_src1 & io_src2;	// @[playground/src/Alu.scala:20:25, :37:37]
       4'b0011:
-        casez_tmp = io_src1 | io_src2;	// @[playground/src/Alu.scala:22:25, :36:37]
+        casez_tmp = io_src1 | io_src2;	// @[playground/src/Alu.scala:22:25, :37:37]
       4'b0100:
-        casez_tmp = io_src1 ^ io_src2;	// @[playground/src/Alu.scala:24:25, :36:37]
+        casez_tmp = io_src1 ^ io_src2;	// @[playground/src/Alu.scala:24:25, :37:37]
       4'b0101:
-        casez_tmp = {31'h0, $signed(io_src1) < $signed(io_src2)};	// @[playground/src/Alu.scala:32:33, :36:37]
+        casez_tmp = {31'h0, $signed(io_src1) < $signed(io_src2)};	// @[playground/src/Alu.scala:32:33, :37:37]
       4'b0110:
-        casez_tmp = _sll_T_1[31:0];	// @[playground/src/Alu.scala:14:18, :26:20, :36:37]
+        casez_tmp = _sll_T_1[31:0];	// @[playground/src/Alu.scala:14:18, :26:20, :37:37]
       4'b0111:
-        casez_tmp = {31'h0, io_src1 < io_src2};	// @[playground/src/Alu.scala:34:33, :36:37]
+        casez_tmp = {31'h0, io_src1 < io_src2};	// @[playground/src/Alu.scala:34:33, :37:37]
       4'b1000:
-        casez_tmp = io_src1 >> _GEN;	// @[playground/src/Alu.scala:28:33, :30:33, :36:37]
+        casez_tmp = io_src1 >> _GEN;	// @[playground/src/Alu.scala:28:33, :30:33, :37:37]
       4'b1001:
-        casez_tmp = $signed($signed(io_src1) >>> _GEN);	// @[playground/src/Alu.scala:28:33, :36:37]
+        casez_tmp = $signed($signed(io_src1) >>> _GEN);	// @[playground/src/Alu.scala:28:33, :37:37]
       4'b1010:
-        casez_tmp = 32'h0;	// @[playground/src/Alu.scala:36:37]
+        casez_tmp = 32'h0;	// @[playground/src/Alu.scala:37:37]
       4'b1011:
-        casez_tmp = 32'h0;	// @[playground/src/Alu.scala:36:37]
+        casez_tmp = {io_src2[31:12], 12'h0};	// @[playground/src/Alu.scala:36:{20,28}, :37:37]
       4'b1100:
-        casez_tmp = 32'h0;	// @[playground/src/Alu.scala:36:37]
+        casez_tmp = 32'h0;	// @[playground/src/Alu.scala:37:37]
       4'b1101:
-        casez_tmp = 32'h0;	// @[playground/src/Alu.scala:36:37]
+        casez_tmp = 32'h0;	// @[playground/src/Alu.scala:37:37]
       4'b1110:
-        casez_tmp = 32'h0;	// @[playground/src/Alu.scala:36:37]
+        casez_tmp = 32'h0;	// @[playground/src/Alu.scala:37:37]
       default:
-        casez_tmp = 32'h0;	// @[playground/src/Alu.scala:36:37]
-    endcase	// @[playground/src/Alu.scala:36:37]
+        casez_tmp = 32'h0;	// @[playground/src/Alu.scala:37:37]
+    endcase	// @[playground/src/Alu.scala:37:37]
   end // always_comb
-  assign io_result = casez_tmp;	// @[<stdin>:814:3, playground/src/Alu.scala:36:37]
+  assign io_result = casez_tmp;	// @[<stdin>:814:3, playground/src/Alu.scala:37:37]
 endmodule
 
