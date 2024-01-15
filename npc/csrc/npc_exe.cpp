00f64b8c10e022377ef479552cb3edbae488a598
tracer-ysyx <tracer@ysyx.org>
2024-01-15 10:01:55 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:01:55 up 18:32,  3 users,  load average: 0.36, 0.27, 0.20

--- npc/csrc/npc_exe.cpp
+++ npc/csrc/npc_exe.cpp
@@ -163,7 +163,10 @@ void npc_exev(uint64_t step){ //之所以不用int因为int是有符号的，批
   switch (npc_state.state) {
     case NPC_RUNNING: npc_state.state = NPC_STOP; break;
     case NPC_END: case NPC_ABORT:
-      if(npc_state.state==NPC_ABORT||npc_state.halt_ret!=0) IFDEF(CONFIG_ITRACE, putIringbuf();); 
+      if(npc_state.state==NPC_ABORT||npc_state.halt_ret!=0){
+        IFDEF(CONFIG_ITRACE, putIringbuf()); 
+        IFDEF(CONFIG_MTRACE, mputIringbuf()); 
+      }
       Log("npc: %s at pc = " FMT_WORD,
           (npc_state.state == NPC_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED):
            (npc_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_CYAN):
