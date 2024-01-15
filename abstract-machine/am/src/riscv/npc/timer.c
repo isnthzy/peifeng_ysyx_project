edc2960290f7d0c7c1bf20938f9aad575ec4c857
tracer-ysyx <tracer@ysyx.org>
2024-01-15 22:01:32 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:01:32 up 1 day,  4:09,  3 users,  load average: 0.47, 0.29, 0.24

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -6,8 +6,7 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint32_t tmp_us=inl(RTC_ADDR);
-  uptime->us = tmp_us;
+  uptime->us = 0;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
