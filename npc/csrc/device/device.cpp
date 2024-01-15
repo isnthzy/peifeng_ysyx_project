82f363cbe1fc35e7e38e7d38246991acb1362ecb
tracer-ysyx <tracer@ysyx.org>
2024-01-15 22:59:18 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:59:18 up 1 day,  5:07,  5 users,  load average: 0.12, 0.25, 0.25

--- npc/csrc/device/device.cpp
+++ npc/csrc/device/device.cpp
@@ -2,6 +2,7 @@
 #define DEVICE_BASE 0xa0000000
 #define SERIAL_PORT (DEVICE_BASE + 0x00003f8)
 #define RTC_ADDR    (DEVICE_BASE + 0x0000048)
+extern uint64_t get_time();
 extern void out_of_bound(paddr_t addr);
 void  device_write(paddr_t addr,word_t data){
   switch (addr)
@@ -15,13 +16,17 @@ void  device_write(paddr_t addr,word_t data){
     break;
   }
 }
-
+uint64_t rtc_us=0;
 word_t device_read(paddr_t addr){
   switch (addr)
   {
   case RTC_ADDR:
+  case RTC_ADDR+4:
+    rtc_us=get_time();
+    if(addr==RTC_ADDR)   return (uint32_t)rtc_us;
+    if(addr==RTC_ADDR+4) return rtc_us >> 32;
     break;
-  
+
   default:
     out_of_bound(addr);
     break;
