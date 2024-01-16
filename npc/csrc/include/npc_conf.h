aa93f23a10ed5c8299234d5e3308ea225e343282
tracer-ysyx <tracer@ysyx.org>
2024-01-16 09:21:10 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:21:10 up 1 day,  6:09,  5 users,  load average: 0.25, 0.27, 0.26

--- npc/csrc/include/npc_conf.h
+++ npc/csrc/include/npc_conf.h
@@ -5,7 +5,7 @@
 #define CONFIG_ITRACE 1
 #define CONFIG_FTRACE 1
 #define CONFIG_MTRACE 1
-// #define CONFIG_DIFFTEST 1
+#define CONFIG_DIFFTEST 1
 #define CONFIG_PC_RESET_OFFSET 0x4
 #define DIFFTEST_TO_REF 1
 #define DIFFTEST_TO_DUT 0
@@ -13,4 +13,4 @@
 #define ITRACE_LOGBUF_SIZE 10
 #define DTRACE_LOGBUF_SIZE 10
 #define ETRACE_LOGBUF_SIZE 10
-//trace最多开128个
+//trace最多开128个,因为我只给了128个
