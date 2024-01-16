2569139234bb15d944bbc0c7aa4766d6fecd92b1
tracer-ysyx <tracer@ysyx.org>
2024-01-16 21:12:19 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:12:19 up 1 day, 17:01,  7 users,  load average: 0.21, 0.25, 0.22

--- npc/csrc/npc_emu/difftest.cpp
+++ npc/csrc/npc_emu/difftest.cpp
@@ -148,7 +148,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
       panic("can not catch up with ref.pc = " FMT_WORD " at pc = " FMT_WORD, ref_r.pc, npc);
     return;
   }
-
+  printf("aaa: pc: %x npc: %x deque: %x\n",pc,npc,deque_pc.front());
   if (!deque_pc.empty()&&deque_pc.front()==npc) {
     printf("aaa: pc: %x npc: %x deque: %x\n",pc,npc,deque_pc.front());
     // to skip the checking of an instruction, just copy the reg state to reference design
