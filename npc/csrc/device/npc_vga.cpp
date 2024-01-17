e6184b5f5375d1e89e2f053b9d1e75fd83775f43
tracer-ysyx <tracer@ysyx.org>
2024-01-17 23:37:59 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:37:59 up  2:39,  3 users,  load average: 0.13, 0.07, 0.04

--- npc/csrc/device/npc_vga.cpp
+++ npc/csrc/device/npc_vga.cpp
@@ -50,8 +50,8 @@ static uint32_t screen_size() {
   return screen_width() * screen_height() * sizeof(uint32_t);
 }
 
-static void *vmem = NULL;
-static uint32_t *vgactl_port_base = NULL;
+static uint32_t vmem[120000];
+static uint32_t vgactl_port_base[2];
 
 #ifdef CONFIG_VGA_SHOW_SCREEN
 #include <SDL2/SDL.h>
@@ -91,7 +91,6 @@ void vga_update_screen() {
 }
 
 void init_vga() {
-  vgactl_port_base = (uint32_t *)new_space(8);
   vgactl_port_base[0] = (screen_width() << 16) | screen_height();
 // #ifdef CONFIG_HAS_PORT_IO
 //   add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 8, NULL);
@@ -99,7 +98,6 @@ void init_vga() {
 //   add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8, NULL);
 // #endif
 
-  vmem = new_space(screen_size());
   // add_mmio_map("vmem", CONFIG_FB_ADDR, vmem, screen_size(), NULL);
   IFDEF(CONFIG_VGA_SHOW_SCREEN, init_screen());
   IFDEF(CONFIG_VGA_SHOW_SCREEN, memset(vmem, 0, screen_size()));
