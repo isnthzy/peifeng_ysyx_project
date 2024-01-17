21c6a01f702efef206f827500a47fc688b1add57
tracer-ysyx <tracer@ysyx.org>
2024-01-17 08:49:55 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:49:55 up 1 day, 19:45,  7 users,  load average: 0.54, 0.38, 0.27

--- npc/csrc/npc_emu/difftest.cpp
+++ npc/csrc/npc_emu/difftest.cpp
@@ -150,7 +150,6 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   }
   // printf("aaa: pc: %x npc: %x deque: %x\n",pc,npc,deque_pc.front());
   if (!deque_pc.empty()&&deque_pc.front()==pc) {
-    printf("aaa: pc: %x npc: %x deque: %x\n",pc,npc,deque_pc.front());
     // to skip the checking of an instruction, just copy the reg state to reference design
     ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF,npc);
     deque_pc.pop_front();
