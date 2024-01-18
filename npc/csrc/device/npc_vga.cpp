ddbf7a39e8da71eee834f888a4054f4d415b8732
tracer-ysyx <tracer@ysyx.org>
2024-01-18 17:01:10 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:01:10 up  4:25,  6 users,  load average: 0.28, 0.22, 0.14

--- npc/csrc/device/npc_vga.cpp
+++ npc/csrc/device/npc_vga.cpp
@@ -45,7 +45,6 @@ static void init_screen() {
 }
 
 static inline void update_screen() {
-  printf("update_screen\n");
   SDL_UpdateTexture(texture, NULL, vmem, SCREEN_W * sizeof(uint32_t));
   SDL_RenderClear(renderer);
   SDL_RenderCopy(renderer, texture, NULL, NULL);
