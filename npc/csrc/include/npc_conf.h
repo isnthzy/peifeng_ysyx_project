17665cc97ba827680a611ac5280fb1dae190db0f
tracer-ysyx <tracer@ysyx.org>
2024-01-19 09:14:42 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:14:42 up 23 min,  2 users,  load average: 0.10, 0.13, 0.05

--- npc/csrc/include/npc_conf.h
+++ npc/csrc/include/npc_conf.h
@@ -2,30 +2,30 @@
 
 #define CONFIG_ISA_riscv 1
 //isa选择
-
+/*---------------------------------------------------------------------------------*/
 // #define CONFIG_TRACE 1
 // #define CONFIG_ITRACE 1
 // #define CONFIG_FTRACE 1
 // #define CONFIG_MTRACE 1
 // #define CONFIG_DIFFTEST 1
-
+//TRACE开关
+/*---------------------------------------------------------------------------------*/
 // #define TRACE_VCD 1
 //波形生成开关
-
+/*---------------------------------------------------------------------------------*/
 #define CONFIG_DEVICE 1
 #define DEVICE_HAS_KEYBOARD 1
 #define CONFIG_HAS_VGA 1
 #define CONFIG_VGA_SHOW_SCREEN 1
 #define CONFIG_VGA_SIZE_400x300 1
-
 //设备开关
-
+/*---------------------------------------------------------------------------------*/
 #define MTRACE_LOGBUF_SIZE 10
 #define ITRACE_LOGBUF_SIZE 10
 #define DTRACE_LOGBUF_SIZE 10
 #define ETRACE_LOGBUF_SIZE 10
 //trace最多开128个,因为我只给了128个
-
+/*---------------------------------------------------------------------------------*/
 #define CONFIG_PC_RESET_OFFSET 0x4
 //pc重置偏移
-
+/*---------------------------------------------------------------------------------*/
