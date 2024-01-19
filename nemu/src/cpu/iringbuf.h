afad03364a1a93a3dc4802c8667dc4fe67fa4e05
tracer-ysyx <tracer@ysyx.org>
2024-01-19 09:52:23 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:52:23 up  1:01,  2 users,  load average: 0.11, 0.17, 0.14

--- nemu/src/cpu/iringbuf.h
+++ nemu/src/cpu/iringbuf.h
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 
 #define MAX_STRING_LENGTH 128
-#define MTRACE_LOGBUF_SIZE 10
+#define MTRACE_LOGBUF_SIZE 128
 #define ITRACE_LOGBUF_SIZE 10
 #define DTRACE_LOGBUF_SIZE 10
 #define ETRACE_LOGBUF_SIZE 10
