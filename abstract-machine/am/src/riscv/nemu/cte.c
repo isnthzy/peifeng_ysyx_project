4a797f32261d80c53bcd43db792939cd01fa2bc0
tracer-ysyx <tracer@ysyx.org>
2024-01-20 21:16:15 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:16:15 up 18:57,  3 users,  load average: 0.33, 0.23, 0.19

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -11,6 +11,10 @@ Context* __am_irq_handle(Context *c) {
       case 0xb: //11 Environment call from M-mode
         if(c->GPR1==-1){
           ev.event=EVENT_YIELD;
+          // c->mepc+=4;
+        }else if(c->GPR1==1){
+          ev.event=EVENT_SYSCALL;
+          // c->mepc+=4;
         }else{ 
           //典中典之pa后边的部分被pa前面的实现坑惨，后边要求为识别的异常时间编号为4，但是经过检查发现
           //自己的输出为0，几番定位定位到这里给了0，照理说传入不对的数据都应该给0
