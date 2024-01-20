6f2f6bd6bf40dd414a59ca749a2425c5104f7cf4
tracer-ysyx <tracer@ysyx.org>
2024-01-20 21:24:57 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:24:57 up 19:06,  3 users,  load average: 0.13, 0.21, 0.18

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -5,7 +5,6 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-  printf("111111111111111111111111111%x\n",c->mcause);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
