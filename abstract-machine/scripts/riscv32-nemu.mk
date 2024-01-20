d53f274a54eb6cac6b5b13365457eebe155ae71a
tracer-ysyx <tracer@ysyx.org>
2024-01-20 23:04:14 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:04:14 up 20:08,  3 users,  load average: 0.38, 0.27, 0.24

--- abstract-machine/scripts/riscv32-nemu.mk
+++ abstract-machine/scripts/riscv32-nemu.mk
@@ -6,5 +6,5 @@ LDFLAGS       += -melf32lriscv                     # overwrite
 
 AM_SRCS += riscv/nemu/start.S \
            riscv/nemu/cte.c \
-          #  riscv/nemu/trap.S \
+           riscv/nemu/trap.S \
            riscv/nemu/vme.c
