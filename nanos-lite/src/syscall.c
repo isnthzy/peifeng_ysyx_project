1916e37d51917c71ed115c53fba3841d6aaf3f37
tracer-ysyx <tracer@ysyx.org>
2024-01-21 11:54:49 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:54:49 up 23:59,  3 users,  load average: 0.59, 0.38, 0.31

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,13 +1,23 @@
 #include <common.h>
 #include "syscall.h"
-
+void strace_log(int gpr){
+  char *syscall_name;
+  switch (gpr)
+  {
+  case SYS_exit:  syscall_name="SYS_exit" ; break;
+  case SYS_yield: syscall_name="SYS_yield"; break;
+  default:
+    panic("Unhandled syscall ID  = %d by strace", gpr);
+    break;
+  }
+  Log("Syscall:%s!!!",syscall_name);
+}
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
   switch (a[0]) {
     case SYS_exit:
-      halt(c->GPRx);
+      halt(c->GPRx); break;
     case SYS_yield:
       yield(); c->GPRx = 0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
