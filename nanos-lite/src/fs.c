ab775636fed06ea532f374fd95f170a81ec42c95
tracer-ysyx <tracer@ysyx.org>
2024-01-22 23:00:35 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:00:35 up 10:50,  1 user,  load average: 0.23, 0.21, 0.15

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -47,6 +47,7 @@ size_t fs_read(int fd, void *buf, size_t len){
   if(file_table[fd].open_offset+len>=file_table[fd].size){
     size_t realsize=file_table[fd].size-file_table[fd].open_offset;
     ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,realsize);
+    file_table[fd].open_offset+=realsize;
     return realsize;
   }
   ramdisk_read (buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
@@ -60,6 +61,7 @@ size_t fs_write(int fd, const void *buf, size_t len){
   if(file_table[fd].open_offset+len>=file_table[fd].size){
     size_t realsize=file_table[fd].size-file_table[fd].open_offset;
     ramdisk_write(buf,file_table[fd].disk_offset+file_table[fd].open_offset,realsize);
+    file_table[fd].open_offset+=realsize;
     return realsize;
   }
   ramdisk_write(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
