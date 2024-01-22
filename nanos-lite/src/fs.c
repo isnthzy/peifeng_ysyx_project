701640b0cbe03ca05ab0230367d3665bc7c3cab1
tracer-ysyx <tracer@ysyx.org>
2024-01-22 17:58:40 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:58:40 up  6:20,  1 user,  load average: 0.10, 0.14, 0.11

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -7,6 +7,7 @@ typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
+  size_t open_offset;
   ReadFn read;
   WriteFn write;
 } Finfo;
@@ -25,9 +26,9 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDIN]  = {"stdin", 0, 0, 0,invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, 0,invalid_read, invalid_write},
+  [FD_STDERR] = {"stderr", 0, 0, 0,invalid_read, invalid_write},
 #include "files.h"
 };
 int file_nums=sizeof(file_table)/sizeof(file_table[0]);
@@ -39,11 +40,21 @@ int fs_open(const char *pathname, int flags, int mode){
   }
   panic("file not found");
 }
-size_t fs_read(int fd, void *buf, size_t len,size_t size){
-  if(len+size>=file_table[fd].size) panic("读了过大的文件");
-  return ramdisk_read(buf,file_table[fd].disk_offset+len,size);
+size_t fs_read(int fd, void *buf, size_t len){
+  size_t size=file_table[fd].size-file_table[fd].open_offset;
+  ramdisk_read(buf,file_table[fd].disk_offset+len,size);
+  file_table[fd].open_offset+=len;
+  return len;
 }
 int fs_close(int fd){
+  return 0;
+}
+size_t fs_write(int fd, const void *buf, size_t len){
+  // if(len>=file_table[fd].size) panic("写入了过大的文件");
+  // return ramdisk_read(buf,file_table[fd].disk_offset+len,size);
+  return 0;
+}
+size_t fs_lseek(int fd, size_t offset, int whence){
   return 0;
 }
 
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,11 @@
 #include <common.h>
 #include <unistd.h>
 #include "syscall.h"
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
 void strace_log(int gpr){
   char *syscall_name;
   switch (gpr)
@@ -9,6 +14,10 @@ void strace_log(int gpr){
   case SYS_yield: syscall_name="SYS_yield"; break;
   case SYS_write: syscall_name="SYS_write"; break;
   case SYS_brk:   syscall_name="SYS_brk";   break;
+  case SYS_read:  syscall_name="SYS_read";  break; 
+  case SYS_open:  syscall_name="SYS_open";  break;
+  case SYS_lseek: syscall_name="SYS_lseek"; break;
+  case SYS_close: syscall_name="SYS_close"; break;  
   default:
     panic("Unhandled syscall ID  = %d by strace", gpr);
     break;
@@ -41,6 +50,18 @@ void do_syscall(Context *c) {
       break;
     case SYS_brk:
       c->GPRx=0; break;
+    case SYS_read:
+      c->GPRx=fs_read(a[1],(void *)a[2],a[3]);
+      break;
+    case SYS_open:
+      c->GPRx=fs_open((char *)a[1],a[2],a[3]);
+      break;
+    case SYS_lseek:
+      c->GPRx=fs_lseek(a[1],a[2],a[3]);
+      break;
+    case SYS_close:
+      c->GPRx=fs_close(a[1]);
+      break;  
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
