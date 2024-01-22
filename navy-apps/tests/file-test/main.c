a95a637ae4f7bd15f465d5664af42d398b7c81f7
tracer-ysyx <tracer@ysyx.org>
2024-01-22 21:06:41 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:06:41 up  9:16,  1 user,  load average: 0.18, 0.16, 0.14

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -21,6 +21,7 @@ int main() {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
   printf("pass test 2\n");
+  fseek(fp, 500 * 5, SEEK_SET);
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
