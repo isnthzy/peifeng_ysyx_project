82c7a3020f7a0e3ec7e7cca76300b767af477962
tracer-ysyx <tracer@ysyx.org>
2024-01-22 19:52:30 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:52:30 up  7:58,  1 user,  load average: 0.26, 0.16, 0.12

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -23,6 +23,7 @@ int main() {
   printf("pass test 2\n");
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
+    printf("i:%d n:%d",i,n);
     assert(n == i + 1);
   }
   printf("pass test 3\n");
