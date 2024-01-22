f09a1f9d6bfec5929bcca3c2b6ecdafa24db9aa4
tracer-ysyx <tracer@ysyx.org>
2024-01-22 16:18:37 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:18:37 up  4:40,  1 user,  load average: 0.14, 0.14, 0.09

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -39,9 +39,9 @@ int fs_open(const char *pathname, int flags, int mode){
   }
   panic("file not found");
 }
-size_t fs_read(int fd, void *buf, size_t len){
+size_t fs_read(int fd, void *buf, size_t len,size_t size){
   if(len>=file_table[fd].size) panic("读了过大的文件");
-  return ramdisk_read(buf,file_table[fd].disk_offset+len,file_table[fd].size-len);
+  return ramdisk_read(buf,file_table[fd].disk_offset+len,size);
 }
 int fs_close(int fd){
   return 0;
