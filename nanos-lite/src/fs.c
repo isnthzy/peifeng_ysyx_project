72a2ef8091aaa3fc9a3df8349b5f938d224c30cf
tracer-ysyx <tracer@ysyx.org>
2024-01-22 17:59:13 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:59:13 up  6:21,  1 user,  load average: 0.39, 0.20, 0.13

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -41,7 +41,7 @@ int fs_open(const char *pathname, int flags, int mode){
   panic("file not found");
 }
 size_t fs_read(int fd, void *buf, size_t len){
-  size_t size=file_table[fd].size-file_table[fd].open_offset;
+  size_t size=file_table[fd].size-file_table[fd].open_offset+1;
   ramdisk_read(buf,file_table[fd].disk_offset+len,size);
   file_table[fd].open_offset+=len;
   return len;
