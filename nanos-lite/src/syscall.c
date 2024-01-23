6e61ee9076cdde530fc712ac97ff973d641bba90
tracer-ysyx <tracer@ysyx.org>
2024-01-23 17:33:49 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:33:49 up 18:13,  1 user,  load average: 0.58, 0.20, 0.11

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -45,6 +45,7 @@ void do_syscall(Context *c) {
         char *write_buf=(char *)a[2];
         for(size_t i=0;i<a[3];i++){
           putch(write_buf[i]);
+          c->GPRx=a[3];
         } 
       }else c->GPRx=fs_write(a[1],(void *)a[2],a[3]);
       c->GPRx=0;
