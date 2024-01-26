840cea19c9183aa8853964a19395eba12f842099
wangxin-wsl2 <isnthzy@gmail.com>
2024-01-26 22:48:17 +0800

    实现把显存抽象成文件，居中的画布（好怪哦，我没用到初始化dev/fb, 显存的显示有点似乎偏移问题，再找找（先commit一发，一会对比一下是哪里出了问题（）））

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -5,6 +5,7 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t serial_write(const void *buf, size_t offset, size_t len);
 size_t events_read(void *buf, size_t offset, size_t len);
 size_t dispinfo_read(void *buf, size_t offset, size_t len);
+size_t fb_write(const void *buf, size_t offset, size_t len);
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
@@ -34,7 +35,7 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, 0,invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, 0,invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, 0,invalid_read, serial_write},
-  [FD_FB]     = {"/dev/fb", 0, 0, 0,invalid_read, invalid_write},
+  [FD_FB]     = {"/dev/fb", 0, 0, 0,invalid_read, fb_write},
   [FD_EVENTS] = {"/dev/events", 0, 0, 0,events_read, invalid_write},
   [FD_DISPINFO]={"/proc/dispinfo", 0, 0, 0,dispinfo_read, invalid_write},
 #include "files.h"
@@ -51,10 +52,13 @@ int fs_open(const char *pathname, int flags, int mode){
 size_t fs_read(int fd, void *buf, size_t len){
   if(fd==FD_STDIN){
     file_table[fd].read(buf,0,len);
+    //open_offset不需要更新
   }else if(fd==FD_EVENTS){
     len=file_table[fd].read(buf,0,len);
+    //open_offset不需要更新
   }else if(fd==FD_DISPINFO){
     len=file_table[fd].read(buf,0,len);
+    //open_offset不需要更新
   }else{
     if(file_table[fd].open_offset>=file_table[fd].size) return 0;
     if(file_table[fd].open_offset+len>=file_table[fd].size){
@@ -74,6 +78,9 @@ int fs_close(int fd){
 size_t fs_write(int fd, const void *buf, size_t len){
   if(fd==FD_STDOUT||fd==FD_STDERR){
     file_table[fd].write(buf,0,len);
+    //open_offset不需要更新
+  }else if(fd==FD_FB){
+    file_table[fd].write(buf,file_table[fd].open_offset,len);
     file_table[fd].open_offset+=len;
   }else{
     if(file_table[fd].open_offset+len>=file_table[fd].size){
@@ -103,11 +110,14 @@ size_t fs_lseek(int fd, size_t offset, int whence){
     panic("Invalid whence");
     break;
   }
+  printf("offset:%d\n",file_table[fd].open_offset);
   return file_table[fd].open_offset;
 }
 
 
 void init_fs() {
+  // fb_buf=malloc
+
   // int num_files = sizeof(file_table) / sizeof(file_table[0]);
 
   // for (int i = 0; i < num_files; i++) {
