e39cfc688270fbcdeba481f4be7762c53adc3704
tracer-ysyx <tracer@ysyx.org>
2024-02-04 13:27:32 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:27:32 up  8:15,  1 user,  load average: 0.26, 0.40, 0.33

--- nemu/src/cpu/iringbuf.h
+++ nemu/src/cpu/iringbuf.h
@@ -8,7 +8,7 @@
 #define DTRACE_LOGBUF_SIZE 10
 #define ETRACE_LOGBUF_SIZE 10
 typedef struct {
-    char buffer[128][MAX_STRING_LENGTH];
+    char buffer[1280][MAX_STRING_LENGTH];
     int head;     // 指向队列头部的指针
     int tail;     // 指向队列尾部的指针
     int size;     // 缓冲区有多大
