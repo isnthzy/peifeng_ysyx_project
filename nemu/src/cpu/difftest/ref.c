c291d12d275b3235cfdd2252da1f99ecaf09ea22
tracer-ysyx <tracer@ysyx.org>
2024-02-06 20:48:33 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:48:33 up 23:40,  1 user,  load average: 0.08, 0.20, 0.21

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -30,7 +30,7 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 __EXPORT void difftest_regcpy(void *dut, bool direction,vaddr_t skip_target) {
   CPU_state* dut_t=dut; 
   if (direction == DIFFTEST_TO_REF) {
-    for(int i=0;i<32;i++){
+    for(int i=0;i<MUXDEF(CONFIG_RVE, 16, 32);i++){
       cpu.gpr[i]=dut_t->gpr[i];
     }
     if(skip_target==0) cpu.pc=dut_t->pc;
@@ -41,7 +41,7 @@ __EXPORT void difftest_regcpy(void *dut, bool direction,vaddr_t skip_target) {
     cpu.mcause=dut_t->mcause;
 
   }else{
-    for(int i=0;i<32;i++){
+    for(int i=0;i<MUXDEF(CONFIG_RVE, 16, 32);i++){
       dut_t->gpr[i]=cpu.gpr[i];
     }
     dut_t->pc=cpu.pc;
