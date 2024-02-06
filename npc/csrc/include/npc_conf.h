bbc59f76e10d84ce2ca72b4457027300550f4955
tracer-ysyx <tracer@ysyx.org>
2024-02-06 21:59:21 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:59:21 up 1 day, 51 min,  1 user,  load average: 0.23, 0.24, 0.26

--- npc/csrc/include/npc_conf.h
+++ npc/csrc/include/npc_conf.h
@@ -21,12 +21,12 @@
 #define CONFIG_VGA_SIZE_400x300 1
 //设备开关
 /*---------------------------------------------------------------------------------*/
-#define MTRACE_LOGBUF_SIZE 1024
+#define MTRACE_LOGBUF_SIZE 10
 #define ITRACE_LOGBUF_SIZE 10
 #define DTRACE_LOGBUF_SIZE 10
 #define ETRACE_LOGBUF_SIZE 10
-#define IRINGBUF_MAX_NUM 1024
-//trace最多开1024个,因为我只给了1024个
+#define IRINGBUF_MAX_NUM 128
+//trace最多开多少IRINGBUF_MAX_NUM给多少
 /*---------------------------------------------------------------------------------*/
 #define CONFIG_PC_RESET_OFFSET 0x4
 //pc重置偏移
