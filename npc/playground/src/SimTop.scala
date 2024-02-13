e27cbdb267fad53b00b6fb2c8fc457db172a2479
tracer-ysyx <tracer@ysyx.org>
2024-02-13 14:13:12 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:13:12 up  2:49,  1 user,  load average: 0.24, 0.22, 0.26

--- npc/playground/src/SimTop.scala
+++ npc/playground/src/SimTop.scala
@@ -37,7 +37,11 @@ object StageConnect {
   def apply[T <: Data](left: DecoupledIO[T], right: DecoupledIO[T]) = {
     val arch = "single"
     // 为展示抽象的思想, 此处代码省略了若干细节
-    if      (arch == "single")   { right.bits := left.bits }
+    if      (arch == "single")   { 
+      left.ready := right.ready
+      left.valid := right.valid 
+      right.bits := left.bits 
+    }
     else if (arch == "multi")    { right <> left }
     // else if (arch == "pipeline") { right <> RegEnable(left, left.fire) }
     // else if (arch == "ooo")      { right <> Queue(left, 16) }
