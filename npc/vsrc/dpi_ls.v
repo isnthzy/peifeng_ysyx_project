4e81f779a86093fa331a944b16db38c01c44b567
tracer-ysyx <tracer@ysyx.org>
2024-02-14 10:48:09 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:48:09 up  1:27,  1 user,  load average: 0.34, 0.19, 0.18

---
+++ npc/vsrc/dpi_ls.v
@@ -0,0 +1,31 @@
+
+import "DPI-C" function void pmem_read (input int raddr, output int rdata);
+import "DPI-C" function void pmem_write(input int waddr, input  int wdata, input byte wmask);
+module dpi_ls(
+   input        clock,
+   input        reset,
+   input        ld_wen,
+   input        st_wen,
+   input [31:0] raddr,
+   output[31:0] rdata,
+   input [ 7:0] wmask,
+   input [31:0] waddr,
+   input [31:0] wdata
+);
+ 
+always_latch @(*) begin
+  if(~reset)begin
+    if(ld_wen&&clock) begin
+      pmem_read (raddr,rdata);
+    end
+    else begin
+      rdata[31:0]=0;
+    end
+
+    if(st_wen&&clock) begin
+      pmem_write(waddr,wdata,wmask);
+    end
+  end
+ end
+endmodule
+    
