c56be84a6ba0e4a8864dc5320d15c601c5af5008
tracer-ysyx <tracer@ysyx.org>
2024-02-16 02:54:48 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:54:48 up  7:15,  1 user,  load average: 0.51, 0.28, 0.20

--- npc/vsrc/IF_stage.sv
+++ npc/vsrc/IF_stage.sv
@@ -17,29 +17,29 @@ module IF_stage(	// @[<stdin>:11:3]
   wire        if_ready_go = 1'h1;	// @[playground/src/IF_stage.scala:14:33, :16:14]
   reg         if_valid;	// @[playground/src/IF_stage.scala:13:33]
   wire        if_allowin = ~if_valid | if_ready_go & IF_IO_ready;	// @[playground/src/IF_stage.scala:13:33, :14:33, :15:32, :17:{16,26,41}]
-  reg  [31:0] REGpc;	// @[playground/src/IF_stage.scala:26:24]
-  wire [31:0] snpc = REGpc + 32'h4;	// @[playground/src/IF_stage.scala:26:24, :27:31, :32:18]
-  wire [31:0] dnpc = IF_epc_bus_epc_wen ? IF_epc_bus_csr_epc : IF_br_bus_dnpc;	// @[playground/src/IF_stage.scala:28:31, :33:15]
-  wire [31:0] nextpc = IF_br_bus_is_jump | IF_epc_bus_epc_wen ? dnpc : snpc;	// @[playground/src/IF_stage.scala:27:31, :28:31, :29:31, :34:{15,33}]
+  reg  [31:0] REGpc;	// @[playground/src/IF_stage.scala:24:24]
+  wire [31:0] snpc = REGpc + 32'h4;	// @[playground/src/IF_stage.scala:24:24, :25:31, :30:18]
+  wire [31:0] dnpc = IF_epc_bus_epc_wen ? IF_epc_bus_csr_epc : IF_br_bus_dnpc;	// @[playground/src/IF_stage.scala:26:31, :31:15]
+  wire [31:0] nextpc = IF_br_bus_is_jump | IF_epc_bus_epc_wen ? dnpc : snpc;	// @[playground/src/IF_stage.scala:25:31, :26:31, :27:31, :32:{15,33}]
   always @(posedge clock) begin	// @[<stdin>:12:11]
     if (reset) begin	// @[<stdin>:12:11]
       if_valid <= 1'h0;	// @[playground/src/IF_stage.scala:13:33]
-      REGpc <= 32'h7FFFFFFC;	// @[playground/src/IF_stage.scala:26:24]
+      REGpc <= 32'h7FFFFFFC;	// @[playground/src/IF_stage.scala:24:24]
     end
     else begin	// @[<stdin>:12:11]
-      if_valid <= if_allowin;	// @[playground/src/IF_stage.scala:13:33, :15:32]
-      REGpc <= nextpc;	// @[playground/src/IF_stage.scala:26:24, :29:31]
+      if_valid <= if_allowin | if_valid;	// @[playground/src/IF_stage.scala:13:33, :15:32, :18:19, :19:13]
+      REGpc <= nextpc;	// @[playground/src/IF_stage.scala:24:24, :27:31]
     end
   end // always @(posedge)
-  read_inst Fetch (	// @[playground/src/IF_stage.scala:30:23]
+  read_inst Fetch (	// @[playground/src/IF_stage.scala:28:23]
     .clock  (clock),
     .reset  (reset),
-    .nextpc (nextpc),	// @[playground/src/IF_stage.scala:29:31]
-    .pc     (REGpc),	// @[playground/src/IF_stage.scala:26:24]
+    .nextpc (nextpc),	// @[playground/src/IF_stage.scala:27:31]
+    .pc     (REGpc),	// @[playground/src/IF_stage.scala:24:24]
     .inst   (IF_IO_bits_inst)
   );
-  assign IF_IO_valid = ~IF_flush & if_valid & if_ready_go;	// @[<stdin>:11:3, playground/src/IF_stage.scala:13:33, :14:33, :23:19]
-  assign IF_IO_bits_nextpc = nextpc;	// @[<stdin>:11:3, playground/src/IF_stage.scala:29:31]
-  assign IF_IO_bits_pc = REGpc;	// @[<stdin>:11:3, playground/src/IF_stage.scala:26:24]
+  assign IF_IO_valid = ~IF_flush & if_valid & if_ready_go;	// @[<stdin>:11:3, playground/src/IF_stage.scala:13:33, :14:33, :21:19]
+  assign IF_IO_bits_nextpc = nextpc;	// @[<stdin>:11:3, playground/src/IF_stage.scala:27:31]
+  assign IF_IO_bits_pc = REGpc;	// @[<stdin>:11:3, playground/src/IF_stage.scala:24:24]
 endmodule
 
