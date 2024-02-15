ccf2ff6cedc95e47b99f86e401b55a296b6f7fc1
tracer-ysyx <tracer@ysyx.org>
2024-02-15 14:45:27 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:45:27 up 16:13,  3 users,  load average: 0.14, 0.37, 0.32

--- npc/playground/src/ID_stage.scala
+++ npc/playground/src/ID_stage.scala
@@ -19,7 +19,7 @@ class ID_stage extends Module {
   id_ready_go:=true.B
   ID.IO.ready := !id_valid || id_ready_go && ID.to_ex.ready
   when(ID.IO.ready){
-    id_valid:=ID.IO.ready
+    id_valid:=ID.IO.valid
   }
   ID.to_ex.valid:=Mux(ID.flush, false.B ,id_valid && id_ready_go)
 
