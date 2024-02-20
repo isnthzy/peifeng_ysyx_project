223039f480ff708116724301aca4a9b26b288ffe
tracer-ysyx <tracer@ysyx.org>
2024-02-20 16:52:40 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:52:40 up  1:35,  1 user,  load average: 0.21, 0.07, 0.07

--- npc/csrc/include/npc_conf.h
+++ npc/csrc/include/npc_conf.h
@@ -26,9 +26,9 @@
 /*---------------------------------------------------------------------------------*/
 
 #define CONFIG_DEVICE 1
-// #define DEVICE_HAS_KEYBOARD 1
-// #define CONFIG_HAS_VGA 1
-// #define CONFIG_VGA_SIZE_400x300 1
+#define DEVICE_HAS_KEYBOARD 1
+#define CONFIG_HAS_VGA 1
+#define CONFIG_VGA_SIZE_400x300 1
 //设备开关,默认串口输出打开
 
 /*---------------------------------------------------------------------------------*/
