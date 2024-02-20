1fcde5162e0f2fe202896386ee24fa7237219863
tracer-ysyx <tracer@ysyx.org>
2024-02-20 21:27:14 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:27:14 up 4 min,  1 user,  load average: 0.48, 0.35, 0.14

--- npc/csrc/include/npc_conf.h
+++ npc/csrc/include/npc_conf.h
@@ -5,8 +5,8 @@
 
 /*---------------------------------------------------------------------------------*/
 
-// #define CONFIG_TRACE 1
-// #define CONFIG_ITRACE 1
+#define CONFIG_TRACE 1
+#define CONFIG_ITRACE 1
 // #define CONFIG_FTRACE 1
 // #define CONFIG_MTRACE 1
 #define CONFIG_DIFFTEST 1
