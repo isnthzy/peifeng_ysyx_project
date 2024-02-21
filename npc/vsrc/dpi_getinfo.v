f85959c240e25d4222f73a67996d8f5df7beff27
tracer-ysyx <tracer@ysyx.org>
2024-02-21 17:32:38 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:32:38 up 45 min,  1 user,  load average: 0.65, 0.45, 0.27

---
+++ npc/vsrc/dpi_getinfo.v
@@ -0,0 +1,19 @@
+
+import "DPI-C" function void get_info(input int pc,input int nextpc,input int inst,input bit dpi_valid);
+module dpi_getinfo(
+    input        clock,
+    input        reset,
+    input        dpi_valid,
+    input [31:0] pc,
+    input [31:0] nextpc,
+    input [31:0] inst
+    
+);
+ always @(*)begin
+   if(~reset)begin
+     get_info(pc,nextpc,inst,dpi_valid);
+     //有可能因为阻塞等传递了无效的数据，需要在仿真环境中处理这些情况
+   end
+  end
+endmodule
+    
