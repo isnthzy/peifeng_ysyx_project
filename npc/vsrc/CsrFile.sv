3f92051b523bf095ec5a13259eca6c4b0413edfe
tracer-ysyx <tracer@ysyx.org>
2024-02-22 21:49:59 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:49:59 up 14:26,  1 user,  load average: 0.37, 0.30, 0.30

--- npc/vsrc/CsrFile.sv
+++ npc/vsrc/CsrFile.sv
@@ -17,20 +17,17 @@ module CsrFile(	// @[<stdin>:816:3]
   reg  [31:0] mtvec;	// @[playground/src/Csr.scala:37:22]
   reg  [31:0] mstatus;	// @[playground/src/Csr.scala:38:22]
   reg  [31:0] mepc;	// @[playground/src/Csr.scala:39:22]
-  reg  [31:0] mcause;	// @[playground/src/Csr.scala:40:22]
   reg  [31:0] mtval;	// @[playground/src/Csr.scala:41:22]
   wire        _GEN = io_csr_waddr == 12'h305;	// @[playground/src/Csr.scala:47:38, :71:22]
   wire        _GEN_0 = io_csr_waddr == 12'h300;	// @[playground/src/Csr.scala:47:38, :73:27]
   wire        _GEN_1 = io_csr_waddr == 12'h341;	// @[playground/src/Csr.scala:47:38, :74:27]
-  wire        _GEN_2 = io_csr_waddr == 12'h342;	// @[playground/src/Csr.scala:47:38, :75:27]
-  wire        _GEN_3 = io_csr_waddr == 12'h343;	// @[playground/src/Csr.scala:47:38, :76:27]
-  wire        _GEN_4 = _GEN | _GEN_0 | _GEN_1 | _GEN_2;	// @[playground/src/Csr.scala:41:22, :71:{22,35}, :73:{27,42}, :74:{27,42}, :75:{27,42}, :76:42]
+  wire        _GEN_2 = io_csr_waddr == 12'h343;	// @[playground/src/Csr.scala:47:38, :76:27]
+  wire        _GEN_3 = _GEN | _GEN_0 | _GEN_1 | io_csr_waddr == 12'h342;	// @[playground/src/Csr.scala:41:22, :47:38, :71:{22,35}, :73:{27,42}, :74:{27,42}, :75:{27,42}, :76:42]
   always @(posedge clock) begin	// @[<stdin>:817:11]
     if (reset) begin	// @[<stdin>:817:11]
       mtvec <= 32'h0;	// @[playground/src/Csr.scala:37:22]
       mstatus <= 32'h1800;	// @[playground/src/Csr.scala:38:22]
       mepc <= 32'h0;	// @[playground/src/Csr.scala:37:22, :39:22]
-      mcause <= 32'h0;	// @[playground/src/Csr.scala:37:22, :40:22]
       mtval <= 32'h0;	// @[playground/src/Csr.scala:37:22, :41:22]
     end
     else begin	// @[<stdin>:817:11]
@@ -46,9 +43,7 @@ module CsrFile(	// @[<stdin>:816:3]
       end
       else	// @[playground/src/Csr.scala:61:20, :70:19, :71:35]
         mepc <= io_csr_wdata;	// @[playground/src/Csr.scala:39:22]
-      if (io_csr_wen & ~(_GEN | _GEN_0 | _GEN_1) & _GEN_2 | io_ecpt_wen)	// @[playground/src/Csr.scala:40:22, :61:20, :63:11, :70:19, :71:{22,35}, :73:{27,42}, :74:{27,42}, :75:{27,42,50}]
-        mcause <= 32'hB;	// @[playground/src/Csr.scala:22:12, :40:22]
-      if (~io_csr_wen | _GEN_4 | ~_GEN_3) begin	// @[playground/src/Csr.scala:38:22, :41:22, :70:19, :71:35, :73:42, :74:42, :75:42, :76:{27,42}]
+      if (~io_csr_wen | _GEN_3 | ~_GEN_2) begin	// @[playground/src/Csr.scala:38:22, :41:22, :70:19, :71:35, :73:42, :74:42, :75:42, :76:{27,42}]
       end
       else	// @[playground/src/Csr.scala:41:22, :70:19, :71:35]
         mtval <= io_csr_wdata;	// @[playground/src/Csr.scala:41:22]
@@ -58,18 +53,18 @@ module CsrFile(	// @[<stdin>:816:3]
     .clock      (clock),
     .reset      (reset),
     .csr_valid  (|io_csr_cmd),	// @[playground/src/Csr.scala:84:36]
-    .assert_wen (io_csr_wen & ~_GEN_4 & ~_GEN_3)	// @[playground/src/Csr.scala:41:22, :43:33, :70:19, :71:35, :73:42, :74:42, :75:42, :76:{27,42}, :77:31]
+    .assert_wen (io_csr_wen & ~_GEN_3 & ~_GEN_2)	// @[playground/src/Csr.scala:41:22, :43:33, :70:19, :71:35, :73:42, :74:42, :75:42, :76:{27,42}, :77:31]
   );
   assign io_out =
     io_csr_raddr == 12'h343
       ? mtval
       : io_csr_raddr == 12'h342
-          ? mcause
+          ? 32'hB
           : io_csr_raddr == 12'h341
               ? mepc
               : io_csr_raddr == 12'h300
                   ? mstatus
-                  : io_csr_raddr == 12'h305 ? mtvec : 32'h0;	// @[<stdin>:816:3, playground/src/Csr.scala:37:22, :38:22, :39:22, :40:22, :41:22, :47:38]
+                  : io_csr_raddr == 12'h305 ? mtvec : 32'h0;	// @[<stdin>:816:3, playground/src/Csr.scala:22:12, :37:22, :38:22, :39:22, :41:22, :47:38]
   assign io_global_mtvec = io_csr_cmd == 5'h4 ? mtvec : 32'h0;	// @[<stdin>:816:3, playground/src/Csr.scala:37:22, :44:18, :57:{18,31}, :58:20]
   assign io_global_mepc = io_csr_cmd == 5'h3 ? mepc : 32'h0;	// @[<stdin>:816:3, playground/src/Csr.scala:37:22, :39:22, :45:17, :66:{18,30}, :67:19]
 endmodule
