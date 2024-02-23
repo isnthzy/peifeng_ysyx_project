d2c0e92a4f5957cbba49ed0e99895f83657386df
tracer-ysyx <tracer@ysyx.org>
2024-02-23 23:11:01 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:11:01 up  1:28,  1 user,  load average: 0.29, 0.37, 0.30

--- npc/playground/src/WB_stage.scala
+++ npc/playground/src/WB_stage.scala
@@ -43,7 +43,7 @@ class WB_stage extends Module {
   DPI_stage.DPI.is_jal:=WB.IO.bits.dpic_bundle.ex_is_jal
   DPI_stage.DPI.is_ret:=WB.IO.bits.dpic_bundle.ex_is_ret
   DPI_stage.DPI.is_rd0:=WB.IO.bits.dpic_bundle.ex_is_rd0
-  DPI_stage.DPI.is_ebreak:=(WB.IO.bits.csr_cmd===CSR.MRET)
+  DPI_stage.DPI.is_ebreak:=(WB.IO.bits.csr_cmd===CSR.BREAK)
   DPI_stage.DPI.ret_reg_data:=WB.IO.bits.result
   // DPI_stage.DPI.csr_commit<>WB.IO.bits.csr_commit
 }
