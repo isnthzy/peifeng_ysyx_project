76258447baf197809f45239feebfb699165491a1
tracer-ysyx <tracer@ysyx.org>
2024-02-27 06:05:09 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  06:05:09 up  7:50,  1 user,  load average: 1.17, 0.79, 0.45

--- npc/playground/src/AXI4Lite_Bundle.scala
+++ npc/playground/src/AXI4Lite_Bundle.scala
@@ -4,7 +4,7 @@ import config.Configs._
 
 
 
-class Axi4LiteMaster extends Bundle{
+class Axi4LiteSlave extends Bundle{
   val ar=Flipped(Decoupled(new AxiAddressBundle()))
   val r=Decoupled(new AxiReadDataBundle())
   val aw=Flipped(Decoupled(new AxiAddressBundle()))
@@ -13,13 +13,13 @@ class Axi4LiteMaster extends Bundle{
 }
 
 
-class Axi4LiteSlave extends Bundle{
-  val ar=Decoupled(new AxiAddressBundle())
-  val r=Flipped(Decoupled(new AxiReadDataBundle()))
-  val aw=Decoupled(new AxiAddressBundle())
-  val w=Decoupled(new AxiWriteDataBundle())
-  val b=Flipped(Decoupled(new AxiWriteResponseBundle()))
-}
+// class Axi4LiteMaster extends Bundle{
+//   val ar=Decoupled(new AxiAddressBundle())
+//   val r=Flipped(Decoupled(new AxiReadDataBundle()))
+//   val aw=Decoupled(new AxiAddressBundle())
+//   val w=Decoupled(new AxiWriteDataBundle())
+//   val b=Flipped(Decoupled(new AxiWriteResponseBundle()))
+// }
 
 class AxiWriteDataBundle extends Bundle {
   val data = UInt(DATA_WIDTH.W)
