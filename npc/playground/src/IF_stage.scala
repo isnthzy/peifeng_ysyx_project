a550aa294a1bf191bfd1719805d5599259738242
tracer-ysyx <tracer@ysyx.org>
2024-02-28 08:15:46 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:15:46 up 14:35,  1 user,  load average: 0.77, 0.38, 0.27

--- npc/playground/src/IF_stage.scala
+++ npc/playground/src/IF_stage.scala
@@ -50,7 +50,7 @@ class IF_stage extends Module {
   
   val DoAddrReadReg=RegInit(false.B)
   DoAddrReadReg:= if_ready_go && ~reset.asBool
-  IF.ar.valid:=DoAddrReadReg
+  IF.ar.valid:=if_ready_go && ~reset.asBool
   IF.ar.bits.addr:=if_nextpc
   IF.ar.bits.prot:=0.U
   IF.r.ready:=if_valid
