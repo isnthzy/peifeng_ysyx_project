53a3dfb0c1fe6f228394871f5c01750703643484
tracer-ysyx <tracer@ysyx.org>
2024-02-29 16:59:23 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:59:23 up 13 min,  1 user,  load average: 0.05, 0.13, 0.13

--- npc/playground/src/IF_stage.scala
+++ npc/playground/src/IF_stage.scala
@@ -13,12 +13,12 @@ class IF_stage extends Module {
   val if_flush=dontTouch(Wire(Bool()))
   if_flush:=IF.for_ex.flush || IF.for_id.flush
 
-  val ResetN=dontTouch(RegInit(false.B))
-  ResetN:=true.B
+  val ResetNReg=dontTouch(RegInit(false.B))
+  ResetNReg:=true.B
   val if_ready_go=dontTouch(Wire(Bool()))
   if_ready_go:=IF.to_id.ready
 
-  IF.to_id.valid:=Mux(if_flush, false.B , ~reset.asBool && if_ready_go)
+  IF.to_id.valid:=Mux(if_flush, false.B , ResetNReg && if_ready_go)
 
 
   val br=Wire(new br_bus())
