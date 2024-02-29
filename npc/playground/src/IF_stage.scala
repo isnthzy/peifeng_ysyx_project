f636141b96a5e09b81c6db7f4fecbd086f0dc730
tracer-ysyx <tracer@ysyx.org>
2024-02-29 16:20:23 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:20:23 up  1:40,  1 user,  load average: 0.63, 0.27, 0.16

--- npc/playground/src/IF_stage.scala
+++ npc/playground/src/IF_stage.scala
@@ -13,13 +13,12 @@ class IF_stage extends Module {
   val if_flush=dontTouch(Wire(Bool()))
   if_flush:=IF.for_ex.flush || IF.for_id.flush
 
-  val if_valid=dontTouch(RegInit(false.B))
+  val ResetN=dontTouch(RegInit(false.B))
+  ResetN:=true.B
   val if_ready_go=dontTouch(Wire(Bool()))
   if_ready_go:=IF.to_id.ready
-  // when(if_ready_go){
-    if_valid:=true.B
-  // }
-  IF.to_id.valid:=Mux(if_flush, false.B , if_valid && if_ready_go)
+
+  IF.to_id.valid:=Mux(if_flush, false.B , ResetN && if_ready_go)
 
 
   val br=Wire(new br_bus())
