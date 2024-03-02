0c0c44a5e731b4787c144fb664d011886ee11420
tracer-ysyx <tracer@ysyx.org>
2024-03-02 10:10:52 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:10:52 up 54 min,  1 user,  load average: 1.45, 0.77, 0.40

---
+++ npc/vsrc/dpic/DpiCsrCommit.v
@@ -0,0 +1,22 @@
+
+import "DPI-C" function void sync_csrfile_regs(input int waddr,input int wdata);
+import "DPI-C" function void sync_csr_exception_regs(input int mcause_in,input int pc_wb);
+module Dpi_CsrCommit(
+    input        clock,
+    input        reset,
+    input        dpi_valid,
+    input        csr_wen,
+    input [31:0] waddr,
+    input [31:0] wdata,
+    input        exception_wen,
+    input [31:0] mcause_in,
+    input [31:0] pc_wb
+);
+ always @(posedge clock)begin
+   if(~reset)begin
+     if(csr_wen&&dpi_valid) sync_csrfile_regs(waddr,wdata);
+     if(exception_wen&&dpi_valid) sync_csr_exception_regs(mcause_in,pc_wb);
+   end
+  end
+endmodule
+    
