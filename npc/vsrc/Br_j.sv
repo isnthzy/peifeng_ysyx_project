2702fae3741fccf75e1fb567631404c90a68f251
tracer-ysyx <tracer@ysyx.org>
2024-03-02 14:23:09 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:23:09 up  4:37,  1 user,  load average: 0.64, 0.56, 0.36

--- npc/vsrc/Br_j.sv
+++ npc/vsrc/Br_j.sv
@@ -10,7 +10,7 @@
   `endif // PRINTF_COND
 `endif // not def PRINTF_COND_
 
-module Br_j(	// @[<stdin>:950:3]
+module Br_j(	// @[<stdin>:954:3]
   input  [3:0]  io_br_type,	// @[playground/src/Br_cond.scala:60:12]
   input  [31:0] io_src1,	// @[playground/src/Br_cond.scala:60:12]
                 io_src2,	// @[playground/src/Br_cond.scala:60:12]
@@ -19,8 +19,8 @@ module Br_j(	// @[<stdin>:950:3]
 );
 
   wire [31:0] _result_T = io_src1 + io_src2;	// @[playground/src/Br_cond.scala:71:21]
-  assign io_taken = io_br_type == 4'h7 | io_br_type == 4'h8;	// @[<stdin>:950:3, playground/src/Br_cond.scala:68:27, :69:{13,26}]
+  assign io_taken = io_br_type == 4'h7 | io_br_type == 4'h8;	// @[<stdin>:954:3, playground/src/Br_cond.scala:68:27, :69:{13,26}]
   assign io_target =
-    io_br_type == 4'h8 ? {_result_T[31:1], 1'h0} : io_br_type == 4'h7 ? _result_T : 32'h0;	// @[<stdin>:950:3, playground/src/Br_cond.scala:68:27, :69:26, :71:21, :73:39, :76:{18,25}]
+    io_br_type == 4'h8 ? {_result_T[31:1], 1'h0} : io_br_type == 4'h7 ? _result_T : 32'h0;	// @[<stdin>:954:3, playground/src/Br_cond.scala:68:27, :69:26, :71:21, :73:39, :76:{18,25}]
 endmodule
 
