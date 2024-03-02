e9441d85cdd13e3ded88951aa67ca3b62c53e0a5
tracer-ysyx <tracer@ysyx.org>
2024-03-02 14:24:17 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:24:17 up  4:38,  1 user,  load average: 0.52, 0.52, 0.36

--- npc/playground/src/PreIF.scala
+++ npc/playground/src/PreIF.scala
@@ -88,10 +88,10 @@ class PreIF_s extends Module {
 
   //----------------------AXI4Lite-----------------------------
 
-  val is_fire=PreIF.to_if.fire
-  dontTouch(is_fire)
-  when(is_fire&&resetn){ //if级控制不用if_valid信号（if级有点特殊）
-    printf("PreIF: pc=%x, nextpc=%x , is_fire=%d\n", PreIF_pc, PreIF_nextpc,is_fire)
+ 
+  dontTouch(PreIF.to_if.fire)
+  when(PreIF.to_if.fire&&resetn){ //if级控制不用if_valid信号（if级有点特殊）
+    printf("PreIF: pc=%x, nextpc=%x , is_fire=%d\n", PreIF_pc, PreIF_nextpc,PreIF.to_if.fire)
     PreIF_pc := PreIF_nextpc //reg类型，更新慢一拍
   }
   //如果遇到阻塞情况，那么if级也要发生阻塞
