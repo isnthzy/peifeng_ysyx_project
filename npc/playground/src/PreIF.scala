ccd042a936eb6901705d1f8f90fc6fca889cf4c9
tracer-ysyx <tracer@ysyx.org>
2024-03-03 17:25:29 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:25:29 up  3:14,  1 user,  load average: 0.32, 0.25, 0.17

--- npc/playground/src/PreIF.scala
+++ npc/playground/src/PreIF.scala
@@ -21,10 +21,10 @@ class PreIF_s extends Module {
   val br=Wire(new br_bus())
   
 
-  // br.nextpc_stall:=(PreIF.for_id.Br_J.nextpc_stall 
-  //                || PreIF.for_ex.Br_B.nextpc_stall
-  //                || PreIF.for_ex.epc.nextpc_stall )
-  br.nextpc_stall:=false.B
+  br.nextpc_stall:=(PreIF.for_id.Br_J.nextpc_stall 
+                 || PreIF.for_ex.Br_B.nextpc_stall
+                 || PreIF.for_ex.epc.nextpc_stall )
+  // br.nextpc_stall:=false.B
   fetch_wen:=PreIF.to_if.ready && !br.nextpc_stall
 
   PreIF_flush:=PreIF.for_ex.flush || PreIF.for_id.flush
@@ -93,7 +93,7 @@ class PreIF_s extends Module {
   //----------------------AXI4Lite-----------------------------
 
  
-  when(PreIF_ready_go){ //if级控制不用if_valid信号（if级有点特殊）
+  when(PreIF_ready_go || br.nextpc_stall){ //if级控制不用if_valid信号（if级有点特殊）
     // printf("PreIF: pc=%x, nextpc=%x , is_fire=%d\n", PreIF_pc, PreIF_nextpc,PreIF.to_if.fire)
     PreIF_pc := PreIF_nextpc //reg类型，更新慢一拍
   }
