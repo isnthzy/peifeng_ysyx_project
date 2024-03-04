f002db6e33573cd8389f8d1d4316dfb0d1b679e2
tracer-ysyx <tracer@ysyx.org>
2024-03-04 20:45:01 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:45:01 up  3:22,  1 user,  load average: 0.85, 0.24, 0.18

--- npc/vsrc/Axi4Lite_Sram_Mem.sv
+++ npc/vsrc/Axi4Lite_Sram_Mem.sv
@@ -29,33 +29,36 @@ module Axi4Lite_Sram_Mem(	// @[<stdin>:1672:3]
   wire _dpi_sram_io_req_T = _io_ar_ready_output & io_ar_valid;	// @[<stdin>:1672:3, src/main/scala/chisel3/util/Decoupled.scala:52:35]
   wire _dpi_sram_io_wr_T = _io_aw_ready_output & io_aw_valid;	// @[<stdin>:1672:3, src/main/scala/chisel3/util/Decoupled.scala:52:35]
   wire _dpi_sram_io_wr_T_1 = _io_w_ready_output & io_w_valid;	// @[<stdin>:1672:3, src/main/scala/chisel3/util/Decoupled.scala:52:35]
-  reg  readDataValidReg;	// @[playground/src/Sram.scala:17:31]
-  reg  writeRespValidReg;	// @[playground/src/Sram.scala:35:32]
+  reg  readDataValidReg;	// @[playground/src/Sram.scala:18:31]
+  reg  writeRespValidReg;	// @[playground/src/Sram.scala:36:32]
   always @(posedge clock) begin	// @[<stdin>:1673:11]
     if (reset) begin	// @[<stdin>:1673:11]
-      readDataValidReg <= 1'h0;	// @[playground/src/Sram.scala:17:31]
-      writeRespValidReg <= 1'h0;	// @[playground/src/Sram.scala:17:31, :35:32]
+      readDataValidReg <= 1'h0;	// @[playground/src/Sram.scala:14:26, :18:31]
+      writeRespValidReg <= 1'h0;	// @[playground/src/Sram.scala:14:26, :36:32]
     end
     else begin	// @[<stdin>:1673:11]
-      readDataValidReg <= _dpi_sram_io_req_T;	// @[playground/src/Sram.scala:17:31, src/main/scala/chisel3/util/Decoupled.scala:52:35]
-      writeRespValidReg <= _dpi_sram_io_wr_T & _dpi_sram_io_wr_T_1;	// @[playground/src/Sram.scala:35:32, :36:18, src/main/scala/chisel3/util/Decoupled.scala:52:35]
+      readDataValidReg <= _dpi_sram_io_req_T;	// @[playground/src/Sram.scala:18:31, src/main/scala/chisel3/util/Decoupled.scala:52:35]
+      writeRespValidReg <= _dpi_sram_io_wr_T & _dpi_sram_io_wr_T_1;	// @[playground/src/Sram.scala:36:32, :37:18, src/main/scala/chisel3/util/Decoupled.scala:52:35]
     end
   end // always @(posedge)
   dpi_sram dpi_sram (	// @[playground/src/Sram.scala:7:22]
     .clock (clock),
-    .addr  (io_ar_bits_addr),
+    .addr
+      (_dpi_sram_io_req_T
+         ? io_ar_bits_addr
+         : _dpi_sram_io_wr_T & _dpi_sram_io_wr_T_1 ? io_aw_bits_addr : 32'h0),	// @[playground/src/Sram.scala:13:24, :14:{26,39}, src/main/scala/chisel3/util/Decoupled.scala:52:35]
     .wdata (io_w_bits_data),
     .wmask (io_w_bits_strb),
-    .req   (_dpi_sram_io_req_T | _dpi_sram_io_wr_T & _dpi_sram_io_wr_T_1),	// @[playground/src/Sram.scala:14:{31,46}, src/main/scala/chisel3/util/Decoupled.scala:52:35]
-    .wr    (_dpi_sram_io_wr_T & _dpi_sram_io_wr_T_1),	// @[playground/src/Sram.scala:15:31, src/main/scala/chisel3/util/Decoupled.scala:52:35]
+    .req   (_dpi_sram_io_req_T | _dpi_sram_io_wr_T & _dpi_sram_io_wr_T_1),	// @[playground/src/Sram.scala:15:{31,46}, src/main/scala/chisel3/util/Decoupled.scala:52:35]
+    .wr    (_dpi_sram_io_wr_T & _dpi_sram_io_wr_T_1),	// @[playground/src/Sram.scala:16:31, src/main/scala/chisel3/util/Decoupled.scala:52:35]
     .rdata (io_r_bits_data)
   );
   assign io_ar_ready = _io_ar_ready_output;	// @[<stdin>:1672:3]
-  assign io_r_valid = readDataValidReg;	// @[<stdin>:1672:3, playground/src/Sram.scala:17:31]
-  assign io_r_bits_resp = 2'h0;	// @[<stdin>:1672:3, playground/src/Sram.scala:25:17]
+  assign io_r_valid = readDataValidReg;	// @[<stdin>:1672:3, playground/src/Sram.scala:18:31]
+  assign io_r_bits_resp = 2'h0;	// @[<stdin>:1672:3, playground/src/Sram.scala:26:17]
   assign io_aw_ready = _io_aw_ready_output;	// @[<stdin>:1672:3]
   assign io_w_ready = _io_w_ready_output;	// @[<stdin>:1672:3]
-  assign io_b_valid = writeRespValidReg;	// @[<stdin>:1672:3, playground/src/Sram.scala:35:32]
-  assign io_b_bits_resp = 2'h0;	// @[<stdin>:1672:3, playground/src/Sram.scala:25:17]
+  assign io_b_valid = writeRespValidReg;	// @[<stdin>:1672:3, playground/src/Sram.scala:36:32]
+  assign io_b_bits_resp = 2'h0;	// @[<stdin>:1672:3, playground/src/Sram.scala:26:17]
 endmodule
 
