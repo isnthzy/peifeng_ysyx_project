b485037e0c5336f735418b96a330cb520b916d5d
tracer-ysyx <tracer@ysyx.org>
2024-03-07 16:32:47 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:32:47 up 11:13,  1 user,  load average: 0.26, 0.35, 0.26

--- npc/playground/src/IF_stage.scala
+++ npc/playground/src/IF_stage.scala
@@ -15,33 +15,38 @@ class IF_stage extends Module {
   })
   val if_inst=dontTouch(WireDefault(0.U(32.W)))
   val if_clog=dontTouch(Wire(Bool()))
-  val if_inst_is_valid=dontTouch(Wire(Bool()))
-
+  val if_inst_ok=dontTouch(Wire(Bool()))
+  val if_inst_buffer=dontTouch(RegInit(0.U(32.W)))
+  val if_use_inst_buffer=dontTouch(RegInit(Bool()))
   val if_flush=dontTouch(Wire(Bool()))
   if_flush:= IF.for_ex.flush || IF.for_id.flush
 
   val if_valid=dontTouch(RegInit(false.B))
   val if_ready_go=dontTouch(Wire(Bool()))
-  if_clog:= ~if_inst_is_valid&&if_valid
+  if_clog:= ~if_inst_ok&&if_valid
   if_ready_go:=Mux(if_clog,false.B,true.B)
-  IF.IO.ready := !if_valid || if_ready_go && IF.to_id.ready 
+  IF.IO.ready:= !if_valid || if_ready_go && IF.to_id.ready 
   when(IF.IO.ready){
     if_valid:=IF.IO.valid
   }
   IF.to_id.valid:=Mux(if_flush, false.B , if_valid && if_ready_go)
   
+  if_inst:=Mux(if_use_inst_buffer,if_inst_buffer,IF.r.bits.data)
 
+  when(IF.to_id.fire){
+    if_use_inst_buffer:=false.B
+  }//加buffer是为了在if级暂存取到的指令，避免因为流水与下一级握手失败if级失去指令
   //-----------------AXI4Lite R  Channel------------------------
   IF.r.ready:=true.B
 
   when(IF.r.fire){
-    if_inst:=IF.r.bits.data
-    if_inst_is_valid:=true.B
+    if_inst_buffer:=IF.r.bits.data
+    if_use_inst_buffer:=true.B
+    if_inst_ok:=true.B
   }.otherwise{
-    if_inst_is_valid:=false.B
+    if_inst_ok:=false.B
   }
   //-----------------AXI4Lite R  Channel------------------------
-
   IF.to_id.bits.pc:=IF.IO.bits.pc
   IF.to_id.bits.nextpc:=IF.IO.bits.nextpc
   IF.to_id.bits.inst:=if_inst
