24ff50f40a75ae0ebe6d101af813780e05f94dc3
tracer-ysyx <tracer@ysyx.org>
2024-03-07 16:48:27 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:48:27 up 11:29,  1 user,  load average: 0.27, 0.20, 0.23

--- npc/playground/src/IF_stage.scala
+++ npc/playground/src/IF_stage.scala
@@ -13,9 +13,10 @@ class IF_stage extends Module {
 
     val r=Flipped(Decoupled(new AxiReadDataBundle()))
   })
-  val if_inst=dontTouch(WireDefault(0.U(32.W)))
   val if_clog=dontTouch(Wire(Bool()))
+  val if_inst=dontTouch(WireDefault(0.U(32.W)))
   val if_inst_ok=dontTouch(Wire(Bool()))
+  val if_inst_ok_buffer=dontTouch(RegInit(false.B))
   val if_inst_buffer=dontTouch(RegInit(0.U(32.W)))
   val if_use_inst_buffer=dontTouch(RegInit(false.B))
   val if_flush=dontTouch(Wire(Bool()))
@@ -32,9 +33,10 @@ class IF_stage extends Module {
   IF.to_id.valid:=Mux(if_flush, false.B , if_valid && if_ready_go)
 
   if_inst:=Mux(if_use_inst_buffer,if_inst_buffer,IF.r.bits.data)
-
+  if_inst_ok:=if_inst_ok_buffer
   when(IF.IO.fire){
     if_use_inst_buffer:=false.B
+    if_inst_ok_buffer:=false.B
   }
   //加buffer是为了在if级暂存取到的指令，避免因为流水与下一级握手失败if级失去指
   //当接收到新的数据时，取消使用buffer
@@ -50,9 +52,8 @@ class IF_stage extends Module {
   when(IF.r.fire){
     if_inst_buffer:=IF.r.bits.data
     if_use_inst_buffer:=true.B
+    if_inst_ok_buffer:=true.B
     if_inst_ok:=true.B
-  }.otherwise{
-    if_inst_ok:=false.B
   }
 //---------------------------AXI4Lite R  Channel------------------------
   IF.to_id.bits.pc:=IF.IO.bits.pc
