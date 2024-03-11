04b8d65401d404b93360471510bf24e95c6c37bd
tracer-ysyx <tracer@ysyx.org>
2024-03-11 20:27:10 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:27:10 up  1:44,  1 user,  load average: 0.51, 0.29, 0.29

--- npc/csrc/include/npc_conf.h
+++ npc/csrc/include/npc_conf.h
@@ -5,11 +5,11 @@
 
 /*---------------------------------------------------------------------------------*/
 
-// #define CONFIG_TRACE 1
-// #define CONFIG_ITRACE 1
-// #define CONFIG_FTRACE 1
-// #define CONFIG_MTRACE 1
-// #define CONFIG_DIFFTEST 1
+#define CONFIG_TRACE 1
+#define CONFIG_ITRACE 1
+#define CONFIG_FTRACE 1
+#define CONFIG_MTRACE 1
+#define CONFIG_DIFFTEST 1
 
 #define NPCLOG_NUM 1000000 //trace最多记录多少个log
 //TRACE开关
@@ -21,7 +21,7 @@
 
 /*---------------------------------------------------------------------------------*/
 
-// #define TRACE_VCD 1
+#define TRACE_VCD 1
 /* 波形生成开关 */
 
 /*---------------------------------------------------------------------------------*/
