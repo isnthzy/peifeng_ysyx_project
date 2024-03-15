aec90dc5c711bbe388493fa84eab0eabbaf7e301
tracer-ysyx <tracer@ysyx.org>
2024-03-15 09:24:20 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:24:20 up 3 min,  1 user,  load average: 0.68, 0.80, 0.37

---
+++ npc/vsrc/dpic/DpiSram.v
@@ -0,0 +1,25 @@
+
+import "DPI-C" function  int pmem_read (input int raddr);
+import "DPI-C" function void pmem_write(input int waddr, input  int wdata, input byte wmask);
+module dpi_sram(
+   input        clock,
+   input [31:0] addr,
+   input [31:0] wdata,
+   input [ 7:0] wmask,
+   input        req,
+   input        wr,
+   output reg [31:0] rdata
+);
+ 
+always @(posedge clock) begin
+    if(req) begin
+      if(wr) begin 
+       pmem_write (addr,wdata,wmask);
+      end
+      else begin
+       rdata<=pmem_read (addr);
+      end
+    end
+end
+endmodule
+    
