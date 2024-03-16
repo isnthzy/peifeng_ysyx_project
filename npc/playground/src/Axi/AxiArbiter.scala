32b4c49b4a0de80b4ec6b05fbd481153a0a50c4e
tracer-ysyx <tracer@ysyx.org>
2024-03-16 12:13:10 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:13:10 up  2:14,  1 user,  load average: 0.63, 0.22, 0.17

--- npc/playground/src/Axi/AxiArbiter.scala
+++ npc/playground/src/Axi/AxiArbiter.scala
@@ -80,15 +80,6 @@ class AxiArbiter extends Module {
   
   io.fs.s.waddr_ok:=false.B
   io.fs.s.wdata_ok:=false.B
-
-  // io.fs.al.raddr_ok:=WireDefault(false.B)
-  // io.fs.dl.rdata_ok:=WireDefault(false.B)
-  // io.fs.dl.rdata:=WireDefault(0.U)
-  // io.ls.al.raddr_ok:=WireDefault(false.B)
-  // io.ls.dl.rdata_ok:=WireDefault(false.B)
-  // io.ls.dl.rdata:=WireDefault(0.U)
-  // io.out.al.ren:=WireDefault(false.B)
-  // io.out.al.raddr:=WireDefault(0.U)
   io.fs.al.raddr_ok:=fs_raddr_ok
   io.fs.dl.rdata_ok:=fs_rdata_ok
   io.fs.dl.rdata:=fs_rdata
