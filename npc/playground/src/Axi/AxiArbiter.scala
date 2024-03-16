11095ec9790cf9f2ec2f88c85840a673a2951147
tracer-ysyx <tracer@ysyx.org>
2024-03-16 10:08:14 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:08:14 up  1:32,  1 user,  load average: 0.37, 0.27, 0.26

--- npc/playground/src/Axi/AxiArbiter.scala
+++ npc/playground/src/Axi/AxiArbiter.scala
@@ -20,42 +20,55 @@ class AxiArbiter extends Module {
 
   val fs_ren_reg=RegInit(false.B)
   val fs_raddr_reg=RegInit(0.U(32.W))
+
+  val fs_raddr_ok=WireDefault(false.B)
+  val fs_rdata_ok=WireDefault(false.B)
+  val fs_rdata=WireDefault(0.U)
+  val ls_raddr_ok=WireDefault(false.B)
+  val ls_rdata_ok=WireDefault(false.B)
+  val ls_rdata=WireDefault(0.U)
+  val out_ren=WireDefault(false.B)
+  val out_raddr=WireDefault(0.U)
   
   when(ArbiterState===arb_idle){
     when(io.fs.al.ren&&io.ls.al.ren){
       ArbiterState:=arb_wait_ls_arready
-      io.out.al.ren:=io.ls.al.ren
-      io.out.al.raddr:=io.ls.al.raddr
+      out_ren:=io.ls.al.ren
+      out_raddr:=io.ls.al.raddr
 
       fs_ren_reg:=io.fs.al.ren
       fs_raddr_reg:=io.fs.al.raddr
     }.otherwise{
       ArbiterState:=arb_wait_fs_arready
-      io.out.al.ren:=io.fs.al.ren
-      io.out.al.raddr:=io.fs.al.raddr
+      out_ren:=io.fs.al.ren
+      out_raddr:=io.fs.al.raddr
     }
   }.elsewhen(ArbiterState===arb_wait_fs_arready){
     when(io.out.al.raddr_ok){
-      io.fs.al.raddr_ok:=true.B
+      fs_raddr_ok:=true.B
       ArbiterState:=arb_wait_fs_rresp
     }
   }.elsewhen(ArbiterState===arb_wait_ls_arready){
     when(io.out.al.raddr_ok){
-      io.ls.al.raddr_ok:=true.B
+      fs_raddr_ok:=true.B
       ArbiterState:=arb_wait_ls_rresp
     }
   }.elsewhen(ArbiterState===arb_wait_fs_rresp){
     when(io.out.dl.rdata_ok){
       ArbiterState:=arb_idle
-      io.fs.dl:=io.out.dl
+      // io.fs.dl:=io.out.dl
+      fs_rdata_ok:=io.out.dl.rdata_ok
+      fs_rdata:=io.out.dl.rdata
     }
   }.elsewhen(ArbiterState===arb_wait_ls_rresp){
     when(io.out.dl.rdata_ok){
-      io.ls.dl:=io.out.dl
+      // io.ls.dl:=io.out.dl
+      ls_rdata_ok:=io.ls.dl.rdata_ok
+      ls_rdata:=io.ls.dl.rdata
       when(fs_ren_reg){
         ArbiterState:=arb_wait_fs_arready
-        io.out.al.ren:=fs_ren_reg
-        io.out.al.raddr:=fs_raddr_reg
+        out_ren:=fs_ren_reg
+        out_raddr:=fs_raddr_reg
 
         fs_ren_reg:=false.B
         fs_raddr_reg:=0.U
@@ -76,6 +89,14 @@ class AxiArbiter extends Module {
   // io.ls.dl.rdata:=WireDefault(0.U)
   // io.out.al.ren:=WireDefault(false.B)
   // io.out.al.raddr:=WireDefault(0.U)
+  io.fs.al.raddr_ok:=fs_raddr_ok
+  io.fs.dl.rdata_ok:=fs_rdata_ok
+  io.fs.dl.rdata:=fs_rdata
+  io.ls.al.raddr_ok:=ls_raddr_ok
+  io.ls.dl.rdata_ok:=ls_rdata_ok
+  io.ls.dl.rdata:=ls_rdata
+  io.out.al.ren:=out_ren
+  io.out.al.raddr:=out_raddr
   io.out.s<>io.ls.s
 }
 
