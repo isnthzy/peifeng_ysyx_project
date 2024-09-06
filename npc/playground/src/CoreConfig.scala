6d80b699ebde40bbd7385dee6c18a500ffef853e
tracer-ysyx <tracer@ysyx.org>
2024-09-06 14:31:01 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:31:01 up  3:05,  1 user,  load average: 0.26, 0.84, 0.85

--- npc/playground/src/CoreConfig.scala
+++ npc/playground/src/CoreConfig.scala
@@ -3,16 +3,16 @@ import chisel3._
 
 object Configs {
   // val START_ADDR = "h7ffffffc".U(32.W)  //开始地址，设计成80000000-4=7ffffffc是为了初始化reset
-  val START_ADDR = "h80000000".U(ADDR_WIDTH .W)
-  val ADDR_WIDTH = 32 // 地址位宽
-  val ADDR_BYTE_WIDTH = ADDR_WIDTH / 8    // 地址位宽按字节算
-  val DATA_WIDTH = 32 // 数据位宽
-  val DATA_WIDTH_H = 16   // 半字数据位宽
-  val DATA_WIDTH_B = 8    // 字节数据位宽
-  val RISCV32E_ECALLREG = 15.U
-  val RISCV32_ECALLREG = 17.U
+  def START_ADDR = "h80000000".U(ADDR_WIDTH .W)
+  def ADDR_WIDTH = 32 // 地址位宽
+  def ADDR_BYTE_WIDTH = ADDR_WIDTH / 8    // 地址位宽按字节算
+  def DATA_WIDTH = 32 // 数据位宽
+  def DATA_WIDTH_H = 16   // 半字数据位宽
+  def DATA_WIDTH_B = 8    // 字节数据位宽
+  def RISCV32E_ECALLREG = 15.U
+  def RISCV32_ECALLREG = 17.U
 
-  val INST_NOP = "h00000013".U(32.W)
+  def INST_NOP = "h00000013".U(32.W)
 }
 
 object ISAConfig{
