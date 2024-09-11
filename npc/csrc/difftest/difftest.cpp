4eb6ff225f8b863ff63f38ded5ec982d96eaed9b
tracer-ysyx <tracer@ysyx.org>
2024-09-11 10:55:10 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:55:10 up  1:31,  1 user,  load average: 0.24, 0.26, 0.26

--- npc/csrc/difftest/difftest.cpp
+++ npc/csrc/difftest/difftest.cpp
@@ -138,7 +138,7 @@ int Difftest::diff_step(){
   for(int i=0;i<idx_commit_num;i++){
     if(dut_commit.load[i].valid){
       if(!nemu_proxy->ref_check_load(dut_commit.load[i].paddr,dut_commit.load[i].len)){
-        wLog("dut paddr = 0x%lx, data = 0x%lx\n", 
+        wLog("dut paddr = " FMT_PADDR " , data = " FMT_WORD , 
               dut_commit.load[i].paddr, dut_commit.load[i].data);
         return NPC_ABORT;
       }
@@ -146,7 +146,7 @@ int Difftest::diff_step(){
     }//NOTE:我好像知道load了为什么不追踪data了，是因为外设！！！,追踪addr和访问类型即可
     if(dut_commit.store[i].valid){
       if(!nemu_proxy->ref_check_store(dut_commit.store[i].paddr,dut_commit.store[i].data,dut_commit.store[i].len)){
-        wLog("dut paddr = 0x%lx, data = 0x%lx\n", 
+        wLog("dut paddr = " FMT_PADDR " , data = " FMT_WORD , 
          dut_commit.store[i].paddr, dut_commit.store[i].data);
          return NPC_ABORT;
       }
