eab1c73f122a4229c5a62e774deaef9321a0a276
tracer-ysyx <tracer@ysyx.org>
2024-09-11 14:15:33 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:15:33 up  4:52,  1 user,  load average: 0.31, 0.21, 0.17

--- npc/playground/src/PipeLine/EX.scala
+++ npc/playground/src/PipeLine/EX.scala
@@ -154,9 +154,9 @@ class ExStage extends Module {
   ex.to_ls.bits.diffStore.paddr:=memMisalignedAddr
   ex.to_ls.bits.diffStore.vaddr:=memMisalignedAddr
   ex.to_ls.bits.diffStore.data:=memWdata
-  ex.to_ls.bits.diffStore.len :=Cat(0.U(4.W),ex.in.bits.ldType===SDEF(ST_SW),
-                                    0.U(1.W),ex.in.bits.ldType===SDEF(ST_SH),
-                                             ex.in.bits.ldType===SDEF(ST_SB))
+  ex.to_ls.bits.diffStore.len :=Cat(0.U(4.W),ex.in.bits.stType===SDEF(ST_SW),
+                                    0.U(1.W),ex.in.bits.stType===SDEF(ST_SH),
+                                             ex.in.bits.stType===SDEF(ST_SB))
   
   ex.to_ls.bits.diffLoad.valid:=loadEn
   ex.to_ls.bits.diffLoad.index:=0.U
