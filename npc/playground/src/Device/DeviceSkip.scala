7624eeea8ef9719d5e35f3d6bb05b5da6138733a
tracer-ysyx <tracer@ysyx.org>
2024-09-11 14:49:19 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:49:19 up  5:25,  1 user,  load average: 1.36, 1.11, 0.64

--- npc/playground/src/Device/DeviceSkip.scala
+++ npc/playground/src/Device/DeviceSkip.scala
@@ -13,7 +13,7 @@ class DeviceSkip extends Module with DeviceAddr{
   })
   val readSkip  = Wire(Bool()) 
   val writeSkip = Wire(Bool()) 
-  io.skip := ( readSkip || writeSkip ) && io.isLoadStore
+  io.skip := readSkip  && io.isLoadStore
 
   readSkip:=(
     (io.addr===RTC_ADDR || io.addr === RTC_ADDR+4.U)
@@ -21,8 +21,8 @@ class DeviceSkip extends Module with DeviceAddr{
   ||(io.addr >=FB_ADDR  && io.addr  <= FB_ADDR+(SCREEN_SIZE*4).U)
   )
 
-  writeSkip:=(
-     io.addr===SERIAL_PORT || io.addr===SYNC_ADDR
-  ||(io.addr >=FB_ADDR     && io.addr  <= FB_ADDR+(SCREEN_SIZE*4).U)
-  )
+  // writeSkip:=(
+  //    io.addr===SERIAL_PORT || io.addr===SYNC_ADDR
+  // ||(io.addr >=FB_ADDR     && io.addr  <= FB_ADDR+(SCREEN_SIZE*4).U)
+  // )
 }
