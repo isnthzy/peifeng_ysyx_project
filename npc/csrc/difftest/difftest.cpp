340b631d08e897f359c47389897ff1c100b24145
tracer-ysyx <tracer@ysyx.org>
2024-09-11 10:55:43 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:55:43 up  1:32,  1 user,  load average: 0.13, 0.23, 0.25

--- npc/csrc/difftest/difftest.cpp
+++ npc/csrc/difftest/difftest.cpp
@@ -136,22 +136,22 @@ int Difftest::diff_step(){
 
   #ifdef CONFIG_MEMDIFF
   for(int i=0;i<idx_commit_num;i++){
-    if(dut_commit.load[i].valid){
-      if(!nemu_proxy->ref_check_load(dut_commit.load[i].paddr,dut_commit.load[i].len)){
-        wLog("dut paddr = " FMT_PADDR " , data = " FMT_WORD , 
-              dut_commit.load[i].paddr, dut_commit.load[i].data);
-        return NPC_ABORT;
-      }
-      dut_commit.load[i].valid=false;
-    }//NOTE:我好像知道load了为什么不追踪data了，是因为外设！！！,追踪addr和访问类型即可
-    if(dut_commit.store[i].valid){
-      if(!nemu_proxy->ref_check_store(dut_commit.store[i].paddr,dut_commit.store[i].data,dut_commit.store[i].len)){
-        wLog("dut paddr = " FMT_PADDR " , data = " FMT_WORD , 
-         dut_commit.store[i].paddr, dut_commit.store[i].data);
-         return NPC_ABORT;
-      }
-      dut_commit.store[i].valid=false;
-    }
+    // if(dut_commit.load[i].valid){
+    //   if(!nemu_proxy->ref_check_load(dut_commit.load[i].paddr,dut_commit.load[i].len)){
+    //     wLog("dut paddr = " FMT_PADDR " , data = " FMT_WORD , 
+    //           dut_commit.load[i].paddr, dut_commit.load[i].data);
+    //     return NPC_ABORT;
+    //   }
+    //   dut_commit.load[i].valid=false;
+    // }//NOTE:我好像知道load了为什么不追踪data了，是因为外设！！！,追踪addr和访问类型即可
+    // if(dut_commit.store[i].valid){
+    //   if(!nemu_proxy->ref_check_store(dut_commit.store[i].paddr,dut_commit.store[i].data,dut_commit.store[i].len)){
+    //     wLog("dut paddr = " FMT_PADDR " , data = " FMT_WORD , 
+    //      dut_commit.store[i].paddr, dut_commit.store[i].data);
+    //      return NPC_ABORT;
+    //   }
+    //   dut_commit.store[i].valid=false;
+    // }
   }
   #endif
 
