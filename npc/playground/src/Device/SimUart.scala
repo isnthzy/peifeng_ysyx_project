747589f11d590957328753f5478321b499be40e4
tracer-ysyx <tracer@ysyx.org>
2024-09-15 16:47:53 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:47:53 up  2:38,  1 user,  load average: 0.50, 0.64, 0.42

--- npc/playground/src/Device/SimUart.scala
+++ npc/playground/src/Device/SimUart.scala
@@ -5,30 +5,32 @@ import Axi.Axi4LiteSlave
 import CoreConfig.Configs._
 
 class SimUart extends Module {
-  val io=IO(new Axi4LiteSlave())
+  val io=IO(new Bundle {
+    val axi=new Axi4LiteSlave()
+  })
 
   val state_idle :: state_write :: state_bresp :: Nil = Enum(2)
   val uartState=RegInit(state_idle)
   val addrWrire=RegInit(0.U(ADDR_WIDTH.W))
-  io.aw.ready:=true.B
+  io.axi.aw.ready:=true.B
 
   switch(uartState){
     is(state_idle){
-      when(io.aw.fire){
+      when(io.axi.aw.fire){
         uartState:=state_write
-        addrWrire:=io.w.bits.data
+        addrWrire:=io.axi.w.bits.data
       } 
     }
     is(state_write){
-      io.w.valid:=true.B
-      when(io.w.fire){
+      io.axi.w.valid:=true.B
+      when(io.axi.w.fire){
         uartState:=state_bresp
         printf("%c",addrWrire)
       }
     }
     is(state_bresp){
-      io.b.valid:=true.B
-      when(io.b.fire){
+      io.axi.b.valid:=true.B
+      when(io.axi.b.fire){
         uartState:=state_idle
       }
     }
