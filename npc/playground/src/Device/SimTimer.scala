a51ef53388f92d076bab8e0992d3a4968032bf1e
tracer-ysyx <tracer@ysyx.org>
2024-09-15 16:32:20 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:32:20 up  2:23,  1 user,  load average: 0.48, 0.24, 0.15

--- npc/playground/src/Device/SimTimer.scala
+++ npc/playground/src/Device/SimTimer.scala
@@ -1,37 +1,37 @@
-package Device
-import chisel3._
-import chisel3.util._
-import Axi.Axi4LiteSlave
-import CoreConfig.Configs._
-import CoreConfig.DeviceConfig
+// package Device
+// import chisel3._
+// import chisel3.util._
+// import Axi.Axi4LiteSlave
+// import CoreConfig.Configs._
+// import CoreConfig.DeviceConfig
 
-class SimTimer extends Module with DeviceConfig{
-  val io=IO(new Axi4LiteSlave())
+// class SimTimer extends Module with DeviceConfig{
+//   val io=IO(new Axi4LiteSlave())
 
-  val state_idle :: state_resp :: Nil = Enum(2)
-  val timerState=RegInit(state_idle)
-  val addrResp=RegInit(0.U(ADDR_WIDTH.W))
-  io.ar.ready:=true.B
+//   val state_idle :: state_resp :: Nil = Enum(2)
+//   val timerState=RegInit(state_idle)
+//   val addrResp=RegInit(0.U(ADDR_WIDTH.W))
+//   io.ar.ready:=true.B
   
-  val timer=RegInit(0.U(64.W))
-  timer:=timer+1.U;
+//   val timer=RegInit(0.U(64.W))
+//   timer:=timer+1.U;
 
-  switch(timerState){
-    is(state_idle){
-      when(io.ar.fire){
-        timerState:=state_resp
-        addrResp:=io.ar.bits.addr
-      }
-    }
-    is(state_resp){
-      when(io.r.fire){
-        when(addrResp===RTC_ADDR){
-          io.r.bits.data:=timer(31,0)
-        }.otherwise{
-          io.r.bits.data:=timer(63,32)
-        }
-        timerState:=state_idle
-      }
-    }
-  }
-}
+//   switch(timerState){
+//     is(state_idle){
+//       when(io.ar.fire){
+//         timerState:=state_resp
+//         addrResp:=io.ar.bits.addr
+//       }
+//     }
+//     is(state_resp){
+//       when(io.r.fire){
+//         when(addrResp===RTC_ADDR){
+//           io.r.bits.data:=timer(31,0)
+//         }.otherwise{
+//           io.r.bits.data:=timer(63,32)
+//         }
+//         timerState:=state_idle
+//       }
+//     }
+//   }
+// }
