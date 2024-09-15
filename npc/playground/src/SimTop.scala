9b4652e219b7a4fc9744d3c8c5f02f71374d48d4
tracer-ysyx <tracer@ysyx.org>
2024-09-15 16:31:37 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:31:37 up  2:22,  1 user,  load average: 0.17, 0.15, 0.12

--- npc/playground/src/SimTop.scala
+++ npc/playground/src/SimTop.scala
@@ -70,8 +70,8 @@ class SimTop extends Module with DeviceConfig{
   Axi4LiteBridge.io.b <>AxiXbarA2X.io.a.b
 
   AxiXbarA2X.io.x(0)<>Axi4LiteSram.io
-  AxiXbarA2X.io.x(1)<>SimUart.io
-  AxiXbarA2X.io.x(2)<>SimTimer.io
+  // AxiXbarA2X.io.x(1)<>SimUart.io
+  // AxiXbarA2X.io.x(2)<>SimTimer.io
 //
 
 // PreIF begin
