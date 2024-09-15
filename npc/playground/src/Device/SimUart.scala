4afb76ca2046cd99eaa9c883ad729836488a33c0
tracer-ysyx <tracer@ysyx.org>
2024-09-15 21:00:21 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:00:21 up  6:51,  1 user,  load average: 0.38, 0.31, 0.34

--- npc/playground/src/Device/SimUart.scala
+++ npc/playground/src/Device/SimUart.scala
@@ -7,28 +7,28 @@ import CoreConfig.Configs._
 class SimUart extends Module {
   val io=IO(new Axi4LiteSlave())
 
-  val state_idle :: state_write :: state_bresp :: Nil = Enum(3)
+  val state_idle ::  state_bresp :: Nil = Enum(2)
   val uartState=RegInit(state_idle)
-  val addrWrite=RegInit(0.U(ADDR_WIDTH.W))
+  val writeBuff=RegInit(0.U(ADDR_WIDTH.W))
+  val useWriteBuff=RegInit(false.B)
   io.aw.ready:=true.B
   
   io.w.ready:=false.B
   io.b.valid:=false.B
 
   io.b.bits:=0.U.asTypeOf(io.b.bits)
+
+  val putchar=Mux(useWriteBuff,writeBuff,io.w.bits.data)
   switch(uartState){
     is(state_idle){
-      io.w.ready:=false.B
-      when(io.aw.fire){
-        uartState:=state_write
-        addrWrite:=io.w.bits.data
-      } 
-    }
-    is(state_write){
       io.w.ready:=true.B
-      when(io.w.fire){
+      when(io.aw.fire&& !io.w.fire){
+        useWriteBuff:=true.B
+      } 
+      when(io.w.fire&&io.aw.fire){
         uartState:=state_bresp
-        printf("%c",addrWrite(7,0))
+        printf("%c",putchar)
+        useWriteBuff:=false.B
       }
     }
     is(state_bresp){
