5d6a2ad67e1d31e0478b93283f20e1c0a39ec6f1
tracer-ysyx <tracer@ysyx.org>
2024-09-16 20:20:13 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:20:13 up  4:32,  1 user,  load average: 0.99, 0.90, 0.61

--- npc/csrc/include/npc_verilator.h
+++ npc/csrc/include/npc_verilator.h
@@ -1,12 +1,13 @@
 #ifndef __NPCVERILATOR_H__
 #define __NPCVERILATOR_H__
-#include "VSimTop___024root.h"
-#include <VSimTop.h>
+// #include "VSimTop___024root.h"
+// #include <VSimTop.h>
+// #include "VSimTop__Dpi.h"
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 #include <verilated_fst_c.h>
 #include "svdpi.h"
-#include "VSimTop__Dpi.h"
+
 
 extern VerilatedContext* contextp;
 extern VSimTop* top;
