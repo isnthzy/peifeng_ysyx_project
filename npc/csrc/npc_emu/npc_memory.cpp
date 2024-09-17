46abc2c975bc6146d21bffee8d4f5f4e9465801a
tracer-ysyx <tracer@ysyx.org>
2024-09-17 19:00:13 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:00:13 up  4:43,  1 user,  load average: 0.53, 0.41, 0.26

--- npc/csrc/npc_emu/npc_memory.cpp
+++ npc/csrc/npc_emu/npc_memory.cpp
@@ -121,7 +121,7 @@ extern "C" void pmem_write(int waddr, int wdata, char wmask) {
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
 }
 
-extern "C" void flash_read(int32_t addr, int32_t *data) { assert(0); }
+extern "C" int flash_read(int32_t addr) { assert(0); }
 extern "C" int32_t mrom_read(int32_t addr) { 
   return paddr_read(addr,4);
 }
