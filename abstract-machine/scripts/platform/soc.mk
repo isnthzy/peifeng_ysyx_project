5fd3f3d06c5ad75fc6d9b165d669574068d556d6
tracer-ysyx <tracer@ysyx.org>
2024-09-18 16:44:00 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:44:00 up  2:59,  1 user,  load average: 0.27, 0.22, 0.20

--- abstract-machine/scripts/platform/soc.mk
+++ abstract-machine/scripts/platform/soc.mk
@@ -8,6 +8,7 @@ LDFLAGS   += -T $(AM_HOME)/am/src/riscv/soc/linker.ld \
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 
 NPCFLAGS+=-l $(shell dirname $(IMAGE).elf)/npc-log.txt
+NPCFLAGS+=--diff=$(NEMU_HOME)/build/riscv32-nemu-interpreter-so
 NPCFLAGS+=-b 
 
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
