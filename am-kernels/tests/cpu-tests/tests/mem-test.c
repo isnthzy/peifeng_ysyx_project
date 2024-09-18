a56adb3ed0d3e7502f4c8c35526ce241fc2fa9d6
tracer-ysyx <tracer@ysyx.org>
2024-09-18 21:08:20 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:08:20 up  7:23,  1 user,  load average: 0.30, 0.17, 0.12

--- am-kernels/tests/cpu-tests/tests/mem-test.c
+++ am-kernels/tests/cpu-tests/tests/mem-test.c
@@ -11,24 +11,24 @@ int main() {
     check((addr & 0xff) == *((char *)(SRAM_BASE + i)));
   }
 
-  for(int i=16;i<32;i++){
+  for(int i=16;i<32;i+=2){
     int addr = SRAM_BASE + i;
     *(volatile short int *)(SRAM_BASE + i) =  addr & 0xffff;
   }
-  for(int i=31;i>=16;i--){
+  for(int i=31;i>=16;i-=2){
     int addr = SRAM_BASE + i;
     check((addr & 0xffff) == *((short int *)(SRAM_BASE + i)));
   }
 
-  // for(int i=32;i<48;i++){
-  //   int addr = SRAM_BASE + i;
-  //   *(volatile int *)(SRAM_BASE + i) =  addr & 0xffffffff;
-  // }
-  // for(int i=47;i>=32;i--){
-  //   int addr = SRAM_BASE + i;
+  for(int i=32;i<48;i+=4){
+    int addr = SRAM_BASE + i;
+    *(volatile int *)(SRAM_BASE + i) =  addr & 0xffffffff;
+  }
+  for(int i=47;i>=32;i-=4){
+    int addr = SRAM_BASE + i;
 
-  //   check((addr & 0xffffffff) == *((int *)(SRAM_BASE + i)));
-  // }
+    check((addr & 0xffffffff) == *((int *)(SRAM_BASE + i)));
+  }
 
 
   return 0;
