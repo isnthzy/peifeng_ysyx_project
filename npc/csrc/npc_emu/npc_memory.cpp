0ad0ddf77087b84903b826421acec04e1923effe
tracer-ysyx <tracer@ysyx.org>
2024-09-22 16:32:10 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:32:10 up 53 min,  1 user,  load average: 0.37, 0.34, 0.25

--- npc/csrc/npc_emu/npc_memory.cpp
+++ npc/csrc/npc_emu/npc_memory.cpp
@@ -102,7 +102,7 @@ extern "C" void pmem_write(int waddr, int wdata, char wmask) {
   // int st_len = (wmask & 0x1) + ((wmask & 0x2) >> 1) + ((wmask & 0x4) >> 2) + ((wmask & 0x8) >> 3);
   // int st_data = (wdata >> (8 * ((wmask & 0x2) >> 1 + (wmask & 0x4) >> 2 * 2 + (wmask & 0x8) >> 3 * 3))) & ((1 << (st_len * 8)) - 1);
   //用了笨方法枚举，暂时没想到什么合适的办法
-  // int st_addr=waddr& ~0x3u; //不需要进行对齐
+  int st_addr=waddr; //不需要进行对齐
   int st_len=0;
   int st_data=0;
   switch (wmask)
@@ -136,7 +136,7 @@ extern "C" void pmem_write(int waddr, int wdata, char wmask) {
     break;
   }
   // printf("waddr:%08x wdata:%08x wlen:%08x\n",st_addr,st_data,st_len);
-  paddr_write(waddr,st_len,st_data);
+  paddr_write(st_addr,st_len,st_data);
   // 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
   // `wmask`中每比特表示`wdata`中1个字节的掩码,
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
