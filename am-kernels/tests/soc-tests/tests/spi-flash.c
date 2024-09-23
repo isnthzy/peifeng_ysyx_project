693ea8dd4cdc686f0bcd20453c83272166dc8fa0
tracer-ysyx <tracer@ysyx.org>
2024-09-23 21:02:21 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:02:21 up  1:28,  1 user,  load average: 0.15, 0.12, 0.15

--- am-kernels/tests/soc-tests/tests/spi-flash.c
+++ am-kernels/tests/soc-tests/tests/spi-flash.c
@@ -1,5 +1,5 @@
 #include "trap.h"
-#define FLASH_BASE 0x30000000L
+#define FLASH_BASE 0x03000000L
 #define SPI_BASE 0x10001000L
 #define SPI_RX  0x0
 #define SPI_TX  0x0
@@ -12,7 +12,7 @@ uint32_t flash_read(uint32_t addr){
   *(volatile int *)(SPI_BASE + SPI_TX + 0x4) = addr;
   *(volatile short int *)(SPI_BASE + SPI_DIVIDER) = 0x009f;
   *(volatile char *)(SPI_BASE + SPI_SS) = 0b00000001;
-  *(volatile short int *)(SPI_BASE + SPI_CTRL) = 0b100101000000;
+  *(volatile short int *)(SPI_BASE + SPI_CTRL) = 0b000101000000;
   while(1){
     volatile short int* spi_state = ((volatile short int *)(SPI_BASE + SPI_CTRL));
     if(((*spi_state) & 0x100) != 0x100){
