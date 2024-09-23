72a29598edd3147d5a6a0e71f333fea9bffcbb69
tracer-ysyx <tracer@ysyx.org>
2024-09-23 17:15:55 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:15:55 up 2 min,  1 user,  load average: 5.18, 1.80, 0.64

--- am-kernels/tests/soc-tests/tests/bitrev-test.c
+++ am-kernels/tests/soc-tests/tests/bitrev-test.c
@@ -7,7 +7,7 @@
 #define SPI_SS   0x18
 int main(){
   char ch = 0x14;
-  *(volatile char *)(SPI_BASE + SPI_TX0) = ch;
+  *(volatile char *)(SPI_BASE + SPI_TX0 + 0x1) = ch;
   *(volatile short int *)(SPI_BASE + SPI_DIVIDER) = 0x009f;
   *(volatile char *)(SPI_BASE + SPI_SS) = 0b10000000;
   *(volatile short int *)(SPI_BASE + SPI_CTRL) = 0b100010000;
@@ -16,13 +16,13 @@ int main(){
 // Name   Reserved ASS IE  LSB Tx_NEG Rx_NEG GO_BSY Reserved CHAR_LEN
 // NOTE:不需要下降沿触发所以不对tx rx处理，ass有点看不懂主要是
 // NOTE:他这个长度是每次传输总长度，例如输入8位输出8位那么总长度就是16位
-  while(true){
+  while(1){
     volatile short int* spi_state = ((volatile short int *)(SPI_BASE + SPI_CTRL));
     if(((*spi_state) & 0x100) != 0x100){
       *(volatile char *)(SPI_BASE + SPI_SS) &= ~0b10000000;
       break;
     }
   }
-  volatile char * ch_rev = (volatile char *)(SPI_BASE + SPI_TX0);
+  volatile char * ch_rev = (volatile char *)(SPI_BASE + SPI_TX0 + 0x1);
   check((*ch_rev)==0x28);
 }
