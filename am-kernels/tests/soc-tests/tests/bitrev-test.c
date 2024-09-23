dd38b96dc9066266de7af2c2054485bc11661bca
tracer-ysyx <tracer@ysyx.org>
2024-09-23 15:26:21 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:26:21 up 39 min,  1 user,  load average: 0.20, 0.19, 0.27

--- am-kernels/tests/soc-tests/tests/bitrev-test.c
+++ am-kernels/tests/soc-tests/tests/bitrev-test.c
@@ -10,11 +10,12 @@ int main(){
   *(volatile char *)(SPI_BASE + SPI_TX0) = ch;
   *(volatile short int *)(SPI_BASE + SPI_DIVIDER) = 0x009f;
   *(volatile char *)(SPI_BASE + SPI_SS) = 0b10000000;
-  *(volatile short int *)(SPI_BASE + SPI_CTRL) = 0b100001000;
+  *(volatile short int *)(SPI_BASE + SPI_CTRL) = 0b100010000;
 // Bit #  31:14    13  12  11  10     9      8   7  6:0
 // Access R        R/W R/W R/W R/W    R/W    R/W R  R/W
 // Name   Reserved ASS IE  LSB Tx_NEG Rx_NEG GO_BSY Reserved CHAR_LEN
 // NOTE:不需要下降沿触发所以不对tx rx处理，ass有点看不懂主要是
+// NOTE:他这个长度是每次传输总长度，例如输入8位输出8位那么总长度就是16位
   while(true){
     volatile short int* spi_state = ((volatile short int *)(SPI_BASE + SPI_CTRL));
     if(((*spi_state) & 0x100) != 0x100){
