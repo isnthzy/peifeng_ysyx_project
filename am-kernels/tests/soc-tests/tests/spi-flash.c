ea9c7fadffaac315e3633c1ffcc36052df5b13c4
tracer-ysyx <tracer@ysyx.org>
2024-09-23 20:11:11 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:11:11 up 37 min,  1 user,  load average: 0.29, 0.18, 0.18

---
+++ am-kernels/tests/soc-tests/tests/spi-flash.c
@@ -0,0 +1,36 @@
+#include "trap.h"
+#define FLASH_BASE 0x30000000L
+#define SPI_BASE 0x10001000L
+#define SPI_RX  0x0
+#define SPI_TX  0x0
+#define SPI_CTRL 0x10
+#define SPI_DIVIDER 0x14
+#define SPI_SS   0x18
+
+uint32_t flash_read(uint32_t addr){
+
+  *(volatile int *)(SPI_BASE + SPI_TX + 0x4) = addr;
+  *(volatile short int *)(SPI_BASE + SPI_DIVIDER) = 0x009f;
+  *(volatile char *)(SPI_BASE + SPI_SS) = 0b00000001;
+  *(volatile short int *)(SPI_BASE + SPI_CTRL) = 0b101000000;
+  while(1){
+    volatile short int* spi_state = ((volatile short int *)(SPI_BASE + SPI_CTRL));
+    if(((*spi_state) & 0x100) != 0x100){
+      *(volatile char *)(SPI_BASE + SPI_SS) &= ~0b00000001;
+      break;
+    }
+  }
+  uint32_t data = *(volatile int *)(SPI_BASE + SPI_RX);
+  return data;
+}
+
+int main(){
+  for(int i=0;i<20;i+=4){
+    uint32_t addr = FLASH_BASE + i;
+    uint32_t data = flash_read(addr);
+    char *ch = (char *)&data; 
+    for(int i=0;i<4;i++){
+      putch(ch[i]);
+    }
+  }
+}
