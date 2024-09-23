75a2570c71f742b416009e6137b6d30f41432d16
wangxin-wsl2 <isnthzy@gmail.com>
2024-09-23 21:16:15 +0800

    Complete "Load the program from flash and execute it"

--- am-kernels/tests/soc-tests/tests/spi-flash.c
+++ am-kernels/tests/soc-tests/tests/spi-flash.c
@@ -1,4 +1,5 @@
 #include "trap.h"
+#define FLASH_BASE 0x03000000L
 #define SPI_BASE 0x10001000L
 #define SPI_RX  0x0
 #define SPI_TX  0x0
@@ -11,7 +12,7 @@ uint32_t flash_read(uint32_t addr){
   *(volatile int *)(SPI_BASE + SPI_TX + 0x4) = addr;
   *(volatile short int *)(SPI_BASE + SPI_DIVIDER) = 0x009f;
   *(volatile char *)(SPI_BASE + SPI_SS) = 0b00000001;
-  *(volatile short int *)(SPI_BASE + SPI_CTRL) = 0b101000000;
+  *(volatile short int *)(SPI_BASE + SPI_CTRL) = 0b000101000000;
   while(1){
     volatile short int* spi_state = ((volatile short int *)(SPI_BASE + SPI_CTRL));
     if(((*spi_state) & 0x100) != 0x100){
@@ -20,8 +21,20 @@ uint32_t flash_read(uint32_t addr){
     }
   }
   uint32_t data = *(volatile int *)(SPI_BASE + SPI_RX);
+  data = ((data >> 24)
+         |(data >> 8 & 0x00ff00)
+         |(data << 8 & 0xff0000)
+         |(data << 24));
+  return data;
 }
 
 int main(){
-  
+  for(int i=0;i<20;i+=4){
+    uint32_t addr = FLASH_BASE + i;
+    uint32_t data = flash_read(addr);
+    char *ch = (char *)&data; 
+    for(int i=0;i<4;i++){
+      putch(ch[i]);
+    }
+  }
 }
