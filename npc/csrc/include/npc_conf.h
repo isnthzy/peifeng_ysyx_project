d21bd3a0da71904a6cf5ab88e8f4c3bc75748754
tracer-ysyx <tracer@ysyx.org>
2024-10-05 17:06:44 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:06:44 up  2:40,  1 user,  load average: 0.39, 0.31, 0.28

--- npc/csrc/include/npc_conf.h
+++ npc/csrc/include/npc_conf.h
@@ -46,7 +46,7 @@
 
 /*---------------------------------------------------------------------------------*/
 
-#define MTRACE_LOGBUF_SIZE 300
+#define MTRACE_LOGBUF_SIZE 10
 #define ITRACE_LOGBUF_SIZE 10
 #define DTRACE_LOGBUF_SIZE 10
 #define ETRACE_LOGBUF_SIZE 10
