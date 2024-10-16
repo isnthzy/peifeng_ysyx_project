2a817279710df3ba7a2156b49597c1d1621fade2
tracer-ysyx <tracer@ysyx.org>
2024-10-16 20:35:24 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:35:24 up  6:07,  1 user,  load average: 0.25, 0.25, 0.20

--- npc/csrc/npc_emu/npc_memory.cpp
+++ npc/csrc/npc_emu/npc_memory.cpp
@@ -192,13 +192,13 @@ static uint8_t sdram_bank[16][8192][1024]={};
 extern "C" void sdrambank_read(uint16_t row,uint16_t col,uint16_t *data,
                                uint8_t bank,uint8_t dqm){
   uint16_t result = sdram_bank[bank][row][(col << 1) + 1] << 8 | sdram_bank[bank][row][col << 1];
-  // printf("row 0x%04x col 0x%04x rdata 0x%04x bank 0x%02x\n",row,col,result,bank);
+  printf("row 0x%04x col 0x%04x rdata 0x%04x bank 0x%02x\n",row,col,result,bank);
   *data=result;
 }
 
 extern "C" void sdrambank_write(uint16_t row,uint16_t col,uint16_t data,
                                 uint8_t bank,uint8_t dqm){
-  // printf("row 0x%04x col 0x%04x data 0x%04x bank 0x%02x dqm 0x%02x\n",row,col,data,bank,dqm);
+  printf("row 0x%04x col 0x%04x data 0x%04x bank 0x%02x dqm 0x%02x\n",row,col,data,bank,dqm);
   switch (dqm)
   {
   case 0x0:
