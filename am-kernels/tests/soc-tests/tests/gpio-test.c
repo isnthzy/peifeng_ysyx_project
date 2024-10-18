b1d46add2e6cb021b49d112cbf20a986e9dadcba
tracer-ysyx <tracer@ysyx.org>
2024-10-18 15:52:33 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:52:33 up 30 min,  1 user,  load average: 0.06, 0.26, 0.24

--- am-kernels/tests/soc-tests/tests/gpio-test.c
+++ am-kernels/tests/soc-tests/tests/gpio-test.c
@@ -1,13 +1,18 @@
 #include "trap.h"
 #define GPIO_BASE 0x10002000L
 int main(){
-  for(int i = 0; i < 2; i++){
-    for(int j = 0; j < 8; j++){
-      int n = 1000;
-      while(n--){
-        *(volatile int *)(GPIO_BASE + i) = 1 << j;
-      }
-      
-    }
+
+  int x = 1000000;
+  while(x--){
+    *(volatile char *)(GPIO_BASE ) = 0xff;
   }
+  // for(int i = 0; i < 2; i++){
+  //   for(int j = 0; j < 8; j++){
+  //     int n = 1000;
+  //     while(n--){
+  //       *(volatile char *)(GPIO_BASE + i) = 1 << j;
+  //     }
+      
+  //   }
+  // }
 }
