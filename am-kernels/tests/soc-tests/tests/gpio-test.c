1241a1eda9bbd6399549530aa44fc3a68ae09d95
tracer-ysyx <tracer@ysyx.org>
2024-10-18 17:00:58 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:00:58 up  1:38,  1 user,  load average: 0.40, 0.52, 0.57

--- am-kernels/tests/soc-tests/tests/gpio-test.c
+++ am-kernels/tests/soc-tests/tests/gpio-test.c
@@ -5,11 +5,11 @@ int main(){
   int x = 10000;
   while(x--){
     if(x%100==0){
-      // volatile char* ch0= (volatile char *)(GPIO_BASE + 0x4);
-      // volatile char* ch1= (volatile char *)(GPIO_BASE + 0x5);
-      // putch((*ch0));
-      // putch(*(ch1));
-      // putch('\n');
+      volatile char* ch0= (volatile char *)(GPIO_BASE + 0x4);
+      volatile char* ch1= (volatile char *)(GPIO_BASE + 0x5);
+      putch((*ch0));
+      putch(*(ch1));
+      putch('\n');
     }
   }
 
