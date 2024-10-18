3894b6384bd6c93e2eb75fffb5a690e384f6ca3d
tracer-ysyx <tracer@ysyx.org>
2024-10-18 16:51:01 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:51:01 up  1:28,  1 user,  load average: 3.92, 1.45, 0.68

--- am-kernels/tests/soc-tests/tests/gpio-test.c
+++ am-kernels/tests/soc-tests/tests/gpio-test.c
@@ -2,26 +2,26 @@
 #define GPIO_BASE 0x10002000L
 int main(){
 
-  int x = 10000;
-  *(volatile char *)(GPIO_BASE ) = 0xff;
-  while(x--){
-    if(x%100==0){
-      volatile char* ch0= (volatile char *)(GPIO_BASE + 0x4);
-      volatile char* ch1= (volatile char *)(GPIO_BASE + 0x5);
-      putch((*ch0));
-      putch(*(ch1));
-      putch('\n');
-    }
-
-
-  }
-  // for(int i = 0; i < 2; i++){
-  //   for(int j = 0; j < 8; j++){
-  //     int n = 1000;
-  //     while(n--){
-  //       *(volatile char *)(GPIO_BASE + i) = 1 << j;
-  //     }
-      
+  // int x = 10000;
+  // *(volatile char *)(GPIO_BASE ) = 0xff;
+  // while(x--){
+  //   if(x%100==0){
+  //     volatile char* ch0= (volatile char *)(GPIO_BASE + 0x4);
+  //     volatile char* ch1= (volatile char *)(GPIO_BASE + 0x5);
+  //     putch((*ch0));
+  //     putch(*(ch1));
+  //     putch('\n');
   //   }
+
+
   // }
+  for(int i = 0; i < 2; i++){
+    for(int j = 0; j < 8; j++){
+      int n = 1000;
+      while(n--){
+        *(volatile char *)(GPIO_BASE + i) = 1 << j;
+      }
+      
+    }
+  }
 }
