161dfa40ef2193992e4da8fdcb27d4e0a61c1ce8
tracer-ysyx <tracer@ysyx.org>
2024-10-22 14:35:39 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:35:39 up  5:19,  1 user,  load average: 0.21, 0.21, 0.18

--- abstract-machine/am/src/riscv/soc/input.c
+++ abstract-machine/am/src/riscv/soc/input.c
@@ -4,38 +4,98 @@
 #define UART_RX_ADDR 0x10000000L
 #define KEYBRD_ADDR  0x10011000L
 
-/*
-#define MAP(c, f) c(f)
-#define NPC_KEYS(f) \
-  f(ESCAPE) f(F1) f(F2) f(F3) f(F4) f(F5) f(F6) f(F7) f(F8) f(F9) f(F10) f(F11) f(F12) \
-f(GRAVE) f(1) f(2) f(3) f(4) f(5) f(6) f(7) f(8) f(9) f(0) f(MINUS) f(EQUALS) f(BACKSPACE) \
-f(TAB) f(Q) f(W) f(E) f(R) f(T) f(Y) f(U) f(I) f(O) f(P) f(LEFTBRACKET) f(RIGHTBRACKET) f(BACKSLASH) \
-f(CAPSLOCK) f(A) f(S) f(D) f(F) f(G) f(H) f(J) f(K) f(L) f(SEMICOLON) f(APOSTROPHE) f(RETURN) \
-f(LSHIFT) f(Z) f(X) f(C) f(V) f(B) f(N) f(M) f(COMMA) f(PERIOD) f(SLASH) f(RSHIFT) \
-f(LCTRL) f(APPLICATION) f(LALT) f(SPACE) f(RALT) f(RCTRL) \
-f(UP) f(DOWN) f(LEFT) f(RIGHT) f(INSERT) f(DELETE) f(HOME) f(END) f(PAGEUP) f(PAGEDOWN)
-
-#define NPC_KEY_NAME(k) NPC_KEY_ ## k,
-
-enum {
-  NPC_KEY_NONE = 0,
-  MAP(NPC_KEYS, NPC_KEY_NAME)
-};
-
-#define SDL_KEYMAP(k) keymap[SDL_SCANCODE_ ## k] = NPC_KEY_ ## k;
-static uint32_t keymap[256] = {};
-
-static void init_keymap() {
-  MAP(NPC_KEYS, SDL_KEYMAP)
-}
-*/
-
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t tmp_key=inw(KEYBRD_ADDR);
   bool tmp_keydown=((tmp_key & 0xf000) != 0xf000);
   kbd->keydown = tmp_keydown;
-  char ascii = tmp_key & 0xff; 
-  kbd->keycode = ascii;
+  char scankey = 0x0; 
+  switch (tmp_key & 0x00ff) {
+    case 0x76: scankey = 1; break; // ESCAPE
+    case 0x05: scankey = 2; break; // F1
+    case 0x06: scankey = 3; break; // F2
+    case 0x04: scankey = 4; break; // F3
+    case 0x0c: scankey = 5; break; // F4
+    case 0x03: scankey = 6; break; // F5
+    case 0x0b: scankey = 7; break; // F6
+    case 0x83: scankey = 8; break; // F7
+    case 0x0a: scankey = 9; break; // F8
+    case 0x01: scankey = 10; break; // F9
+
+    case 0x09: scankey = 11; break; // F10
+    case 0x78: scankey = 12; break; // F11
+    case 0x0e: scankey = 13; break; // GARVE
+    case 0x16: scankey = 14; break; // 1
+    case 0x1e: scankey = 15; break; // 2
+    case 0x26: scankey = 16; break; // 3
+    case 0x25: scankey = 17; break; // 4
+    case 0x2e: scankey = 18; break; // 5
+
+    case 0x36: scankey = 19; break; // 6
+    case 0x3d: scankey = 20; break; // 7
+    case 0x3e: scankey = 21; break; // 8
+    case 0x46: scankey = 22; break; // 9
+    case 0x45: scankey = 23; break; // 0
+    case 0x4e: scankey = 24; break; // MINUS
+    case 0x55: scankey = 25; break; // EQUALS
+    case 0x66: scankey = 26; break; // BACKSPACE
+    case 0x0d: scankey = 27; break; // TAB
+    case 0x15: scankey = 28; break; // q
+    case 0x1d: scankey = 29; break; // w
+    case 0x24: scankey = 30; break; // e
+    case 0x2d: scankey = 31; break; // r
+    case 0x2c: scankey = 32; break; // t
+    case 0x35: scankey = 33; break; // y
+    case 0x3c: scankey = 34; break; // u
+    case 0x43: scankey = 35; break; // i
+    case 0x44: scankey = 36; break; // o
+    case 0x4d: scankey = 37; break; // p
+    case 0x54: scankey = 38; break; // LEFTBRACKET
+    case 0x5b: scankey = 39; break; // RIGHTBRACKET
+    case 0x5d: scankey = 40; break; // BACKSLASH
+    case 0x58: scankey = 41; break; // CAPSLOCK
+    case 0x1c: scankey = 42; break; // a
+    case 0x1b: scankey = 43; break; // s
+    case 0x23: scankey = 43; break; // d
+    case 0x2b: scankey = 43; break; // f
+    case 0x34: scankey = 43; break; // g
+    case 0x33: scankey = 43; break; // h
+    case 0x3b: scankey = 43; break; // j
+    case 0x42: scankey = 43; break; // k
+    case 0x4b: scankey = 43; break; // l
+    case 0x4c: scankey = 43; break; // SEMICOLON
+    case 0x52: scankey = 43; break; // APOSTROPHE
+    case 0x5a: scankey = 43; break; // RETURN
+    case 0x12: scankey = 44; break; // LSHIFT
+    case 0x1a: scankey = 45; break; // z
+    case 0x22: scankey = 46; break; // x
+    case 0x21: scankey = 47; break; // c
+    case 0x2a: scankey = 48; break; // v
+    case 0x32: scankey = 49; break; // b
+    case 0x31: scankey = 50; break; // n
+    case 0x3a: scankey = 51; break; // m
+    case 0x41: scankey = 52; break; // COMMA
+    case 0x49: scankey = 53; break; // PERIOD
+    case 0x4a: scankey = 54; break; // SLASH
+    case 0x59: scankey = 55; break; // RSHIFT
+    case 0x14: scankey = 56; break; // LCTRL
+    case 0x11: scankey = 57; break; // LALT
+    case 0x29: scankey = 57; break; // SPACE
+    case 0xe011: scankey = 57; break; // RALT
+    case 0xe014: scankey = 57; break; // RCTRL
+    case 0xe075: scankey = 58; break; // up
+    case 0xe072: scankey = 59; break; // down
+    case 0xe06b: scankey = 60; break; // left
+    case 0xe074: scankey = 61; break; // right
+    case 0xe070: scankey = 62; break; // insert
+    case 0xe071: scankey = 63; break; // delete
+    case 0xe06c: scankey = 64; break; // home
+    case 0xe069: scankey = 65; break; // end
+    case 0xe07d: scankey = 66; break; // pageup
+    case 0xe07a: scankey = 67; break; // pagedown
+
+    default:  scankey = 0x00; // Default case
+  }
+  kbd->keycode = scankey;
 }
 
 void __am_uart_rx(AM_UART_RX_T *recv) {
