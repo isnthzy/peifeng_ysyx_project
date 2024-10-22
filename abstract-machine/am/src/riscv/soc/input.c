06c5c7f5b9ece5d49bc174ea22805eec55182032
tracer-ysyx <tracer@ysyx.org>
2024-10-22 09:38:44 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:38:44 up 22 min,  1 user,  load average: 0.47, 0.46, 0.45

--- abstract-machine/am/src/riscv/soc/input.c
+++ abstract-machine/am/src/riscv/soc/input.c
@@ -2,14 +2,15 @@
 #include <riscv/riscv.h>
 
 #define UART_RX_ADDR 0x10000000L
+#define KEYBRD_ADDR  0x10011000L
 // #define KEYDOWN_MASK 0x8000
 // #define DEVICE_BASE  0xa0000000
 // #define KBD_ADDR     (DEVICE_BASE + 0x0000060)
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  // uint32_t tmp_key=inl(KBD_ADDR);
-  // bool tmp_keydown=((tmp_key&KEYDOWN_MASK)!=0);
-  // kbd->keydown = tmp_keydown;
-  // kbd->keycode = tmp_key&~KEYDOWN_MASK;
+  uint32_t tmp_key=inb(KEYBRD_ADDR);
+  bool tmp_keydown=(tmp_key!=0x0);
+  kbd->keydown = tmp_keydown;
+  kbd->keycode = tmp_key;
 }
 
 void __am_uart_rx(AM_UART_RX_T *recv) {
