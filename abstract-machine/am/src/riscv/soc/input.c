346ef2be768b370c41b684e14e829fef6205a4f6
tracer-ysyx <tracer@ysyx.org>
2024-10-22 13:49:07 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:49:07 up  4:33,  1 user,  load average: 0.09, 0.10, 0.11

--- abstract-machine/am/src/riscv/soc/input.c
+++ abstract-machine/am/src/riscv/soc/input.c
@@ -3,63 +3,38 @@
 
 #define UART_RX_ADDR 0x10000000L
 #define KEYBRD_ADDR  0x10011000L
-// #define KEYDOWN_MASK 0x8000
-// #define DEVICE_BASE  0xa0000000
-// #define KBD_ADDR     (DEVICE_BASE + 0x0000060)
+
+/*
+#define MAP(c, f) c(f)
+#define NPC_KEYS(f) \
+  f(ESCAPE) f(F1) f(F2) f(F3) f(F4) f(F5) f(F6) f(F7) f(F8) f(F9) f(F10) f(F11) f(F12) \
+f(GRAVE) f(1) f(2) f(3) f(4) f(5) f(6) f(7) f(8) f(9) f(0) f(MINUS) f(EQUALS) f(BACKSPACE) \
+f(TAB) f(Q) f(W) f(E) f(R) f(T) f(Y) f(U) f(I) f(O) f(P) f(LEFTBRACKET) f(RIGHTBRACKET) f(BACKSLASH) \
+f(CAPSLOCK) f(A) f(S) f(D) f(F) f(G) f(H) f(J) f(K) f(L) f(SEMICOLON) f(APOSTROPHE) f(RETURN) \
+f(LSHIFT) f(Z) f(X) f(C) f(V) f(B) f(N) f(M) f(COMMA) f(PERIOD) f(SLASH) f(RSHIFT) \
+f(LCTRL) f(APPLICATION) f(LALT) f(SPACE) f(RALT) f(RCTRL) \
+f(UP) f(DOWN) f(LEFT) f(RIGHT) f(INSERT) f(DELETE) f(HOME) f(END) f(PAGEUP) f(PAGEDOWN)
+
+#define NPC_KEY_NAME(k) NPC_KEY_ ## k,
+
+enum {
+  NPC_KEY_NONE = 0,
+  MAP(NPC_KEYS, NPC_KEY_NAME)
+};
+
+#define SDL_KEYMAP(k) keymap[SDL_SCANCODE_ ## k] = NPC_KEY_ ## k;
+static uint32_t keymap[256] = {};
+
+static void init_keymap() {
+  MAP(NPC_KEYS, SDL_KEYMAP)
+}
+*/
+
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t tmp_key=inw(KEYBRD_ADDR);
   bool tmp_keydown=((tmp_key & 0xf000) != 0xf000);
   kbd->keydown = tmp_keydown;
-  char ascii = 0x0; 
-  switch (tmp_key & 0xff) {
-    case 0x16: ascii = 0x30; break; // 0
-    case 0x1e: ascii = 0x31; break; // 1
-    case 0x26: ascii = 0x32; break; // 2
-    case 0x25: ascii = 0x33; break; // 3
-    case 0x2e: ascii = 0x34; break; // 4
-    case 0x36: ascii = 0x35; break; // 5
-    case 0x3d: ascii = 0x36; break; // 6
-    case 0x3e: ascii = 0x37; break; // 7
-    case 0x46: ascii = 0x38; break; // 8
-    case 0x45: ascii = 0x39; break; // 9
-
-    case 0x41: ascii = 0x2c; break; // ,
-    case 0x49: ascii = 0x2e; break; // .
-    case 0x4a: ascii = 0x2f; break; // /
-    case 0x4c: ascii = 0x3b; break; // ;
-    case 0x52: ascii = 0x27; break; // '
-    case 0x54: ascii = 0x5b; break; // [
-    case 0x5b: ascii = 0x5d; break; // ]
-    case 0x5a: ascii = 0x0a; break; // enter
-
-    case 0x15: ascii = 0x51; break; // Q
-    case 0x1d: ascii = 0x57; break; // W
-    case 0x24: ascii = 0x45; break; // E
-    case 0x2d: ascii = 0x52; break; // R
-    case 0x2c: ascii = 0x54; break; // T
-    case 0x35: ascii = 0x59; break; // Y
-    case 0x3c: ascii = 0x55; break; // U
-    case 0x43: ascii = 0x49; break; // I
-    case 0x44: ascii = 0x4f; break; // O
-    case 0x4d: ascii = 0x50; break; // P
-    case 0x1c: ascii = 0x41; break; // A
-    case 0x1b: ascii = 0x53; break; // S
-    case 0x23: ascii = 0x44; break; // D
-    case 0x2b: ascii = 0x46; break; // F
-    case 0x34: ascii = 0x47; break; // G
-    case 0x33: ascii = 0x48; break; // H
-    case 0x3b: ascii = 0x4a; break; // J
-    case 0x42: ascii = 0x4b; break; // K
-    case 0x4b: ascii = 0x4c; break; // L
-    case 0x1a: ascii = 0x5a; break; // Z
-    case 0x22: ascii = 0x58; break; // X
-    case 0x21: ascii = 0x43; break; // C
-    case 0x2a: ascii = 0x56; break; // V
-    case 0x32: ascii = 0x42; break; // B
-    case 0x31: ascii = 0x4e; break; // N
-    case 0x3a: ascii = 0x4d; break; // M
-    default:  ascii = 0x00; // Default case
-  }
+  char ascii = tmp_key & 0xff; 
   kbd->keycode = ascii;
 }
 
