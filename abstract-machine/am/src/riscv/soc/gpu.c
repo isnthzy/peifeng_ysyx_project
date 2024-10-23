4b927b7a882076f888371409766185b87f1ab8a7
tracer-ysyx <tracer@ysyx.org>
2024-10-23 20:25:19 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:25:19 up 10:00,  1 user,  load average: 0.48, 0.57, 0.52

--- abstract-machine/am/src/riscv/soc/gpu.c
+++ abstract-machine/am/src/riscv/soc/gpu.c
@@ -47,9 +47,8 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 	那么这时候我们只需要把需要取出来的数据放进要放的位置就好了
   */
   uint32_t *pixels=ctl->pixels;
-  
-  for(int x=ctl->x;x<ctl->x+ctl->w;x++){
-    for(int y=ctl->y;y<ctl->y+ctl->h;y++){
+  for(int y=ctl->y;y<ctl->y+ctl->h;y++){
+    for(int x=ctl->x;x<ctl->x+ctl->w;x++){
       fb[vga_begin*y+x]=pixels[(y-ctl->y)*ctl->w+(x-ctl->x)];
     }
   }
