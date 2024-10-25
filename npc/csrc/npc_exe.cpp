c44afbeb3971c39c463c7e7a362e62fe7a71517b
tracer-ysyx <tracer@ysyx.org>
2024-10-25 11:55:08 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:55:08 up  1:59,  1 user,  load average: 0.58, 0.61, 0.43

--- npc/csrc/npc_exe.cpp
+++ npc/csrc/npc_exe.cpp
@@ -38,6 +38,7 @@ static uint64_t open_npc_calculate_inst_total = 0;
 extern "C" void open_npc_calculate_ipc(){
   g_clock_cnt = 0;
   open_npc_calculate_inst_total = g_nr_guest_inst;
+  printf("open npc calculate ipc\n");
 }
 #endif
 
