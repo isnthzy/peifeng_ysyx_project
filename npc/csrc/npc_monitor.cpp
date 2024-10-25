415548ac26f24738c9961012e1d9b0d95d21a838
tracer-ysyx <tracer@ysyx.org>
2024-10-25 15:00:26 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:00:26 up  5:04,  1 user,  load average: 0.39, 0.22, 0.17

--- npc/csrc/npc_monitor.cpp
+++ npc/csrc/npc_monitor.cpp
@@ -187,7 +187,7 @@ void init_monitor(int argc, char *argv[]) {
   /* Initialize the simple debugger. */
   init_sdb();
 
-  double ipc = 763687 / 9533727;
+  double ipc = (double)763687 / 9533727;
   printf("npc ipc = %.4f\n", ipc);
 
   IFDEF(CONFIG_TRACE, init_disasm(
