6935e50999dddbff976e959a16272ba34d4a722b
tracer-ysyx <tracer@ysyx.org>
2024-10-25 11:57:23 +0800

    > sim RTL ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:57:23 up  2:01,  1 user,  load average: 0.56, 0.66, 0.48

--- npc/csrc/npc_exe.cpp
+++ npc/csrc/npc_exe.cpp
@@ -46,6 +46,7 @@ static void statistic() {
   IFNDEF(CONFIG_TARGET_AM, setlocale(LC_NUMERIC, ""));
 #define NUMBERIC_FMT MUXDEF(CONFIG_TARGET_AM, "%", "%'") PRIu64
   double ipc = (g_nr_guest_inst - open_npc_calculate_inst_total) / g_clock_cnt;
+  printf("%ld %ld\n", (g_nr_guest_inst - open_npc_calculate_inst_total),g_clock_cnt);
   Log("npc ipc = %.4f", ipc);
   Log("host time spent = " NUMBERIC_FMT " us", g_timer);
   Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
