b1cfe997e4c5f0dfda01d50c011a53b477bf1adc
tracer-ysyx <tracer@ysyx.org>
2024-10-25 11:34:03 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:34:03 up  1:37,  1 user,  load average: 0.58, 0.36, 0.30

--- npc/csrc/npc_exe.cpp
+++ npc/csrc/npc_exe.cpp
@@ -35,7 +35,7 @@ void npc_quit(){
 static uint64_t open_npc_calculate_inst_total = 0;
 //NOTE:开启npc计算时的指令数量，计算ipc时减去他
 #ifdef CONFIG_YSYXSOC
-extern "C" void open_npc_calculate_ipc(){
+extern "C" void open_npc_calculate_ipc(bool valid){
   g_clock_cnt = 0;
   open_npc_calculate_inst_total = g_nr_guest_inst;
 }
