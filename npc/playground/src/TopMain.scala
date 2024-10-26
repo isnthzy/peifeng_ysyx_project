da7250be6733021ef73d304e5dfb8f4e6a13c30a
tracer-ysyx <tracer@ysyx.org>
2024-10-26 10:44:44 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:44:44 up 46 min,  1 user,  load average: 0.38, 0.28, 0.25

--- npc/playground/src/TopMain.scala
+++ npc/playground/src/TopMain.scala
@@ -13,8 +13,8 @@ object TopMain extends App {
     FirtoolOption(
       "--lowering-options=disallowLocalVariables,locationInfoStyle=wrapInAtSquareBracket"
     ), //香山firtool参数
-    FirtoolOption("--preserve-aggregate=vec"),
-    FirtoolOption("--split-verilog"), //分离生成的verilog文件
+    // FirtoolOption("--preserve-aggregate=vec"),
+    // FirtoolOption("--split-verilog"), //分离生成的verilog文件
     FirtoolOption("-o=vsrc"), //设置分离后verilog文件的路径
     FirtoolOption("--disable-all-randomization") //禁止随机数
   )
