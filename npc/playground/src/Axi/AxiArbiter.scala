fe1683e9d1029ad2b78f9e82c0ebb37c43382347
tracer-ysyx <tracer@ysyx.org>
2024-11-01 21:46:10 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:46:10 up  3:15,  1 user,  load average: 0.25, 0.20, 0.22

--- npc/playground/src/Axi/AxiArbiter.scala
+++ npc/playground/src/Axi/AxiArbiter.scala
@@ -61,6 +61,7 @@ class AxiArbiter(inNum: Int) extends Module {
   val writeChosenIdx = RegInit(0.U(log2Ceil(inNum).W))
   (writeArb.io.in zip io.in.map(_.wr)).foreach{case (writeArb,in) => writeArb <> in}
   val chose = dontTouch(Wire(Bool()))
+  val chossss= dontTouch(0.U === writeArb.io.chosen && writeArb.io.out.fire && ArbWriteState === arb_write_idle)
   for(i <- 0 until inNum){
     when(i.U === writeArb.io.chosen && writeArb.io.out.fire 
       && ArbWriteState === arb_write_idle){
