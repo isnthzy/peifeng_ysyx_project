657cb00f95a9395aefb77d64481d9ba3e31cb44c
tracer-ysyx <tracer@ysyx.org>
2024-11-01 18:57:12 +0800

    > generate verilog ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:57:12 up 26 min,  1 user,  load average: 0.81, 0.32, 0.17

--- npc/playground/src/Axi/AxiBridge.scala
+++ npc/playground/src/Axi/AxiBridge.scala
@@ -43,7 +43,7 @@ class Axi4Bridge extends Module with CacheConfig {
   io.ar.bits.size:=Mux(readCacheLine,"b10".U,io.in.rd.bits.stype)
   io.r.ready:=true.B
 
-  io.in.rd.ready:=io.ar.ready
+  io.in.rd.ready:=ReadRequstState===ar_idle
   io.in.rret.valid:=io.r.valid
   io.in.rret.bits.data:=io.r.bits.data
   io.in.rret.bits.last:=io.r.bits.last
