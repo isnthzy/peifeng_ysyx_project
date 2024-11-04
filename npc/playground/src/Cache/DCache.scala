fcba2f82f49c52da4fb47b3c9a99eee27fd32c44
wangxin-wsl2 <isnthzy@gmail.com>
2024-11-04 10:36:27 +0800

    add(ICACHE): burst Icache && Pseudo-dcache supports memory access signal transfer signal CacheIO

---
+++ npc/playground/src/Cache/DCache.scala
@@ -0,0 +1,34 @@
+package Cache
+import chisel3._
+import chisel3.util._
+import CoreConfig.Configs._
+import Axi.{AxiBridgeAddrLoad,AxiBridgeStore,AxiBridgeDataLoad}
+//NOTE:DCache未实现
+class DCache extends Module{
+  val io=IO(new Bundle{
+    val in=Flipped(new Bundle {
+      val al=new AxiBridgeAddrLoad()
+      val dl=new AxiBridgeDataLoad()
+      val s =new AxiBridgeStore()
+    })
+    val out=new AxiCacheIO()
+  })
+  io.out.rret.ready:=true.B
+  io.out.wret.ready:=true.B
+
+  io.out.rd.valid:=io.in.al.ren
+  io.out.rd.bits.stype:=io.in.al.rsize
+  io.out.rd.bits.addr :=io.in.al.raddr
+  io.in.al.raddr_ok:=io.out.rd.fire
+  io.in.dl.rdata:=io.out.rret.bits.data
+  io.in.dl.rdata_ok:=io.out.rret.fire
+
+  io.out.wr.valid:=io.in.s.wen
+  io.out.wr.bits.stype:=io.in.s.wsize
+  io.out.wr.bits.addr:=io.in.s.waddr
+  io.out.wr.bits.data:=io.in.s.wdata
+  io.out.wr.bits.strb:=io.in.s.wstrb
+  dontTouch(io.out.wr.ready)
+  io.in.s.waddr_ok:=io.out.wr.fire
+  io.in.s.wdata_ok:=io.out.wret.fire
+}
--- npc/playground/src/Cache/ICache.scala
+++ npc/playground/src/Cache/ICache.scala
@@ -2,11 +2,11 @@ package Cache
 import chisel3._
 import chisel3.util._
 import CoreConfig.CacheConfig
-import Axi.AxiBridge
+// import Axi.AxiBridge
 import Util.RandomNum
 
 class ICache extends Module with CacheConfig {
-  val BANK_SIZE = 1 << LINE_WIDTH
+  val BANK_SIZE = 1 << INDEX_WIDTH
   val io = IO(new Bundle {
     val valid  = Input(Bool())
     val tag    = Input(UInt(TAG_WIDTH.W)) //tag下一周期输入
@@ -21,7 +21,7 @@ class ICache extends Module with CacheConfig {
   val DataBank = Array.fill(WAY_NUM_I)(Module(new DataRAM(BANK_SIZE, LINE_WIDTH)).io)
   val TagvBank = Array.fill(WAY_NUM_I)(Module(new TagvRAM(BANK_SIZE, TAG_WIDTH)).io)
   //外部维护一个tagv，根据reset清除
-  val tagValid = RegInit(VecInit(Seq.fill(BANK_SIZE)(0.U(WAY_NUM_I.W))))
+  val tagValid = RegInit(VecInit(Seq.fill(BANK_SIZE)(0.U.asTypeOf(Vec(WAY_NUM_I,Bool())))))
   val readTagv = Wire(Vec(WAY_NUM_I, new Bundle {
     val v = Bool()
     val tag = UInt(TAG_WIDTH.W)
@@ -31,14 +31,14 @@ class ICache extends Module with CacheConfig {
   val requestIdxBuff = RegInit(0.U(INDEX_WIDTH.W))
   val requestTagBuff = RegInit(0.U(TAG_WIDTH.W))
   val requestOffsetBuff = RegInit(0.U(OFFSET_WIDTH.W))
-  val hitWayBuff = RegInit(0.U(log2Ceil(WAY_NUM_I).W))
   val readDataLineBuff = RegInit(VecInit(Seq.fill(LINE_WORD_NUM)(0.U(32.W))))
   val readDataLineIdx  = RegInit(0.U(log2Ceil(LINE_WORD_NUM).W))
-  
+
+  val idxConflit = Wire(Vec(2,Bool()))  
   for(i <- 0 until WAY_NUM_I){
-    readTagv(i).v   := tagValid(requestIdxBuff)(i)
+    readTagv(i).v   := RegNext(tagValid(requestIdxBuff)(i))
     readTagv(i).tag := TagvBank(i).douta
-    readData(i) := DataBank(i).doutb
+    readData(i) := Mux(RegNext(idxConflit(i)),readDataLineBuff.asUInt,DataBank(i).doutb)
   }
 
   val (s_idle   ::
@@ -46,31 +46,55 @@ class ICache extends Module with CacheConfig {
        s_miss   ::
        s_refill  ::
        s_respond ::
-       Nil) = Enum(7)
+       Nil) = Enum(5)
   val cacheState = RegInit(s_idle)
-  val reqIndex = WireDefault(0.U(log2Ceil(INDEX_WIDTH).W))
-  val cacheReqValid = cacheState === s_idle && io.valid
+  val isIdle    = cacheState === s_idle
+  val isLookup  = cacheState === s_lookup
+  val isMiss    = cacheState === s_miss
+  val isRefill  = cacheState === s_refill
+  val isRespond = cacheState === s_respond
+
+  val cacheLookupHit = WireDefault(false.B)
+  val cacheHitWay    = WireDefault(0.U(log2Ceil(WAY_NUM_I).W))
+  val dataReqIdx = Wire(UInt(INDEX_WIDTH.W))
+  val cacheReqValid = (isIdle  || isRespond
+                    ||(cacheLookupHit && isLookup)) && io.valid
   val randomWay = RandomNum("b10111011".U)(log2Ceil(WAY_NUM_I) - 1,0)
+  val replaceWayBuff = RegInit(0.U(log2Ceil(WAY_NUM_I).W))
+  dataReqIdx := Mux(cacheReqValid,io.index,requestIdxBuff)
+
   for(i <- 0 until WAY_NUM_I){
+    idxConflit(i) :=(requestIdxBuff === dataReqIdx 
+                  && isRespond && (replaceWayBuff === i.U))
+
     DataBank(i).clka := clock    
-    DataBank(i).wea  := (cacheState === s_respond) && (hitWayBuff === i.U)
+    DataBank(i).wea  := (isRespond) && (replaceWayBuff === i.U)
     DataBank(i).addra := requestIdxBuff
     DataBank(i).dina  := readDataLineBuff.asUInt
     DataBank(i).clkb  := clock
-    DataBank(i).addrb := Mux(cacheReqValid,io.index,requestIdxBuff)
+    DataBank(i).addrb := Mux(idxConflit(i),(dataReqIdx + 1.U),dataReqIdx)
+    //NOTE:addra与addrb不能是同一地址,因此这里设计
   
     TagvBank(i).clka := clock
-    TagvBank(i).wea  := (cacheState === s_miss) && (randomWay === i.U)
+    TagvBank(i).wea  := isMiss && io.out.rd.fire && randomWay === i.U
     TagvBank(i).addra := Mux(cacheReqValid,io.index,requestIdxBuff)
     TagvBank(i).dina  := requestTagBuff
-    // tagValid(requestIdxBuff)(i) := 
+    when(isMiss && io.out.rd.fire && randomWay === i.U){
+      tagValid(requestIdxBuff)(i) := 1.U
+    }
+    
   } //NOTE:设置默认值，后续通过覆写实现读
 
-  io.dataRp := cacheState === s_respond
-  io.rdata  := readDataLineBuff(requestOffsetBuff(OFFSET_WIDTH - 1,1))
-  io.out.rd.valid := cacheState === s_miss
+  val cacheUnBusy = isIdle || isRespond || cacheLookupHit
+  val targetWordAddr = requestOffsetBuff(OFFSET_WIDTH - 1,2)
+  val dataBankOutLine = readData(cacheHitWay).asTypeOf(readDataLineBuff)
+  
+  io.addrRp := cacheUnBusy
+  io.dataRp := isRespond || cacheLookupHit
+  io.rdata  := Mux(isLookup,dataBankOutLine(targetWordAddr),readDataLineBuff(targetWordAddr))
+  io.out.rd.valid := isMiss
   io.out.rd.bits.stype := "b100".U
-  io.out.rd.bits.addr := Cat(requestTagBuff,requestIdxBuff,requestOffsetBuff)
+  io.out.rd.bits.addr := Cat(requestTagBuff,requestIdxBuff,0.U(OFFSET_WIDTH.W))
   io.out.rret.ready := true.B
   io.out.wret.ready := true.B
 
@@ -84,19 +108,31 @@ class ICache extends Module with CacheConfig {
       }
     }
     is(s_lookup){
-      cacheState := s_miss
       for(i <- 0 until WAY_NUM_I){
-        when(readTagv(i).v && readTagv(i).tag === io.tag){
-          hitWayBuff := i.U
-          cacheState := s_respond
-          readDataLineBuff := readData(i)
+        when(readTagv(i).v && readTagv(i).tag === requestTagBuff){
+          cacheLookupHit := true.B
+          cacheHitWay := i.U
+          readDataLineBuff := readData(i).asTypeOf(readDataLineBuff)
         }
+      }
+      when(cacheLookupHit){
+        when(io.valid){
+          requestTagBuff    := io.tag
+          requestIdxBuff    := io.index
+          requestOffsetBuff := io.offset
+          cacheState := s_lookup
+        }.otherwise{
+          cacheState := s_idle
+        }
+      }.otherwise{
+        cacheState := s_miss
       }
     }
     is(s_miss){
       readDataLineIdx  := 0.U
       readDataLineBuff := 0.U.asTypeOf(readDataLineBuff)
       when(io.out.rd.fire){
+        replaceWayBuff := randomWay
         cacheState := s_refill
       }
     }
@@ -121,7 +157,8 @@ class ICache extends Module with CacheConfig {
     }
   }
 
-
+  io.out.wr.bits:=0.U.asTypeOf(io.out.wr.bits)
+  io.out.wr.valid:=false.B
 }
 
 
@@ -132,13 +169,13 @@ class AxiCacheReadIO extends Bundle{
 
 class AxiCacheReadReturnIO extends Bundle{
   val last  = Input(Bool())
-  val port  = Input(UInt(2.W))
+  val resp  = Input(UInt(2.W))
   val data  = Input(UInt(32.W))
 }
 
 class AxiCacheWriteReturnIO extends Bundle{
   val last  = Input(Bool())
-  val port  = Input(UInt(2.W))
+  val resp  = Input(UInt(2.W))
 }
 
 class AxiCacheWriteIO extends Bundle{
@@ -156,4 +193,16 @@ class AxiCacheIO extends Bundle{
   val wr  = DecoupledIO(new AxiCacheWriteIO())
   val rret = Flipped(DecoupledIO(new AxiCacheReadReturnIO()))
   val wret = Flipped(DecoupledIO(new AxiCacheWriteReturnIO()))
+}
+
+class Core2AxiReadIO extends Bundle{
+  val req  = Output(Bool())
+  val addr = Output(UInt(32.W))
+  val size = Output(UInt(3.W))
+  val addrOk = Input(Bool())
+}
+
+class Core2AxiRespondIO extends Bundle{
+  val dataOk= Input(Bool())
+  val data  = Input(UInt(32.W))
 }
