4503a22eccb2e03436fc552c3b5635934aca8ca5
tracer-ysyx <tracer@ysyx.org>
2024-11-28 10:19:49 +0800

    > compile NEMU ysyx_23060115 王植鑫 Linux LAPTOP-HO4CPFBD 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:19:49 up 58 min,  1 user,  load average: 0.33, 0.15, 0.11

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -5,7 +5,7 @@ void init_trace(const char *build_path){
   char itrace_build_path[512];
   // char mtrace_build_path[512];
   if (build_path != NULL) {
-    sprintf(itrace_build_path, "%s/npc-itrace.txt", build_path);
+    sprintf(itrace_build_path, "%s/nemu-itrace.txt", build_path);
     // sprintf(mtrace_build_path, "%s/mtrace.txt", build_path);
     itrace_fp = fopen(itrace_build_path, "w");
     Assert(itrace_fp, "Can not open '%s'", itrace_build_path);
