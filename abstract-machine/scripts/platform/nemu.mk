f395d0768030f0ba14acbfafb461ba5ada79e2ad
isnthzy <isnthzy@gmail.com>
2024-11-28 09:38:29 +0800

    add: use build path to store traces

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -12,12 +12,13 @@ LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
 # NEMUFLAGS += -b
-NEMUFLAGS += -f $(IMAGE).elf
+# NEMUFLAGS += -f $(IMAGE).elf
 #最后在native.mk编译,一个名叫riscv32-nemu-interpreter 的c++文件传入int main(argc, argv)[nemu-main.c]
 #/home/wangxin/ysyx-workbench/nemu/build/riscv32-nemu-interpreter 
 # -l -b /home/wangxin/ysyx-workbench/am-kernels/tests/cpu-tests/build/nemu-log.txt
 # /home/wangxin/ysyx-workbench/am-kernels/tests/cpu-tests/build/bit-riscv32-nemu.bin
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
+NEMUFLAGS += --path=$(shell dirname $(IMAGE).elf)
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
